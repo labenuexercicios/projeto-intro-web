8ca59bd7ac16ec272a89a4bb937d1e988bf6d54b
MathMendesReis <matheusmendesreis@outlook.com>
2022-07-25 19:03:52 -0300

    Exercicio concluido

---
+++ Semana1.js
@@ -0,0 +1,27 @@
+// Exercicio 1
+let nomeSerie;
+let anoLancamentoSerie;
+let isBrasileira;
+
+// Exercicio 2
+const nomeSerie1 = "Lá Casa de Papel";
+const anoLancamentoSerie1 = 2017;
+const isBrasileira1 = false;
+
+const nomeSerie2 = "Sob Pressão";
+const anoLancamentoSerie2 = 2017;
+const isBrasileira2 = true;
+
+const nomeSerie3 = "Greys Anatomy";
+const anoLancamentoSerie3 = 2005;
+const isBrasileira3 = false;
+
+// Exercicio 3
+const media = (anoLancamentoSerie1 + anoLancamentoSerie2 + anoLancamentoSerie3) / 3;
+
+console.log("A media e igual a",media);
+
+// Exercicio 4
+console.log(isBrasileira1);
+console.log(isBrasileira2);
+console.log(isBrasileira3);
