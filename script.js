515062ff5a0b3ec96941c404a26669ba7cf4ea9a
Adriano Silva <adrianowic0101@gmail.com>
2022-07-28 16:11:53 -0300

    Foi criado um arquivo HTML e JS, e foi feito  o link entre elas. Três conjuntos de variaveis foram criadas, com os valores de string, number e booleano. Foi feito um calculo de média dos valores number das variaveis e o resultado guardado em uma outra variavel chamada media. Uma verificação para saber se os valores das variaveis lista é true também foi feita e o resultado guardado numa variavel chamada verificarLista.

---
+++ script.js
@@ -0,0 +1,19 @@
+const nome1 = 'Sandman - Preludios e Noturnos';
+const arco1 = 1;
+const lista1 = true
+
+const nome2 = 'Sandman - Casa de Bonecas';
+const arco2 = 2;
+const lista2 = true;
+
+const nome3 = 'Sandman - Terra dos Sonhos';
+const arco3 = 3;
+const lista3 = true;
+
+//Calculando a media
+const media = (arco1+arco2+arco3)/3
+console.log(media)
+
+//Verificando a lista
+const verificarLista = lista1 && lista2 && lista3
+console.log(verificarLista)
