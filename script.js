59341325f4a3ccce61deed83fe832e0ae079c26c
roni <ronildo.sad2@hotmail.com>
2022-07-29 22:55:13 -0300

    enviando meu projeto web

---
+++ script.js
@@ -0,0 +1,72 @@
+const nome1 = "Aranha-marrom";
+const tempoMedioDeVida1 = 2;
+const arrayCor1 = [ "Marrom" ];
+const extinto1 = false;
+
+const nome2 = "Viuva-negra";
+const tempoMedioDeVida2 = 3;
+const arrayCor2 = [ "preta" ];
+const extinto2 = false;
+
+
+const nome3 = "Aranha-armadeira";
+const tempoMedioDeVida3 = 2;
+const arrayCor3 = [ "marrom" ];
+const extinto3 = false;
+
+let mediaDeTempoDeVida = tempoMedioDeVida1 + tempoMedioDeVida2 + tempoMedioDeVida3
+let mediaDeTempoDeVidaFinal =  mediaDeTempoDeVida / 3
+
+let media = mediaDeTempoDeVidaFinal
+
+
+
+
+
+
+console.log("A media de vida é:",media)
+
+
+
+const verificarExtinto = extinto1 && extinto2 && extinto3;
+
+console.log(verificarExtinto)
+
+
+
+console.log(
+
+`${nome1.toLocaleUpperCase()} 
+Tempo de vida media: ${tempoMedioDeVida1}
+Qual a cor da aranha: ${arrayCor1}
+Ela esté extinta? ${extinto1}`
+    
+           )
+console.log(
+
+`${nome2.toLocaleUpperCase()} 
+Tempo de vida media: ${tempoMedioDeVida2}
+Qual a cor da aranha: ${arrayCor2}
+Ela esté extinta? ${extinto2}`
+    
+           )
+console.log(
+
+`${nome3.toLocaleUpperCase()} 
+Tempo de vida media: ${tempoMedioDeVida3}
+Qual a cor da aranha: ${arrayCor3}
+Ela esté extinta? ${extinto3}`
+    
+           )
+
+
+           c
+   
+
+
+
+
+//FULANO
+// idade: 33
+// verificou email?: false
+// projetos: ["Projeto de HTML", "Projeto de CSS", "Projeto React"] 
