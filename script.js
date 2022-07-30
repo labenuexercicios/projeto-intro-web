7a422bbb7d8472045a4a47436382fd5a6fe09095
Bruno Ferreira <bgsf1991@gmail.com>
2022-07-29 23:07:35 -0300

    Projeto Intro Web (Semana1)

---
+++ script.js
@@ -0,0 +1,74 @@
+/////////////////////// 10 LINGUAGENS DE PROGRAMAÇÃO POPULARES ////////////////////////////
+
+const linguagem1 = 'Python';
+const ranking1 = 1;
+const indicadoPara1 = 'Iniciantes';
+const dificuldade1 = 'Fácil';
+const facilDeAprender1 = true;
+const mediaSalarial1 = 5469;
+
+const linguagem2 = 'C#';
+const ranking2 = 2;
+const indicadoPara2 = 'Iniciantes e Intermediários';
+const dificuldade2 = 'Moderada';
+const facilDeAprender2 = true;
+const mediaSalarial2 = 4624;
+
+const linguagem3 = 'C++';
+const ranking3 = 3;
+const indicadoPara3 = 'Programadores Experientes';
+const dificuldade3 = 'Difícil';
+const facilDeAprender3 = false;
+const mediaSalarial3 = 7156;
+
+const linguagem4 = 'JavaScript';
+const ranking4 = 4;
+const indicadoPara4 = 'Iniciantes';
+const dificuldade4 = 'Relativamente Fácil';
+const facilDeAprender4 = true;
+const mediaSalarial4 = 4814;
+
+const linguagem5 = 'PHP';
+const ranking5 = 5;
+const indicadoPara5 = 'Iniciantes';
+const dificuldade5 = 'Bastante Fácil';
+const facilDeAprender5 = true;
+const mediaSalarial5 = 5082;
+
+const linguagem6 = 'Swift';
+const ranking6 = 6;
+const indicadoPara6 = 'Iniciantes';
+const dificuldade6 = 'Fácil';
+const facilDeAprender6 = true;
+const mediaSalarial6 = 6021;
+
+const linguagem7 = 'Java';
+const ranking7 = 7;
+const indicadoPara7 = 'Iniciantes e Intermediários';
+const dificuldade7 = 'Moderado';
+const facilDeAprender7 = true;
+const mediaSalarial7 = 6000;
+
+const linguagem8 = 'Go';
+const ranking8 = 8;
+const indicadoPara8 = 'Iniciantes';
+const dificuldade8 = 'Fácil';
+const facilDeAprender8 = true;
+const mediaSalarial8 = 4995;
+
+const linguagem9 = 'SQL';
+const ranking9 = 9;
+const indicadoPara9 = 'Iniciantes';
+const dificuldade9 = 'Fácil';
+const facilDeAprender9 = true;
+const mediaSalarial9 = 5763;
+
+const linguagem10 = 'Ruby';
+const ranking10 = 10;
+const indicadoPara10 = 'Iniciantes';
+const dificuldade10 = 'Fácil';
+const facilDeAprender10 = true;
+const mediaSalarial10 = 5560;
+
+const mediaFinal = (mediaSalarial1 + mediaSalarial2 + mediaSalarial3 + mediaSalarial4 + mediaSalarial5 + mediaSalarial6 + mediaSalarial7 + mediaSalarial8 + mediaSalarial9 + mediaSalarial10) / 10;
+console.log('Média salarial:', mediaFinal);
