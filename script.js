4ffa2a3fc3a55827ac79abdd34e2e5448463d02b
MathMendesReis <matheusmendesreis@outlook.com>
2022-07-29 22:27:04 -0300

    Exercicio da semana dois concluido

---
+++ script.js
@@ -0,0 +1,35 @@
+// Exercicio 1
+let nomeSerie;
+let anoLancamentoSerie;
+let isBrasileira;
+
+// Exercicio 2
+const nomeSerie1 = "Lá Casa de Papel";
+const anoLancamentoSerie1 = 2017;
+const isBrasileira1 = false;
+const atoresParticipantes1 = ["Pedro Alonso", "Alba Flores", "Esther Acebo",];
+
+const nomeSerie2 = "Sob Pressão";
+const anoLancamentoSerie2 = 2017;
+const isBrasileira2 = true;
+const atoresParticipantes2 = ["Julio Andrade", "Marjorie Estiano", "Bruno Garcia"];
+
+const nomeSerie3 = "Greys Anatomy";
+const anoLancamentoSerie3 = 2005;
+const isBrasileira3 = false;
+const atoresParticipantes3 = ["Ellen Pompeo", "Sandra Oh", "Justin Chambers"];
+
+// Exercicio 3
+const media = (anoLancamentoSerie1 + anoLancamentoSerie2 + anoLancamentoSerie3) / 3;
+
+console.log("A media e igual a",media);
+
+// Exercicio 4
+console.log(isBrasileira1);
+console.log(isBrasileira2);
+console.log(isBrasileira3);
+console.log("Relatorio: \nNome da Serie: "+nomeSerie1+"\nAno de lançamento: "+anoLancamentoSerie1+"\nA serie é uma produção brasileira: "+isBrasileira1+"\nFazem parte do elenco: "+atoresParticipantes1[0]+", "+atoresParticipantes1[1]+" e "+atoresParticipantes1[2]+".");
+
+console.log("Relatorio: \nNome da Serie: "+nomeSerie2+"\nAno de lançamento: "+anoLancamentoSerie2+"\nA serie é uma produção brasileira: "+isBrasileira2+"\nFazem parte do elenco: "+atoresParticipantes2[0]+", "+atoresParticipantes2[1]+" e "+atoresParticipantes2[2]+".");
+
+console.log("Relatorio: \nNome da Serie: "+nomeSerie3+"\nAno de lançamento: "+anoLancamentoSerie3+"\nA serie é uma produção brasileira: "+isBrasileira3+"\nFazem parte do elenco: "+atoresParticipantes3[0]+", "+atoresParticipantes3[1]+" e "+atoresParticipantes3[2]+".");
