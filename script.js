4b6fb4306fd9fd247754e008a4910fb64b1a1392
MathMendesReis <matheusmendesreis@outlook.com>
2022-08-02 21:20:00 -0300

    Semana 3 - FEITO POR COMPLETO

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
-// Exercicio 1
-let nomeSerie;
-let anoLancamentoSerie;
-let isBrasileira;
+// // Exercicio 1
+// let nomeSerie;
+// let anoLancamentoSerie;
+// let isBrasileira;
 
-// Exercicio 2
-const nomeSerie1 = "Lá Casa de Papel";
-const anoLancamentoSerie1 = 2017;
-const isBrasileira1 = false;
-const atoresParticipantes1 = ["Pedro Alonso", "Alba Flores", "Esther Acebo",];
+// // Exercicio 2
+// const nomeSerie1 = "Lá Casa de Papel";
+// const anoLancamentoSerie1 = 2017;
+// const isBrasileira1 = false;
+// const atoresParticipantes1 = ["Pedro Alonso", "Alba Flores", "Esther Acebo",];
 
-const nomeSerie2 = "Sob Pressão";
-const anoLancamentoSerie2 = 2017;
-const isBrasileira2 = true;
-const atoresParticipantes2 = ["Julio Andrade", "Marjorie Estiano", "Bruno Garcia"];
+// const nomeSerie2 = "Sob Pressão";
+// const anoLancamentoSerie2 = 2017;
+// const isBrasileira2 = true;
+// const atoresParticipantes2 = ["Julio Andrade", "Marjorie Estiano", "Bruno Garcia"];
 
-const nomeSerie3 = "Greys Anatomy";
-const anoLancamentoSerie3 = 2005;
-const isBrasileira3 = false;
-const atoresParticipantes3 = ["Ellen Pompeo", "Sandra Oh", "Justin Chambers"];
+// const nomeSerie3 = "Greys Anatomy";
+// const anoLancamentoSerie3 = 2005;
+// const isBrasileira3 = false;
+// const atoresParticipantes3 = ["Ellen Pompeo", "Sandra Oh", "Justin Chambers"];
 
-// Exercicio 3
-const media = (anoLancamentoSerie1 + anoLancamentoSerie2 + anoLancamentoSerie3) / 3;
+// // Exercicio 3
+// const media = (anoLancamentoSerie1 + anoLancamentoSerie2 + anoLancamentoSerie3) / 3;
 
-console.log("A media e igual a",media);
+// console.log("A media e igual a",media);
 
-// Exercicio 4
-console.log(isBrasileira1);
-console.log(isBrasileira2);
-console.log(isBrasileira3);
-console.log("Relatorio: \nNome da Serie: "+nomeSerie1+"\nAno de lançamento: "+anoLancamentoSerie1+"\nA serie é uma produção brasileira: "+isBrasileira1+"\nFazem parte do elenco: "+atoresParticipantes1[0]+", "+atoresParticipantes1[1]+" e "+atoresParticipantes1[2]+".");
+// // Exercicio 4
+// console.log(isBrasileira1);
+// console.log(isBrasileira2);
+// console.log(isBrasileira3);
+// console.log("Relatorio: \nNome da Serie: "+nomeSerie1.toUpperCase()+"\nAno de lançamento: "+anoLancamentoSerie1+"\nA serie é uma produção brasileira: "+isBrasileira1+"\nFazem parte do elenco: "+atoresParticipantes1[0]+", "+atoresParticipantes1[1]+" e "+atoresParticipantes1[2]+".");
 
-console.log("Relatorio: \nNome da Serie: "+nomeSerie2+"\nAno de lançamento: "+anoLancamentoSerie2+"\nA serie é uma produção brasileira: "+isBrasileira2+"\nFazem parte do elenco: "+atoresParticipantes2[0]+", "+atoresParticipantes2[1]+" e "+atoresParticipantes2[2]+".");
+// console.log("Relatorio: \nNome da Serie: "+nomeSerie2.toUpperCase()+"\nAno de lançamento: "+anoLancamentoSerie2+"\nA serie é uma produção brasileira: "+isBrasileira2+"\nFazem parte do elenco: "+atoresParticipantes2[0]+", "+atoresParticipantes2[1]+" e "+atoresParticipantes2[2]+".");
 
-console.log("Relatorio: \nNome da Serie: "+nomeSerie3+"\nAno de lançamento: "+anoLancamentoSerie3+"\nA serie é uma produção brasileira: "+isBrasileira3+"\nFazem parte do elenco: "+atoresParticipantes3[0]+", "+atoresParticipantes3[1]+" e "+atoresParticipantes3[2]+".");
+// console.log("Relatorio: \nNome da Serie: "+nomeSerie3.toUpperCase()+"\nAno de lançamento: "+anoLancamentoSerie3+"\nA serie é uma produção brasileira: "+isBrasileira3+"\nFazem parte do elenco: "+atoresParticipantes3[0]+", "+atoresParticipantes3[1]+" e "+atoresParticipantes3[2]+".");
+
+const series ={
+    nome: [],
+    anoDelancamento: 0.0,
+    elencoPrincipal: []
+};
+
+const bancoDeSeries = []
+
+const laCasaDePapel = {...series, nome: 'lá casa de papel', anoDelancamento: 2017, elencoPrincipal: ['Álvaro Morte', 'Úrsula Corberó', 'Pedro Alonso']}
+
+const sobPressao = {...series, nome: 'Sob Pressão', anoDelancamento: 2017, elencoPrincipal: ['Ellen Pompeo", "Sandra Oh", "Justin Chambers']}
+
+const greysAnatomy = {...series, nome: 'Greys Anatomy', anoDelancamento: 2005, elencoPrincipal: ['Ellen Pompeo", "Sandra Oh", "Justin Chambers']}
+
+
+bancoDeSeries.push(laCasaDePapel, sobPressao, greysAnatomy)
+
