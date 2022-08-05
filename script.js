d4d4b1905502e42995fcf8557522621ff39d9675
Bruno Ferreira <bgsf1991@gmail.com>
2022-08-05 19:46:40 -0300

    Projeto Intro Web (Semana2)

--- script.js
+++ script.js
@@ -5,13 +5,23 @@ const ranking1 = 1;
 const indicadoPara1 = 'Iniciantes';
 const dificuldade1 = 'Fácil';
 const facilDeAprender1 = true;
+const usos1 = ['Desenvolvimento web back-end', 'Aplicações Desktop', 'Ciência de Dados', 'Automação', 'Machine Learning'];
 const mediaSalarial1 = 5469;
+// const linguagem1 = {
+//     nome: 'Python',
+//     indicadoPara: 'Iniciantes',
+//     dificuldade: 'Fácil',
+//     facilDeAprender: true,
+//     usos: ['Desenvolvimento web back-end', 'Aplicações Desktop', 'Ciência de Dados', 'Automação', 'Machine Learning'],
+//     mediaSalarial: 5469
+// }
 
 const linguagem2 = 'C#';
 const ranking2 = 2;
 const indicadoPara2 = 'Iniciantes e Intermediários';
 const dificuldade2 = 'Moderada';
 const facilDeAprender2 = true;
+const usos2 = ['Aplicações web', 'Aplicações móveis', 'Desenvolvimento de jogos', 'Realidade virtual', 'Aplicações Linux e Mac'];
 const mediaSalarial2 = 4624;
 
 const linguagem3 = 'C++';
@@ -19,6 +29,7 @@ const ranking3 = 3;
 const indicadoPara3 = 'Programadores Experientes';
 const dificuldade3 = 'Difícil';
 const facilDeAprender3 = false;
+const usos3 = ['Desenvolvimento mobile', 'Desenvolvimento de videogames', 'Machine Learning', 'Aplicações Server-side e Client-side', 'Sistemas Operacionais'];
 const mediaSalarial3 = 7156;
 
 const linguagem4 = 'JavaScript';
@@ -26,6 +37,7 @@ const ranking4 = 4;
 const indicadoPara4 = 'Iniciantes';
 const dificuldade4 = 'Relativamente Fácil';
 const facilDeAprender4 = true;
+const usos4 = ['Desenvolvimento web front-end', 'Desenvolvimento de jogos', 'Aplicações web'];
 const mediaSalarial4 = 4814;
 
 const linguagem5 = 'PHP';
@@ -33,6 +45,7 @@ const ranking5 = 5;
 const indicadoPara5 = 'Iniciantes';
 const dificuldade5 = 'Bastante Fácil';
 const facilDeAprender5 = true;
+const usos5 = ['Desenvolvimento web', 'Aplicações Desktop', 'Programas de Computador'];
 const mediaSalarial5 = 5082;
 
 const linguagem6 = 'Swift';
@@ -40,6 +53,7 @@ const ranking6 = 6;
 const indicadoPara6 = 'Iniciantes';
 const dificuldade6 = 'Fácil';
 const facilDeAprender6 = true;
+const usos6 = ['Desenvolvimento de Software', 'Desenvolvimento macOS e iOS'];
 const mediaSalarial6 = 6021;
 
 const linguagem7 = 'Java';
@@ -47,6 +61,7 @@ const ranking7 = 7;
 const indicadoPara7 = 'Iniciantes e Intermediários';
 const dificuldade7 = 'Moderado';
 const facilDeAprender7 = true;
+const usos7 = ['Desenvolvimento mobile', 'Aplicações Desktop', 'Aplicações Web', 'Desenvolvimento de Jogos', 'Machine Learning', 'Computação Científica'];
 const mediaSalarial7 = 6000;
 
 const linguagem8 = 'Go';
@@ -54,6 +69,7 @@ const ranking8 = 8;
 const indicadoPara8 = 'Iniciantes';
 const dificuldade8 = 'Fácil';
 const facilDeAprender8 = true;
+const usos8 = ['Desenvolvimento web back-end', 'Aplicações Web', 'Big Data', 'Machine Learning'];
 const mediaSalarial8 = 4995;
 
 const linguagem9 = 'SQL';
@@ -61,6 +77,7 @@ const ranking9 = 9;
 const indicadoPara9 = 'Iniciantes';
 const dificuldade9 = 'Fácil';
 const facilDeAprender9 = true;
+const usos9 = ['Ciência de Dados', 'Gerenciamento de Banco de Dados', 'Business Intelligence'];
 const mediaSalarial9 = 5763;
 
 const linguagem10 = 'Ruby';
@@ -68,7 +85,12 @@ const ranking10 = 10;
 const indicadoPara10 = 'Iniciantes';
 const dificuldade10 = 'Fácil';
 const facilDeAprender10 = true;
+const usos10 = ['Desenvolvimento web front-end', 'Desenvolvimento web back-end'];
 const mediaSalarial10 = 5560;
 
 const mediaFinal = (mediaSalarial1 + mediaSalarial2 + mediaSalarial3 + mediaSalarial4 + mediaSalarial5 + mediaSalarial6 + mediaSalarial7 + mediaSalarial8 + mediaSalarial9 + mediaSalarial10) / 10;
-console.log('Média salarial:', mediaFinal);
+console.log('Média salarial somando todas as linguagens listadas:', mediaFinal);
+
+const facilDeAprender = facilDeAprender1 && facilDeAprender2 && facilDeAprender3 && facilDeAprender4 && facilDeAprender5 && facilDeAprender6 && facilDeAprender7 && facilDeAprender8 && facilDeAprender9 && facilDeAprender10;
+console.log(facilDeAprender);
+
