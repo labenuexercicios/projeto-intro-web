f5b3e5710c0395c0c7eeb01d5da31ccf1d441c3f
Bruno Ferreira <bgsf1991@gmail.com>
2022-08-05 21:09:49 -0300

    Projeto Intro Web (Semana3)

--- script.js
+++ script.js
@@ -1,96 +1,183 @@
 /////////////////////// 10 LINGUAGENS DE PROGRAMAÇÃO POPULARES ////////////////////////////
 
-const linguagem1 = 'Python';
-const ranking1 = 1;
-const indicadoPara1 = 'Iniciantes';
-const dificuldade1 = 'Fácil';
-const facilDeAprender1 = true;
-const usos1 = ['Desenvolvimento web back-end', 'Aplicações Desktop', 'Ciência de Dados', 'Automação', 'Machine Learning'];
-const mediaSalarial1 = 5469;
-// const linguagem1 = {
-//     nome: 'Python',
-//     indicadoPara: 'Iniciantes',
-//     dificuldade: 'Fácil',
-//     facilDeAprender: true,
-//     usos: ['Desenvolvimento web back-end', 'Aplicações Desktop', 'Ciência de Dados', 'Automação', 'Machine Learning'],
-//     mediaSalarial: 5469
-// }
+// const linguagem1 = 'Python';
+// const ranking1 = 1;
+// const indicadoPara1 = 'Iniciantes';
+// const dificuldade1 = 'Fácil';
+// const facilDeAprender1 = true;
+// const usos1 = ['Desenvolvimento web back-end', 'Aplicações Desktop', 'Ciência de Dados', 'Automação', 'Machine Learning'];
+// const mediaSalarial1 = 5469;
 
-const linguagem2 = 'C#';
-const ranking2 = 2;
-const indicadoPara2 = 'Iniciantes e Intermediários';
-const dificuldade2 = 'Moderada';
-const facilDeAprender2 = true;
-const usos2 = ['Aplicações web', 'Aplicações móveis', 'Desenvolvimento de jogos', 'Realidade virtual', 'Aplicações Linux e Mac'];
-const mediaSalarial2 = 4624;
+// const linguagem2 = 'C#';
+// const ranking2 = 2;
+// const indicadoPara2 = 'Iniciantes e Intermediários';
+// const dificuldade2 = 'Moderada';
+// const facilDeAprender2 = true;
+// const usos2 = ['Aplicações web', 'Aplicações móveis', 'Desenvolvimento de jogos', 'Realidade virtual', 'Aplicações Linux e Mac'];
+// const mediaSalarial2 = 4624;
 
-const linguagem3 = 'C++';
-const ranking3 = 3;
-const indicadoPara3 = 'Programadores Experientes';
-const dificuldade3 = 'Difícil';
-const facilDeAprender3 = false;
-const usos3 = ['Desenvolvimento mobile', 'Desenvolvimento de videogames', 'Machine Learning', 'Aplicações Server-side e Client-side', 'Sistemas Operacionais'];
-const mediaSalarial3 = 7156;
+// const linguagem3 = 'C++';
+// const ranking3 = 3;
+// const indicadoPara3 = 'Programadores Experientes';
+// const dificuldade3 = 'Difícil';
+// const facilDeAprender3 = false;
+// const usos3 = ['Desenvolvimento mobile', 'Desenvolvimento de videogames', 'Machine Learning', 'Aplicações Server-side e Client-side', 'Sistemas Operacionais'];
+// const mediaSalarial3 = 7156;
 
-const linguagem4 = 'JavaScript';
-const ranking4 = 4;
-const indicadoPara4 = 'Iniciantes';
-const dificuldade4 = 'Relativamente Fácil';
-const facilDeAprender4 = true;
-const usos4 = ['Desenvolvimento web front-end', 'Desenvolvimento de jogos', 'Aplicações web'];
-const mediaSalarial4 = 4814;
+// const linguagem4 = 'JavaScript';
+// const ranking4 = 4;
+// const indicadoPara4 = 'Iniciantes';
+// const dificuldade4 = 'Relativamente Fácil';
+// const facilDeAprender4 = true;
+// const usos4 = ['Desenvolvimento web front-end', 'Desenvolvimento de jogos', 'Aplicações web'];
+// const mediaSalarial4 = 4814;
 
-const linguagem5 = 'PHP';
-const ranking5 = 5;
-const indicadoPara5 = 'Iniciantes';
-const dificuldade5 = 'Bastante Fácil';
-const facilDeAprender5 = true;
-const usos5 = ['Desenvolvimento web', 'Aplicações Desktop', 'Programas de Computador'];
-const mediaSalarial5 = 5082;
+// const linguagem5 = 'PHP';
+// const ranking5 = 5;
+// const indicadoPara5 = 'Iniciantes';
+// const dificuldade5 = 'Bastante Fácil';
+// const facilDeAprender5 = true;
+// const usos5 = ['Desenvolvimento web', 'Aplicações Desktop', 'Programas de Computador'];
+// const mediaSalarial5 = 5082;
 
-const linguagem6 = 'Swift';
-const ranking6 = 6;
-const indicadoPara6 = 'Iniciantes';
-const dificuldade6 = 'Fácil';
-const facilDeAprender6 = true;
-const usos6 = ['Desenvolvimento de Software', 'Desenvolvimento macOS e iOS'];
-const mediaSalarial6 = 6021;
+// const linguagem6 = 'Swift';
+// const ranking6 = 6;
+// const indicadoPara6 = 'Iniciantes';
+// const dificuldade6 = 'Fácil';
+// const facilDeAprender6 = true;
+// const usos6 = ['Desenvolvimento de Software', 'Desenvolvimento macOS e iOS'];
+// const mediaSalarial6 = 6021;
 
-const linguagem7 = 'Java';
-const ranking7 = 7;
-const indicadoPara7 = 'Iniciantes e Intermediários';
-const dificuldade7 = 'Moderado';
-const facilDeAprender7 = true;
-const usos7 = ['Desenvolvimento mobile', 'Aplicações Desktop', 'Aplicações Web', 'Desenvolvimento de Jogos', 'Machine Learning', 'Computação Científica'];
-const mediaSalarial7 = 6000;
+// const linguagem7 = 'Java';
+// const ranking7 = 7;
+// const indicadoPara7 = 'Iniciantes e Intermediários';
+// const dificuldade7 = 'Moderado';
+// const facilDeAprender7 = true;
+// const usos7 = ['Desenvolvimento mobile', 'Aplicações Desktop', 'Aplicações Web', 'Desenvolvimento de Jogos', 'Machine Learning', 'Computação Científica'];
+// const mediaSalarial7 = 6000;
 
-const linguagem8 = 'Go';
-const ranking8 = 8;
-const indicadoPara8 = 'Iniciantes';
-const dificuldade8 = 'Fácil';
-const facilDeAprender8 = true;
-const usos8 = ['Desenvolvimento web back-end', 'Aplicações Web', 'Big Data', 'Machine Learning'];
-const mediaSalarial8 = 4995;
+// const linguagem8 = 'Go';
+// const ranking8 = 8;
+// const indicadoPara8 = 'Iniciantes';
+// const dificuldade8 = 'Fácil';
+// const facilDeAprender8 = true;
+// const usos8 = ['Desenvolvimento web back-end', 'Aplicações Web', 'Big Data', 'Machine Learning'];
+// const mediaSalarial8 = 4995;
 
-const linguagem9 = 'SQL';
-const ranking9 = 9;
-const indicadoPara9 = 'Iniciantes';
-const dificuldade9 = 'Fácil';
-const facilDeAprender9 = true;
-const usos9 = ['Ciência de Dados', 'Gerenciamento de Banco de Dados', 'Business Intelligence'];
-const mediaSalarial9 = 5763;
+// const linguagem9 = 'SQL';
+// const ranking9 = 9;
+// const indicadoPara9 = 'Iniciantes';
+// const dificuldade9 = 'Fácil';
+// const facilDeAprender9 = true;
+// const usos9 = ['Ciência de Dados', 'Gerenciamento de Banco de Dados', 'Business Intelligence'];
+// const mediaSalarial9 = 5763;
 
-const linguagem10 = 'Ruby';
-const ranking10 = 10;
-const indicadoPara10 = 'Iniciantes';
-const dificuldade10 = 'Fácil';
-const facilDeAprender10 = true;
-const usos10 = ['Desenvolvimento web front-end', 'Desenvolvimento web back-end'];
-const mediaSalarial10 = 5560;
+// const linguagem10 = 'Ruby';
+// const ranking10 = 10;
+// const indicadoPara10 = 'Iniciantes';
+// const dificuldade10 = 'Fácil';
+// const facilDeAprender10 = true;
+// const usos10 = ['Desenvolvimento web front-end', 'Desenvolvimento web back-end'];
+// const mediaSalarial10 = 5560;
 
-const mediaFinal = (mediaSalarial1 + mediaSalarial2 + mediaSalarial3 + mediaSalarial4 + mediaSalarial5 + mediaSalarial6 + mediaSalarial7 + mediaSalarial8 + mediaSalarial9 + mediaSalarial10) / 10;
-console.log('Média salarial somando todas as linguagens listadas:', mediaFinal);
+// const mediaFinal = (mediaSalarial1 + mediaSalarial2 + mediaSalarial3 + mediaSalarial4 + mediaSalarial5 + mediaSalarial6 + mediaSalarial7 + mediaSalarial8 + mediaSalarial9 + mediaSalarial10) / 10;
+// console.log('Média salarial somando todas as linguagens listadas:', mediaFinal);
 
-const facilDeAprender = facilDeAprender1 && facilDeAprender2 && facilDeAprender3 && facilDeAprender4 && facilDeAprender5 && facilDeAprender6 && facilDeAprender7 && facilDeAprender8 && facilDeAprender9 && facilDeAprender10;
-console.log(facilDeAprender);
+// const facilDeAprender = facilDeAprender1 && facilDeAprender2 && facilDeAprender3 && facilDeAprender4 && facilDeAprender5 && facilDeAprender6 && facilDeAprender7 && facilDeAprender8 && facilDeAprender9 && facilDeAprender10;
+// console.log(facilDeAprender);
 
+const linguagem1 = {
+    nome: 'Python',
+    indicadoPara: 'Iniciantes',
+    dificuldade: 'Fácil',
+    facilDeAprender: true,
+    usos: ['Desenvolvimento web back-end', 'Aplicações Desktop', 'Ciência de Dados', 'Automação', 'Machine Learning'],
+    mediaSalarial: 5469
+}
+
+const linguagem2 = {
+    nome: 'C#',
+    indicadoPara: 'Iniciantes e Intermediários',
+    dificuldade: 'Moderada',
+    facilDeAprender: true,
+    usos: ['Aplicações web', 'Aplicações móveis', 'Desenvolvimento de jogos', 'Realidade virtual', 'Aplicações Linux e Mac'],
+    mediaSalarial: 4624
+}
+
+const linguagem3 = {
+    nome: 'C++',
+    indicadoPara: 'Programadores Experientes',
+    dificuldade: 'Difícil',
+    facilDeAprender: false,
+    usos: ['Desenvolvimento mobile', 'Desenvolvimento de videogames', 'Machine Learning', 'Aplicações Server-side e Client-side', 'Sistemas Operacionais'],
+    mediaSalarial: 7156
+}
+
+const linguagem4 = {
+    nome: 'JavaScript',
+    indicadoPara: 'Iniciantes',
+    dificuldade: 'Relativamente Fácil',
+    facilDeAprender: true,
+    usos: ['Desenvolvimento web front-end', 'Desenvolvimento de jogos', 'Aplicações web'],
+    mediaSalarial: 4814
+}
+
+const linguagem5 = {
+    nome: 'PHP',
+    indicadoPara: 'Iniciantes',
+    dificuldade: 'Bastante Fácil',
+    facilDeAprender: true,
+    usos: ['Desenvolvimento web', 'Aplicações Desktop', 'Programas de Computador'],
+    mediaSalarial: 5082
+}
+
+const linguagem6 = {
+    nome: 'Swift',
+    indicadoPara: 'Iniciantes',
+    dificuldade: 'Fácil',
+    facilDeAprender: true,
+    usos: ['Desenvolvimento de Software', 'Desenvolvimento macOS e iOS'],
+    mediaSalarial: 6021
+}
+
+const linguagem7 = {
+    nome: 'Java',
+    indicadoPara: 'Iniciantes e Intermediários',
+    dificuldade: 'Moderado',
+    facilDeAprender: true,
+    usos: ['Desenvolvimento mobile', 'Aplicações Desktop', 'Aplicações Web', 'Desenvolvimento de Jogos', 'Machine Learning', 'Computação Científica'],
+    mediaSalarial: 6000
+}
+
+const linguagem8 = {
+    nome: 'Go',
+    indicadoPara: 'Iniciantes',
+    dificuldade: 'Fácil',
+    facilDeAprender: true,
+    usos: ['Desenvolvimento web back-end', 'Aplicações Web', 'Big Data', 'Machine Learning'],
+    mediaSalarial: 4995
+}
+
+const linguagem9 = {
+    nome: 'SQL',
+    indicadoPara: 'Iniciantes',
+    dificuldade: 'Fácil',
+    facilDeAprender: true,
+    usos: ['Ciência de Dados', 'Gerenciamento de Banco de Dados', 'Business Intelligence'],
+    mediaSalarial: 5763
+}
+
+const linguagem10 = {
+    nome: 'Ruby',
+    indicadoPara: 'Iniciantes',
+    dificuldade: 'Fácil',
+    facilDeAprender: true,
+    usos: ['Desenvolvimento web front-end', 'Desenvolvimento web back-end'],
+    mediaSalarial: 5560
+}
+
+let linguagens = [];
+
+linguagens.push(linguagem1, linguagem2, linguagem3, linguagem4, linguagem5, linguagem6, linguagem7, linguagem8, linguagem9, linguagem10);
+
+console.log(linguagens);
