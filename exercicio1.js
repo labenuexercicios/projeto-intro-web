2dd196a3901b6d3fe0b53a9b26e00e088d4ae2ce
MilenaCruzJusta <milenajusta86@gmail.com>
2022-08-09 09:33:04 -0300

    ex02

--- exercicio1.js
+++ exercicio1.js
@@ -1,32 +1,30 @@
+const unidadeSaude = {
+    paciente1 : {
+    nome : "M.B,S",
+    prontuario : 1001,
+    emDesintox : true,
+    pts : ["auriculoterapia", "grupo de meditação", "psicologia"],
+},
 
+    paciente2 : {
+    nome : "F.R.T",
+    prontuario : 1002,
+    emDesintox : true,
+    pts : ["avaliação psiquiátrica", "terapia ocupacional", "grupo criarte"],
+},
 
-
-
-const UnidadeSaude={
-    paciente1 = {
-    nome = "M.B,S",
-    prontuario = 1001,
-    emDesintox = true,
-    pts = ["auriculoterapia", "grupo de meditação", "psicologia"],
-    },
-
-    paciente2 ={
-    nome = "F.R.T",
-    prontuario = 1002,
-    emDesintox = true,
-    pts = ["avaliação psiquiátrica", "terapia ocupacional", "grupo criarte"],
-    },
-
-    paciente3 = {
-    nome = "J.R.C",
-    prontuario = 1003,
-    emDesintox = false,
-    pts = ["avaliação clínica", "auriculoterapia", "grupo cabeça feita"],
-    }
-
+    paciente3 : {
+    nome : "J.R.C",
+    prontuario : 1003,
+    emDesintox : false,
+    pts : ["avaliação clínica", "auriculoterapia", "grupo cabeça feita"],
+}
 
 }
 
-console.log(paciente1.nome.toUpperCase())
-console.log(paciente2.nome.toUpperCase())
+
+console.log(paciente1)
+
+console.log(paciente2.nome)
+
 console.log(paciente3.nome.toUpperCase())
