2408f570b3e8f99ff6674f1059d1b512a0139937
MilenaCruzJusta <milenajusta86@gmail.com>
2022-08-09 05:50:36 -0300

    indexando

---
+++ exercicio1.js
@@ -0,0 +1,32 @@
+
+
+
+
+const UnidadeSaude={
+    paciente1 = {
+    nome = "M.B,S",
+    prontuario = 1001,
+    emDesintox = true,
+    pts = ["auriculoterapia", "grupo de meditação", "psicologia"],
+    },
+
+    paciente2 ={
+    nome = "F.R.T",
+    prontuario = 1002,
+    emDesintox = true,
+    pts = ["avaliação psiquiátrica", "terapia ocupacional", "grupo criarte"],
+    },
+
+    paciente3 = {
+    nome = "J.R.C",
+    prontuario = 1003,
+    emDesintox = false,
+    pts = ["avaliação clínica", "auriculoterapia", "grupo cabeça feita"],
+    }
+
+
+}
+
+console.log(paciente1.nome.toUpperCase())
+console.log(paciente2.nome.toUpperCase())
+console.log(paciente3.nome.toUpperCase())
