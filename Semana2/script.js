c2a2e311a984dddf5342a7583daa3fb719946910
Joanadayse <daysejoana3@gmail.com>
2022-08-12 15:33:02 -0300

    semana2

---
+++ Semana2/script.js
@@ -0,0 +1,32 @@
+const planeta1= "mercurio";
+const peridoDeOrbita1=88;
+const luas1=false;
+const composicaoQuimica1 = ["Hélio" ,"Hidrogênio"]
+
+console.log(`${planeta1.toUpperCase()}\n ${peridoDeOrbita1}: \n ${luas1}: \n ${composicaoQuimica1}`)
+    
+
+
+const planeta2="venus";
+const peridoDeOrbita2=225;
+const luas2=false;
+const composicaoQuimica2 = ["helio" ,"sodio" , "oxigenio"]
+
+console.log(`${planeta2.toUpperCase()}\n ${peridoDeOrbita2}: \n ${luas2}: \n ${composicaoQuimica2}`)
+
+
+const planeta3 ="terra";
+const peridoDeOrbita3= 365;
+const luas3 = true;
+const composicaoQuimica3=["ferro" , "oxigenio" , "silicio"]
+
+console.log(`${planeta3.toUpperCase()}\n ${peridoDeOrbita3}: \n ${luas1}: \n ${composicaoQuimica3}`)
+
+
+
+const planeta4 = "marte";
+const peridoDeOrbita4= 687;
+const luas4 = true;
+const composicaoQuimica4 =["Nitrogênio" ,"Oxigênio", "DióxidoDeCarbo"]
+
+console.log(`${planeta4.toUpperCase()}\n ${peridoDeOrbita4}: \n ${luas1}: \n ${composicaoQuimica4}`)
