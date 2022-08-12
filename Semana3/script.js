f7f1d25092983285b1a1470aa255356d6d915b2b
Joanadayse <daysejoana3@gmail.com>
2022-08-12 15:38:43 -0300

    semana3

---
+++ Semana3/script.js
@@ -0,0 +1,33 @@
+const planetas = []
+
+const planeta =[ {
+
+    nome: "mercurio",
+    peridoDeOrbital:88,
+    luas:false,
+    composicaoQuimica : ["Hélio" ,"Hidrogênio"]
+    },
+     
+    {
+    
+    nome:"venus",
+    peridoDeOrbital:225,
+    luas:false,
+    composicaoQuimica:["helio" ,"sodio" , "oxigenio"]},
+    
+    
+    {
+    nome :"terra",
+    peridoDeOrbita: 365,
+    luas : true,
+    composicaoQuimica :["ferro" , "oxigenio" , "silicio"]},
+    
+    {
+    nome : "marte",
+    peridoDeOrbita: 687,
+    luas: true,
+    composicaoQuimica :["Nitrogênio" ,"Oxigênio", "DióxidoDeCarbo"] },
+    ]
+
+    planetas.push(planeta)
+    console.log(planetas)
