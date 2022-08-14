3d3a2bd94e455ec6c34d8f5fd91ce6b9bb7c0d1b
DavidAlves28 <dav.alvesc28@gmail.com>
2022-08-14 17:56:28 -0300

    semana1-2-3-4

---
+++ script.js
@@ -0,0 +1,97 @@
+// Semana 1 e 3 // SEMANA 3 1. Transforme os itens que criamos nas últimas semanas em objetos.
+
+const animal1 = {
+    nome: 'Rinoceronte-Branco',
+    nomeCientifico: 'Ceratotherium-simum',
+    tempoVida: [40, 50],
+    clase: 'Mamíferos',
+    alturaAnimal: 180,
+    pesoAnimal: 2500,
+    isExtinto: true,
+    localHabita: 'África do Sul, Botsuana, Quênia e Zâmbia, entre outro,',
+    descrisaoAnimal: 'O rinoceronte-branco (Ceratotherium simum) habita na África do Sul, Botsuana, Quênia e Zâmbia, entre outros. Tem duas subespécies, o rinoceronte-branco-do-sul e o rinoceronte-branco-do-norte, extinto na natureza desde 2018. Mesmo assim, ainda há dois indivíduos do sexo feminino em cativeiro. É particularmente grande, pois um indivíduo adulto masculino pode ultrapassar 180 cm de altura e 2.500 kg de peso.É um animal herbívoro que vive na savana e no campo. Quando em uma corrida, pode atingir até 50 km/h. É também um animal gregário, vivendo em comunidades de 10 a 20 indivíduos, que atingem a maturidade sexual tardiamente, por volta dos 7 anos de idade. Segundo a IUCN, é considerada uma espécie quase ameaçada de extinção, dado que existe um interesse internacional pelas espécies para a caça e a fabricação de artesanato e jóias',
+}
+
+const animal2 = {
+    nome: 'Hipopótamo',
+    nomeCientifico: 'Hippopotamus anfíbio',
+    tempoVida: [40, 50],
+    clase: 'Mamíferos',
+    alturaAnimal: 500,
+    pesoAnimal: 1500,
+    isExtinto: false,
+    descrisaoAnimal: 'O hipopótamo-comum (Hippopotamus anfíbio) é o terceiro maior animal terrestre do mundo. Pode pesar entre 1.300 e 1.500 kg e pode atingir velocidades de até 30 km/h. Vive em rios, manguezais e lagos, onde se refresca durante as horas mais quentes do dia. O hipopótamo-comum pode ser encontrado do Egito a Moçambique, embora existam quatro outras espécies que, juntas, povoam um grande número de países africanos.Eles são animais particularmente agressivos, em relação a outros animais e outros da mesma espécie. Precisamente por esta razão, muitas pessoas se perguntam por que os hipopótamos atacam. Eles são vulneráveis no quesito risco de extinção, de acordo com a IUCN, principalmente devido à venda internacional de suas presas de marfim e ao consumo de sua carne pela população local',
+}
+const animal3 = {
+    nome: 'Crocodilo',
+    nomeCientifico: 'Crocodylus suchus',
+    tempoVida: [50, 80],
+    clase: 'Répteis',
+    tamanhoAnimal: 600,
+    pesoAnimal: 1500,
+    isExtinto: false,
+    descrisaoAnimal: 'Existem três espécies de crocodilos que habitam as áreas de florestas da África: o crocodilo-do-oeste-africano (Crocodylus suchus), o crocodilo-de-focinho-delgado (Mecistops cataphractus) e o crocodilo-do-nilo (Crocodylus niloticus). Estamos falando de grandes répteis que habitam vários tipos de rios, lagoas e pântanos. Podem ultrapassar 6 metros de comprimento e 1.500 quilos.Dependendo da espécie, estes animais da África também podem viver em água salgada. A dieta dos crocodilos é baseada no consumo de vertebrados e invertebrados, embora possa variar de acordo com a espécie. Eles têm uma pele dura, cheia de escamas, e sua expectativa de vida pode exceder 80 anos. É importante conhecer as diferenças entre crocodilos e jacarés, para não confundi-los. Algumas espécies, como o crocodilo-de-focinho-delgado, estão criticamente ameaçadas de extinção.',
+}
+const animal4 = {
+    nome: 'Leão',
+    nomeCientifico: 'Panthera leo',
+    tempoVida: [8, 10],
+    clase: 'Mamíferos',
+    tamanhoAnimal: 125,
+    pesoAnimal: 260,
+    isExtinto: false,
+    descrisaoAnimal: 'O leão (Panthera leo) é o animal com o qual fechamos a lista dos cinco grandes da África. Este superpredador é o único com dimorfismo sexual, o que nos permite diferenciar os machos, com sua crina densa, das fêmeas, que carecem dela. É considerado o maior felino da África e o segundo maior do mundo, somente atrás do tigre. Os machos podem chegar a 260 kg de peso, enquanto as fêmeas pesam no máximo 180 kg. A altura até a cernelha está entre 100 e 125 cm.As fêmeas são as encarregadas da caça, para isso, elas coordenam e perseguem a presa escolhida, podendo atingir até 59 km/h em acelerações rápidas. Estes animais africanos podem alimentar-se de zebras, gnus, javalis ou qualquer outro animal. Um detalhe que poucas pessoas sabem é que o leão e as hienas são rivais que lutam entre si pela caça, e embora geralmente se pense que a hiena é um animal necrófago, a verdade é que é o leão que muitas vezes age como um animal oportunista roubando comida das hienas.O leão é considerado em estado vunerável de acordo com a UICN, pois sua população diminui anualmente, existindo atualmente um total de 23 mil a 39 mil espécimes adulto',
+}
+const animal5 = {
+    nome: 'Javali-africano',
+    nomeCientifico: ' Phacochoerus africanus',
+    tempoVida: [20],
+    clase: 'Mamíferos',
+    tamanhoAnimal: 150,
+    pesoAnimal: 150,
+    isExtinto: false,
+    descrisaoAnimal: 'O facocero, também conhecido como javali-africano, apesar de não ser de fato um javali, é o nome que se refere aos animais do gênero Phacochoerus, que inclui duas espécies africanas, o Phacochoerus africanus e o Phacochoerus aethiopicus. Eles habitam savanas e áreas semi-desérticas, onde se alimentam de todo tipo de frutas e vegetais, embora sua dieta também inclua ovos, aves e carniça. Portanto, eles são animais onívoros.Estes animais africanos também são sociáveis, pois compartilham áreas de descanso, alimentação ou banho com outras espécies. Além disso, estamos falando de um gênero de animais inteligentes, que aproveitam os ninhos de outros animais, tais como os porco-formigueiro (Orycteropus afer) para se refugiarem dos predadores enquanto dormem. Assim como os gnus, os javalis são considerados espécies de menor preocupação pela IUCN por não correrem risco de extinção.',
+}
+const animal6 = {
+    nome: 'Dodô',
+    nomeCientifico: 'Raphus cucullatus)',
+    tempoVida: [21],
+    clase: 'Aves',
+    tamanhoAnimal: 100,
+    pesoAnimal: 20,
+    isExtinto: true,
+    descrisaoAnimal: 'é a mais famosa ave extinta pelo homem. Nativo das Ilhas Maurício, esses animais sofreram com as frequentes navegações dos anos 1500. Nessa época, a ilha foi invadida por colonos que trouxeram animais exóticos como porcos, ratos e principalmente macacos que destruíam os ninhos do Dodô. Além disso, as aves eram muito caçadas para consumo dos próprios navegantes enquanto estavam na ilha, pois não tinham medo de humanos e eram de fácil captura, fazendo com que a ave desaparecesse completamente em meados do século XVII. Pertencente à subfamília Raphinae, os Dodôs possuíam as asas atrofiadas e não eram capazes de voar. Mediam cerca de 1 m de altura com aproximadamente 20 Kg. Suas penas variavam entre as cores branca, cinza e preto. Possuía o bico longo e curvado na ponta, adaptado para sua alimentação que era composta principalmente de peixes, sementes e frutas.',
+}
+
+// 3. Faça um cálculo de média, entre os valores numéricos respectivos de cada item. Imprima o valor da média utilizando um console.log. Guarde este valor em uma const;
+const mediaPeso = (animal1.pesoAnimal + animal2.pesoAnimal + animal3.pesoAnimal + animal4.pesoAnimal + animal5.pesoAnimal + animal6.pesoAnimal) / 6
+//  console.log(mediaPeso);
+
+//  4. Com um console.log, imprima o resultado de uma operação lógica que checa se todos os valores de variáveis booleanas criadas até aqui são verdadeiras.
+const verificaExtincao = animal1.isExtinto && animal2.isExtinto && animal3.isExtinto && animal4.isExtinto && animal5.isExtinto && animal6.isExtinto
+// console.log(verificaExtincao);
+
+// semana 2 arrays 1. Crie pelo menos mais uma característica para o item que você criou. Esta característica deve ser um array. Mantenha o tipo de dado do array criado sempre o mesmo, isto é, se é um array de strings, só deve receber strings.
+const animais = [animal1.nome.toUpperCase(), animal2.nome.toUpperCase(), animal3.nome.toUpperCase(), animal4.nome.toUpperCase(), animal5.nome.toUpperCase(), animal6.nome.toUpperCase()]
+const nomesCientifico = [animal1.nomeCientifico.toLocaleUpperCase(), animal2.nomeCientifico.toLocaleUpperCase(), animal3.nomeCientifico.toLocaleUpperCase(), animal4.nomeCientifico.toLocaleUpperCase(), animal5.nomeCientifico.toLocaleUpperCase(), animal6.nomeCientifico.toLocaleUpperCase()]
+// console.log(animais,nomesCientifico);
+
+// SEMANA 3 2. Crie um array para guardar os objetos. Este array deve estar vazio, por enquanto; 3. Adicione os objetos criados no item 1 ao array de objetos criado no item 2, utilizando o push()
+const animaisAfrica = [];
+animaisAfrica.push(animal1, animal2, animal3, animal4, animal5, animal6)
+// console.log(animaisAfrica);
+
+// SEMANA 4 1. Altere o item “Adicione os novos objetos no array de objetos, utilizando o push()” (item 3 da semana 3), para criar uma verificação antes de dar o push. A caraterística booleana do objeto deve ser validada. Isto é, o objeto só deve ser adicionado ao array se a propriedade booleana for true;
+// 2. Crie uma condição else, que, em caso de valor false na condição acima, exiba um **ALERT** avisando para o usuário que o item não foi adicionado, e não faça o push
+const animaisExtintos = [];
+if (animaisAfrica) {
+    if (animal1.isExtinto) { animaisExtintos.push(animal1), alert(`Animal:${animal1.nome}, é extinto`) } else { alert(`Animal: ${animal1.nome}, não é extinto`) }
+    if (animal2.isExtinto) { animaisExtintos.push(animal2), alert(`Animal:${animal2.nome}, é extinto`) } else { alert(`Animal: ${animal2.nome}, não é extinto`) }
+    if (animal3.isExtinto) { animaisExtintos.push(animal3), alert(`Animal:${animal3.nome}, é extinto`) } else { alert(`Animal: ${animal3.nome}, não é extinto`) }
+    if (animal4.isExtinto) { animaisExtintos.push(animal4), alert(`Animal:${animal4.nome}, é extinto`) } else { alert(`Animal: ${animal4.nome}, não é extinto`) }
+    if (animal5.isExtinto) { animaisExtintos.push(animal5), alert(`Animal:${animal5.nome}, é extinto`) } else { alert(`Animal: ${animal5.nome}, não é extinto`) }
+    if (animal6.isExtinto) { animaisExtintos.push(animal6), alert(`Animal:${animal6.nome}, é extinto`) } else { alert(`Animal: ${animal6.nome}, não é extinto`) }
+}
+// console.log(animaisExtintos);
+// SEMANA . Reescrevendo o relatório da semana 2, altere a forma que a característica de array, criada no item 1 da semana 2, faça um laço que guarde todos os valores da propriedade array do objeto em uma mesma string. Utilize esta string no relatório2. Ainda no relatório, altere-o para que ele seja criado utilizando um laço. Ou seja, você não deve mais imprimir individualmente cada item do relatório. Cada item deve ser exibido a partir de uma iteração do laço. Para testar, adicione mais um item ao array de objetos, e verifique se ele é exibido corretamente.
+
