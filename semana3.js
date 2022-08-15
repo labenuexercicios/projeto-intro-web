21a49c02be5197b1a7d3ffc6e47e0d0637154edb
MilenaCruzJusta <milenajusta86@gmail.com>
2022-08-15 16:41:54 -0300

    fimSem3

--- semana3.js
+++ semana3.js
@@ -1,62 +1,31 @@
-/* . Transforme os itens que criamos nas últimas semanas em objetos.
-Exemplo
-//ANTES
-  const nome1 = "Fulano";
-  const idade1 = 33;
-  const verificouEmail1 = false; 
-//DEPOIS
-objeto1 = { 
-    nome: "Fulano", 
-    idade: 33,
-    verificouEmail: false
-  } 
-2. Crie um array para guardar os objetos. Este array deve estar vazio, por enquanto;
-3. Adicione os objetos criados no item 1 ao array de objetos criado no item 2, utilizando o push()
-Ao fim, você deve ter algo parecido com isso:
-[
-  { 
-    nome: "Fulano", 
-    idade: 33,
-    verificouEmail: false
-  },
-  { 
-    nome: "Beltrano", 
-    idade: 41,
-    verificouEmail: true
-  },
-  { 
-    nome: "Fulano", 
-    idade: 27,
-    verificouEmail: true
-  },
- */
-
-  
-   paciente1 = {
-   nome : "M.B,S",
-   prontuario : 1001,
-   emDesintox : true,
-   pts : ["auriculoterapia", "grupo de meditação", "psicologia"],
+  paciente1 = {
+  nome : "M.B.S",
+  prontuario : 1001,
+  emDesintox : true,
+  pts : ["auriculoterapia", "grupo de meditação", "psicologia"],
 },
 
-   paciente2 = {
-   nome : "F.R.T",
-   prontuario : 1002,
-   emDesintox : true,
-   pts : ["avaliação psiquiátrica", "terapia ocupacional", "grupo criarte"],
+  paciente2 = {
+  nome : "F.R.T",
+  prontuario : 1002,
+  emDesintox : true,
+  pts : ["avaliação psiquiátrica", "terapia ocupacional", "grupo criarte"],
 },
 
-   paciente3 = {
-   nome : "J.R.C",
-   prontuario : 1003,
-   emDesintox : false,
-   pts : ["avaliação clínica", "auriculoterapia", "grupo cabeça feita"],
+  paciente3 = {
+  nome : "J.R.C",
+  prontuario : 1003,
+  emDesintox : false,
+  pts : ["avaliação clínica", "auriculoterapia", "grupo cabeça feita"],
 }
 
+const arrayUnidSaude =[]
 
-const arrayDeObjetos = []
-   
-   arrayDeObjetos.push(paciente1, paciente2, paciente3)
+const arrayCAPS = [arrayUnidSaude.push(paciente1), arrayUnidSaude.push(paciente2), arrayUnidSaude.push(paciente3)]
+console.log(arrayCAPS)
 
-   console.log(arrayDeObjetos)
-   
+   console.log(paciente1.nome.toUpperCase())
+   console.log(paciente2.nome.toUpperCase())
+   console.log(paciente3.nome.toUpperCase())
+   console.log(arrayUnidSaude.nome.toUpperCase())
+   console.log(arrayCAPS.nome.toUpperCase())
