cfcde1c022a3dad686c2564c9f199c7c9fbff667
marcusvrr <marcusvrr2@gmail.com>
2022-08-20 19:50:49 -0300

    Requisições da semana 5 concluidas

--- script.js
+++ script.js
@@ -63,80 +63,89 @@ const personagem8 = {
 };
 
 /*Faça um cálculo de média, entre os valores numéricos respectivos de cada item. Imprima o valor da média utilizando um console.log.
-Guarde este valor em uma const
-const somaAltura = (altura1 + altura2 + altura3 + altura4 + altura5 + altura6 + altura7 + altura8) / 8;
+Guarde este valor em uma const*/
+const somaAltura = (personagem1.altura + personagem2.altura + personagem3.altura + personagem4.altura + personagem5.altura + personagem6.altura + personagem7.altura + personagem8.altura) / 8;
 console.log(somaAltura)
 
-Com um console.log, imprima o resultado de uma operação lógica que checa se todos os valores de variáveis booleanas
-criadas até aqui são verdadeiras.
+/*Com um console.log, imprima o resultado de uma operação lógica que checa se todos os valores de variáveis booleanas
+criadas até aqui são verdadeiras.*/
 
-const verifSeBruxo = bruxo1 && bruxo2 && bruxo3 && bruxo4 && bruxo5 && bruxo6 && bruxo7 && bruxo8;
+const verifSeBruxo = personagem1.bruxo && personagem2.bruxo && personagem3.bruxo && personagem4.bruxo && personagem5.bruxo && personagem6.bruxo && personagem7.bruxo && personagem8.bruxo;
 console.log(verifSeBruxo); //true pq todas as const são verdadeiras.
 
 //Semana 2
 /*1. Crie pelo menos mais uma característica para o item que você criou. Esta característica deve ser um array. Mantenha o tipo de dado do array criado sempre o mesmo, isto é, se é um array de strings, só deve receber strings.
 2. Exiba um relatório utilizando console.log(), mostrando todos os dados de cada um dos itens criados até aqui
   1. O log deve exibir o dado de nome, título ou afim sempre em LETRAS MAIÚSCULAS, como no exemplo abaixo.
-const relatorio1 = `${perso1.toUpperCase()},\n${altura1},\n${casa1},\n${bruxo1},\n${amigos1}`;
+const relatorio1 = `${bruxoHp1.toUpperCase()},\n${altura1},\n${casa1},\n${bruxo1},\n${amigos1}`;
 console.log(relatorio1);
-const relatorio2 = `${perso2.toUpperCase()},\n${altura2},\n${casa2},\n${bruxo2},\n${amigos2}`;
+const relatorio2 = `${bruxoHp2.toUpperCase()},\n${altura2},\n${casa2},\n${bruxo2},\n${amigos2}`;
 console.log(relatorio2);
-const relatorio3 = `${perso3.toUpperCase()},\n${altura3},\n${casa3},\n${bruxo3},\n${amigos3}`;
+const relatorio3 = `${bruxoHp3.toUpperCase()},\n${altura3},\n${casa3},\n${bruxo3},\n${amigos3}`;
 console.log(relatorio3);
-const relatorio4 = `${perso4.toUpperCase()},\n${altura4},\n${casa4},\n${bruxo4},\n${amigos4}`;
+const relatorio4 = `${bruxoHp4.toUpperCase()},\n${altura4},\n${casa4},\n${bruxo4},\n${amigos4}`;
 console.log(relatorio4);
-const relatorio5 = `${perso5.toUpperCase()},\n${altura5},\n${casa5},\n${bruxo5},\n${amigos5}`;
+const relatorio5 = `${bruxoHp5.toUpperCase()},\n${altura5},\n${casa5},\n${bruxo5},\n${amigos5}`;
 console.log(relatorio5);
-const relatorio6 = `${perso6.toUpperCase()},\n${altura6},\n${casa6},\n${bruxo6},\n${amigos6}`;
+const relatorio6 = `${bruxoHp6.toUpperCase()},\n${altura6},\n${casa6},\n${bruxo6},\n${amigos6}`;
 console.log(relatorio6);
-const relatorio7 = `${perso7.toUpperCase()},\n${altura7},\n${casa7},\n${bruxo7},\n${amigos7}`;
+const relatorio7 = `${bruxoHp7.toUpperCase()},\n${altura7},\n${casa7},\n${bruxo7},\n${amigos7}`;
 console.log(relatorio7);
-const relatorio8 = `${perso8.toUpperCase()},\n${altura8},\n${casa8},\n${bruxo8},\n${amigos8}`;
+const relatorio8 = `${bruxoHp8.toUpperCase()},\n${altura8},\n${casa8},\n${bruxo8},\n${amigos8}`;
 console.log(relatorio8);*/
 
 //semana 3 transformar as constantes em objetos, crir um array e colocar os objetos por push:
 
-const bruxosHP = [];
+const bruxosHp = [];
 if(personagem1.bruxo){
-  bruxosHP.push(personagem1);
+  bruxosHp.push(personagem1);
 }else{
   alert("Personagem não adicionado pois não é bruxo")
 }
 
 if(personagem2.bruxo){
-  bruxosHP.push(personagem2);
+  bruxosHp.push(personagem2);
 }else{
   alert("Personagem não adicionado pois não é bruxo")
 };
 
 if(personagem3.bruxo){
-  bruxosHP.push(personagem3);
+  bruxosHp.push(personagem3);
 }else{
   alert("Personagem não adicionado pois não é bruxo")
 };
 if(personagem4.bruxo){
-  bruxosHP.push(personagem4);
+  bruxosHp.push(personagem4);
 }else{
   alert("Personagem não adicionado pois não é bruxo")
 };
 if(personagem5.bruxo){
-  bruxosHP.push(personagem5);
+  bruxosHp.push(personagem5);
 }else{
   alert("Personagem não adicionado pois não é bruxo")
 };
 if(personagem6.bruxo){
-  bruxosHP.push(personagem6);
+  bruxosHp.push(personagem6);
 }else{
   alert("Personagem não adicionado pois não é bruxo")
 };
 if(personagem7.bruxo){
-  bruxosHP.push(personagem7);
+  bruxosHp.push(personagem7);
 }else{
   alert("Personagem não adicionado pois não é bruxo")
 };
 if(personagem8.bruxo){
-  bruxosHP.push(personagem8);
+  bruxosHp.push(personagem8);
 }else{
   alert("Personagem não adicionado pois não é bruxo")
 };
-console.log(bruxosHP)
+console.log(bruxosHp)
+
+let relatorio = []
+
+for(let i = 0; i < bruxosHp.length; i++){
+  let temporaria = bruxosHp[i].amigos.join();
+  relatorio.push(temporaria);
+  console.log(`${bruxosHp[i].nome.toUpperCase()}`,`\n${bruxosHp[i].altura}, \n${bruxosHp[i].casa}, \n${bruxosHp[i].bruxo}; \n${relatorio[i]}`);;
+}
+console.log(relatorio)
