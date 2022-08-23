d89fdddfa304ff1c1b12a925a97e45a303bc92d8
Everton Mello <everton.mello.281@gmail.com>
2022-08-23 17:46:16 -0300

    Semana 5 ok

--- script.js
+++ script.js
@@ -18,6 +18,28 @@ const kakashi = {
     aindaVivo: true,
     transformcaoDeNatureza: ["Água","Fogo","Raio(afinidade)","Terra","Vento","Yang","Yin","Gelo"]
 }
+const itachi = {
+    nome: "Itachi",
+    idade: 21,
+    aindaVivo: false,
+    transformcaoDeNatureza: ["Água","Fogo","Vento","Yang","Yin"]
+}
+let strNaruto = ""
+let strMinato = ""
+let strKakashi = ""
+let strItachi = ""
+for(str of naruto.transformcaoDeNatureza) {
+    strNaruto += str+", "
+}
+for(str of minato.transformcaoDeNatureza) {
+    strMinato += str+", "
+}
+for(str of kakashi.transformcaoDeNatureza) {
+    strKakashi += str+", "
+}
+for(str of itachi.transformcaoDeNatureza) {
+    strItachi += str+", "
+}
 
 const arrayPersonagens = [];
 
@@ -36,15 +58,28 @@ if(kakashi.aindaVivo){
 }else{
     alert("O Objeto Kakashi não foi adicionado ao array")
 }
+if(itachi.aindaVivo){
+    arrayPersonagens.push(itachi);
+}else{
+    alert("O Objeto Itachi não foi adicionado ao array")
+}
 
-const mediaIdade = (naruto.idade + minato.idade + kakashi.idade) / 3;
+const mediaIdade = (naruto.idade + minato.idade + kakashi.idade + itachi.idade) / 4;
 console.log("A média das idades dos personagens é: ", mediaIdade);
 
-const todosVivos = naruto.aindaVivo && minato.aindaVivo && kakashi.aindaVivo;
+const todosVivos = naruto.aindaVivo && minato.aindaVivo && kakashi.aindaVivo && itachi.aindaVivo;
 console.log("Todos personagens ainda estão vivos? ", todosVivos);
 
-console.log(" Nome:",naruto.nome.toUpperCase(),'\n','Idade:',naruto.idade,'anos','\n','Ainda está vivo?',naruto.aindaVivo,'\n','Transformações de Natureza:',naruto.transformcaoDeNatureza);
+for(let i in arrayPersonagens){
+    let str = `Nome: ${arrayPersonagens[i].nome.toUpperCase()} \nIdade: ${arrayPersonagens[i].idade}\nAinda está vivo? ${arrayPersonagens[i].aindaVivo}\nTransformação da natureza: `
+    for(j of arrayPersonagens[i].transformcaoDeNatureza){
+        str += `${j},`
+    }
+    console.log(str);
+}
 
-console.log(" Nome:",minato.nome.toUpperCase(),'\n','Idade:',minato.idade,'anos','\n','Ainda está vivo?',minato.aindaVivo,'\n','Transformações de Natureza:',minato.transformcaoDeNatureza);
+// console.log(" Nome:",naruto.nome.toUpperCase(),'\n','Idade:',naruto.idade,'anos','\n','Ainda está vivo?',naruto.aindaVivo,'\n','Transformações de Natureza:',strNaruto);
 
-console.log(" Nome:",kakashi.nome.toUpperCase(),'\n','Idade:',kakashi.idade,'anos','\n','Ainda está vivo?',kakashi.aindaVivo,'\n','Transformações de Natureza:',kakashi.transformcaoDeNatureza);
+// console.log(" Nome:",minato.nome.toUpperCase(),'\n','Idade:',minato.idade,'anos','\n','Ainda está vivo?',minato.aindaVivo,'\n','Transformações de Natureza:',strMinato);
+
+// console.log(" Nome:",kakashi.nome.toUpperCase(),'\n','Idade:',kakashi.idade,'anos','\n','Ainda está vivo?',kakashi.aindaVivo,'\n','Transformações de Natureza:',strKakashi);
