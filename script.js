36539e33237312cd49e0e315a3cd8452ffa0f67b
Kieffer Torricilia <kieffer.torricilia@gmail.com>
2022-08-23 18:25:17 -0300

    Mudança dos filmes para Os Vingadores

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 //Filmes para ver antes de morrer
 let array = [
 {
-    nome: "TENET",
-    anoLancamento: 2020,
+    nome: "OS VINGADORES",
+    anoLancamento: 2012,
     valeAPena: true,
-    diretor: "Christopher Nolan",
-    roteiro: ["Chistopher Nolan"],
+    diretor: "Joss Whedon",
+    elenco: ["Robert Downey Jr.", "Scarlett Johansson", "Chris Evans", "Mark Rufallo", "Chris Hemsworth", "Jeremy Renner",
+"Tom Hiddleston", "Samuel L. Jackson"],
 },
 
 {
-    nome: "THE SUN SHINES BRIGHT",
-    anoLancamento: 1953,
+    nome: "VINGADORES: ERA DE ULTRON",
+    anoLancamento: 2015,
     valeAPena: true,
-    diretor: "John Ford",
-    roteiro: ["Frank Hotaling"],
+    diretor: "Joss Whedon",
+    elenco: ["Robert Downey Jr.", "Scarlett Johansson", "Chris Evans", "Mark Rufallo", "Chris Hemsworth", "Jeremy Renner",
+    "Tom Hiddleston", "Samuel L. Jackson", "Elizabeth Olsen", "James Spader", "Paul Bettany", ],
 },
 
 {
-    nome: "GREEN BOOK",
+    nome: "VINGADORES: GUERRA INFINITA",
     anoLancamento: 2018,
     valeAPena: true,
-    diretor: "Peter Farrely",
-    roteiro: ["Peter Farrely", "Nick Vallelonga", "Bryan Hayes Currie"],
+    diretor: "Anthony e Joe Russo",
+    elenco: ["Robert Downey Jr.", "Scarlett Johansson", "Chris Evans", "Mark Rufallo", "Chris Hemsworth", "Jeremy Renner",
+    "Tom Hiddleston", "Samuel L. Jackson", "Elizabeth Olsen", "Paul Bettany",],
 },
 
 {
-    nome: "WHIPLASH",
-    anoLancamento: 2014,
+    nome: "VINGADORES: ULTIMATO",
+    anoLancamento: 2019,
     valeAPena: true,
-    diretor: "Damien Chazelle",
-    roteiro: ["Damien Chazelle", "Olivia Hamil"]
+    diretor: "Anthony e Joe Russo",
+    elenco: ["Robert Downey Jr.", "Scarlett Johansson", "Chris Evans", "Mark Rufallo", "Chris Hemsworth", "Jeremy Renner",
+    "Tom Hiddleston", "Samuel L. Jackson", "Elizabeth Olsen", "Paul Bettany"]
 },
 ]
 
@@ -57,33 +61,33 @@ console.log(lista)
 
 let string1 = []
     
-for(i in array[0].roteiro){
-    if(i < array[0].roteiro.length - 1){
-        string1 += `${array[0].roteiro[i]}, `
+for(i in array[0].elenco){
+    if(i < array[0].elenco.length - 1){
+        string1 += `${array[0].elenco[i]}, `
     }else{
-        string1 += `${array[0].roteiro[i]}`
+        string1 += `${array[0].elenco[i]}`
     } 
 }
 
 let string2 = []
 
 
-for(i in array[1].roteiro){
-    if(i < array[1].roteiro.length - 1){
-        string2 += `${array[1].roteiro[i]}, `
+for(i in array[1].elenco){
+    if(i < array[1].elenco.length - 1){
+        string2 += `${array[1].elenco[i]}, `
     }else{
-        string2 += `${array[1].roteiro[i]}`
+        string2 += `${array[1].elenco[i]}`
     } 
 }
 
 
 let string3 = []
 
-for(i in array[2].roteiro){
-    if(i < array[2].roteiro.length - 1){
-        string3 += `${array[2].roteiro[i]}, `
+for(i in array[2].elenco){
+    if(i < array[2].elenco.length - 1){
+        string3 += `${array[2].elenco[i]}, `
     }else{
-        string3 += `${array[2].roteiro[i]}`
+        string3 += `${array[2].elenco[i]}`
     } 
 }
 
@@ -92,5 +96,5 @@ let roteiros = []
 roteiros.push(string1, string2, string3)
 
 for(i in array){
- console.log(`nome: ${array[i].nome} \nlançamento: ${array[i].anoLancamento} \nvale a pena?: ${array[i].valeAPena} \ndiretor: ${array[i].diretor} \nroteiro: ${array[i].roteiro}`)
+ console.log(`nome: ${array[i].nome} \nlançamento: ${array[i].anoLancamento} \nvale a pena?: ${array[i].valeAPena} \ndiretor: ${array[i].diretor} \nroteiro: ${array[i].elenco}`)
 }
