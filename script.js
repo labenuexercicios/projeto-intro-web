620a09a7619ef8daa13e7bd1d557ae72a8d54efc
myllenavieira <vieirasilvamyllena@gmail.com>
2022-08-23 21:33:26 -0300

    Mudança de array para objetos

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
-const material1 = "Aço 304";
-const class1 = 304;
-const disponibilidade1 = true;
+objeto1 = {
+    material: "Aço AISI 304",
+    numeroClassificação: 304,
+    disponibilidade: true,
+    caracteristicas: ['resistência a corrosão', 'boa resistência mecânica', 'resistência a alta temperatura']
+}
 
-const material2 = "Aço 316";
-const class2 = 316;
-const disponibilidade2 = false;
+objeto2 = {
+    material: "Aço 316",
+    numeroClassificação: 316,
+    disponibilidade: true,
+    caracteristicas: ['resistência a corrosão', 'excelente desempenho em meio salino', 'propriedades não magnéticas']
+}
 
-const material3 = "Aço 1020";
-const class3 = 1020;
-const disponibilidade3 = true;
+objeto3 = {
+    material: "Aço 1020",
+    numeroClassificação: 316,
+    disponibilidade: true,
+    caracteristicas: ['baixa temperabilidade', 'boa forjabilidade', 'baixa resistência mecânica']
+}
 
-const media = (class1 + class2 + class3)/3
+const dados = [];
+dados.push(objeto1);
+dados.push(objeto2);
+dados.push(objeto3);
+
+console.log(`Material: ${material1}
+Classificação: ${class1}
+Disponibilidade: ${disponibilidade1}
+Características: ${caracteristicas1}`);
+
+console.log(`Material: ${material2}
+Classificação: ${class2}
+Disponibilidade: ${disponibilidade2}
+Características: ${caracteristicas2}`);
+
+console.log(`Material: ${material3}
+Classificação: ${class3}
+Disponibilidade: ${disponibilidade3}
+Características: ${caracteristicas3}`);
+
+const media = (class1 + class2 + class3) / 3
 console.log(media);
 
 const disponibilidade = disponibilidade1 && disponibilidade2 && disponibilidade3;
