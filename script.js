2fea26e548c83467feeca5a670b3e08aedf93e37
Kieffer Torricilia <kieffer.torricilia@gmail.com>
2022-08-27 20:08:35 -0300

    Semana 6

--- script.js
+++ script.js
@@ -1,100 +1,79 @@
 //Filmes para ver antes de morrer
-let array = [
-{
+
+let objeto1 ={
     nome: "OS VINGADORES",
     anoLancamento: 2012,
     valeAPena: true,
     diretor: "Joss Whedon",
     elenco: ["Robert Downey Jr.", "Scarlett Johansson", "Chris Evans", "Mark Rufallo", "Chris Hemsworth", "Jeremy Renner",
 "Tom Hiddleston", "Samuel L. Jackson"],
-},
+};
 
-{
+let objeto2 = {
     nome: "VINGADORES: ERA DE ULTRON",
     anoLancamento: 2015,
     valeAPena: true,
     diretor: "Joss Whedon",
     elenco: ["Robert Downey Jr.", "Scarlett Johansson", "Chris Evans", "Mark Rufallo", "Chris Hemsworth", "Jeremy Renner",
     "Tom Hiddleston", "Samuel L. Jackson", "Elizabeth Olsen", "James Spader", "Paul Bettany", ],
-},
+};
 
-{
+let objeto3 = {
     nome: "VINGADORES: GUERRA INFINITA",
     anoLancamento: 2018,
     valeAPena: true,
     diretor: "Anthony e Joe Russo",
     elenco: ["Robert Downey Jr.", "Scarlett Johansson", "Chris Evans", "Mark Rufallo", "Chris Hemsworth", "Jeremy Renner",
     "Tom Hiddleston", "Samuel L. Jackson", "Elizabeth Olsen", "Paul Bettany",],
-},
+};
 
-{
+let objeto4 = {
     nome: "VINGADORES: ULTIMATO",
     anoLancamento: 2019,
     valeAPena: true,
     diretor: "Anthony e Joe Russo",
     elenco: ["Robert Downey Jr.", "Scarlett Johansson", "Chris Evans", "Mark Rufallo", "Chris Hemsworth", "Jeremy Renner",
     "Tom Hiddleston", "Samuel L. Jackson", "Elizabeth Olsen", "Paul Bettany"]
-},
-]
+};
 
+let arrayObjetos = []
+arrayObjetos.push(objeto1, objeto2, objeto3, objeto4)
 
 let media = 0
-for(let i in array){
-    media = media + array[i].anoLancamento
+for(let i in arrayObjetos){
+    media += arrayObjetos[i].anoLancamento
 }
-console.log(`Media dos anos de lançamento: ${media/array.length}`)
+console.log(`Media dos anos de lançamento: ${media/arrayObjetos.length}`)
 
-let verificarValeAPena = array[0].valeAPena && array[1].valeAPena && array[2].valeAPena && array[3].valeAPena
-    console.log(verificarValeAPena)
-    
-    
+let verificarValeAPena = arrayObjetos[0].valeAPena && arrayObjetos[1].valeAPena && arrayObjetos[2].valeAPena && arrayObjetos[3].valeAPena
+console.log(verificarValeAPena)
 
+ 
+//Semana 4 exercício 1
 let lista = []
 
 
-for(i in array){
-    array[i].valeAPena ? lista.push(array[i]) : alert(`Filme da posição ${i} não foi adicionado`)
+for(i in arrayObjetos){
+    arrayObjetos[i].valeAPena ? lista.push(arrayObjetos[i]) : alert(`Filme da posição ${i} não foi adicionado`)
 }
+// console.log(lista)
 
-
-console.log(lista)
-
-let string1 = []
+for(i of objeto1.elenco){
     
-for(i in array[0].elenco){
-    if(i < array[0].elenco.length - 1){
-        string1 += `${array[0].elenco[i]}, `
-    }else{
-        string1 += `${array[0].elenco[i]}`
-    } 
-}
-
-let string2 = []
-
-
-for(i in array[1].elenco){
-    if(i < array[1].elenco.length - 1){
-        string2 += `${array[1].elenco[i]}, `
-    }else{
-        string2 += `${array[1].elenco[i]}`
-    } 
 }
 
 
-let string3 = []
+// for(i in arrayObjetos){
+//  console.log(`nome: ${arrayObjetos[i].nome} \nlançamento: ${arrayObjetos[i].anoLancamento} \nvale a pena?: ${arrayObjetos[i].valeAPena} \ndiretor: ${arrayObjetos[i].diretor} \nroteiro: ${arrayObjetos[i].elenco}`)
+// }
 
-for(i in array[2].elenco){
-    if(i < array[2].elenco.length - 1){
-        string3 += `${array[2].elenco[i]}, `
-    }else{
-        string3 += `${array[2].elenco[i]}`
-    } 
+
+
+teste = (objeto) =>{
+    for(i in objeto){
+        console.log(objeto[i])
+    }
 }
+teste(objeto1)
+  
 
-
-let roteiros = []
-roteiros.push(string1, string2, string3)
-
-for(i in array){
- console.log(`nome: ${array[i].nome} \nlançamento: ${array[i].anoLancamento} \nvale a pena?: ${array[i].valeAPena} \ndiretor: ${array[i].diretor} \nroteiro: ${array[i].elenco}`)
-}
