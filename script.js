b72b2611271c4a5bf5472149bcd96e4afbbef0db
Matheus Mendes <matheusmendesreis@outlook.com>
2022-08-28 18:59:58 -0300

    Semana cinco

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
-// // Semana 1
+// Semana 1
 
-// // Exercicio 1
-// let nome
-// let anoLancamento = 0
-// let isSerie
-// let totTemporadas = 0
-// let genero = []
-// let elencoPrincipal = []
+// Exercicio 1
+let nome
+let anoLancamento = 0
+let isSerie
+let totTemporadas = 0
+let genero = []
+let elencoPrincipal = []
 
-// // Exercicio 2
-// let nome1 = 'greys anatomy'
-// let anoLancamento1 = Number(2005)
-// let isSerie1 = true
-// let totTemporadas1 = Number(19)
-// let genero1 = ['drama', 'ficção medica', 'romance', 'comedia dramatica']
-// let elencoPrincipal1 = ['ellen pompeo', 'patrick dempsey', 'sandra oh']
+// Exercicio 2
+let nome1 = 'greys anatomy'
+let anoLancamento1 = Number(2005)
+let isSerie1 = true
+let totTemporadas1 = Number(19)
+let genero1 = ['drama', 'ficção medica', 'romance', 'comedia dramatica']
+let elencoPrincipal1 = ['ellen pompeo', 'patrick dempsey', 'sandra oh']
 
-// let nome2 = 'lá casa de papel'
-// let anoLancamento2 = Number(2017)
-// let isSerie2 = true
-// let totTemporadas2 = Number(4)
-// let genero2 = ['assalto', 'drama', 'suspense', 'crime']
-// let elencoPrincipal2 = ['alvaro morte', 'alba flores', 'miguel herran']
+let nome2 = 'lá casa de papel'
+let anoLancamento2 = Number(2017)
+let isSerie2 = true
+let totTemporadas2 = Number(4)
+let genero2 = ['assalto', 'drama', 'suspense', 'crime']
+let elencoPrincipal2 = ['alvaro morte', 'alba flores', 'miguel herran']
 
 
-// let nome3 = 'vikings'
-// let anoLancamento3 = Number(2013)
-// let isSerie3 = true
-// let totTemporadas3 = Number(6)
-// let genero3 = ['guerra', 'drama', 'ação', 'aventura']
-// let elencoPrincipal3 = ['travis fimmel', 'clive standen', 'gustaf skarsgard']
+let nome3 = 'vikings'
+let anoLancamento3 = Number(2013)
+let isSerie3 = true
+let totTemporadas3 = Number(6)
+let genero3 = ['guerra', 'drama', 'ação', 'aventura']
+let elencoPrincipal3 = ['travis fimmel', 'clive standen', 'gustaf skarsgard']
 
-// // Exercicio 3
-// const mediaGa = (anoLancamento1 + totTemporadas1) / 2
-// const mediaLCP = (anoLancamento2 + totTemporadas2) / 2
-// const mediaVk = (anoLancamento3 + totTemporadas3) / 2
+// Exercicio 3
+const mediaGa = (anoLancamento1 + totTemporadas1) / 2
+const mediaLCP = (anoLancamento2 + totTemporadas2) / 2
+const mediaVk = (anoLancamento3 + totTemporadas3) / 2
 
-// // Exercicio 4
+// Exercicio 4
 // if (isSerie1 === true && isSerie2 === true && isSerie3 === true) {
 //     console.log('Os valores são verdadeiros')
 // } else {
 //     console.log('Os valores são falsos')
 // }
 
-// // Semana 2
+// Semana 2
 
-// //Exercicio 2
+//Exercicio 2
 // console.log('Nome da serie: ' + nome1.toUpperCase(), '\nLançamento: ' + anoLancamento1 + ' \nE uma serie: ' + isSerie1 + '\nTotal de de temporadas: ' + totTemporadas1 + '\nGenero: ' + genero1 + '\nElenco principal: ' + elencoPrincipal1)
 // console.log('\n\n')
 // console.log('Nome da serie: ' + nome2.toUpperCase(), '\nLançamento: ' + anoLancamento2 + ' \nE uma serie: ' + isSerie2 + '\nTotal de de temporadas: ' + totTemporadas2 + '\nGenero: ' + genero2 + '\nElenco principal: ' + elencoPrincipal2)
@@ -94,21 +94,45 @@ const bdSeries = []
 if(laCasaDePapel.isSerie){
     bdSeries.push(laCasaDePapel)
 }else{
-    alert('A serie ',laCasaDePapel.nome,'não foi adicionada.')
+    // alert('A serie ',laCasaDePapel.nome,'não foi adicionada.')
     
 }
 
 if(greysAnatomy.isSerie){
     bdSeries.push(greysAnatomy)
 }else{
-    alert('A serie ',greysAnatomy.nome,' não foi adicionado')
+    // alert('A serie ',greysAnatomy.nome,' não foi adicionado')
 }
 
 if (vikings.isSerie) {
     bdSeries.push(vikings)
 } else {
-    alert('A serie ',vikings.nome,' não foi adicionado')
+    // alert('A serie ',vikings.nome,' não foi adicionado')
+}
+// console.log(teste)
+
+
+let = stringGreysAnatomy = ""
+for(let i in greysAnatomy.elencoPrincipal){
+    stringGreysAnatomy += greysAnatomy.elencoPrincipal1[i] + " "
+}
+
+let = stringLaCasaDePapel = ""
+for(let i in laCasaDePapel.elencoPrincipal1){
+    stringLaCasaDePapel += laCasaDePapel.elencoPrincipal1[i] + " "
+}
+
+let = stringVikings = ""
+for(let i in vikings.elencoPrincipal1){
+    stringVikings += vikings.elencoPrincipal1[i] + " "
+}
+
+let teste = ""
+for(let i in vikings[i]){
+    teste = i + " "
 }
 
 
-console.log(bdSeries)
+
+
+
