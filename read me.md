5c6d4e3e8870b3c9690ca162ee1445cb0c0e5066
Humberto de Oliveira <humbertosilva06@gmail.com>
2022-08-31 12:49:23 -0300

    projeto introdução a web semana 1

---
+++ read me.md
@@ -0,0 +1,10 @@
+# Projeto intro-web
+## Semana 1
+
+1- foi criado o item manga e suas caracteristicas, com pelo menos uma string, um number e um booleano
+
+2- foi criado três conjuntos de item com dados diferentes
+
+3- feito um calculo de média dos volumes de cada obra, e armazenada em uma const
+
+4- impressão de variavel que verifica se todos os valores booleanos são verdadeiros
---
+++ script.js
@@ -0,0 +1,57 @@
+//Semana 1
+//1 e 2 -criação dos itens e de 3 conjuntos
+
+const manga1 = "Naruto"
+const nomeOriginal = "Naruto"
+const sinopse = "Doze anos antes do começo da série, um poderosa criatura chamada Raposa de nove caudas ataca a Vila oculta da folha, uma aldeia de ninjas, causando destruição e inumeras mortes. Como resposta, o lider da vila, conhecido como Quarto Hokage, sacrifica sua vida para selar o demonio no corpo do recém-nascido Naruto Uzumaki.\n\nA vila porém, trata o garoto como se o próprio fosse o demoonio raposa, o isolando durante a maior parte de sua infãncia. O enredo conta a história de Naruto, agora um ninja adolescente, e seu objetivo de se tornar o novo Hokage, fazendo assim, com que todos reconheçam sua existência "
+const nomeRoteirista = "Masashi kishimoto"
+const nomeArtista = "Masashi Kishimoto"
+const genero = "Ação, aventura, comédia, drama"
+const concluido = true
+const numeroVolumes= 72
+const anoLancamento = 1999
+const anoEncerramento = 2015
+const revistaPublicada = "Weekly shonen jump"
+const editora = "Shueisha"
+const mangasRelacionados = "Boruto"
+
+const manga2 = "Real"
+const nomeOriginal2 = "Real"
+const sinopse2 = "A história gira em torno de três adolescentes: Nomiya Tomomi, um rapaz que abandonou o ensino médio, Togawa Kiyoharu, um ex-corredor que agora joga basquete sobre cadeira de rodas e Takahashi Hisanobu, um popular líder do time de basquetebol da escola que agora está paraplegico em função de um acidente.\n\n Real apresenta um elenco de personagens que se veem marginalizados pela sociedade, mas todos unidos por uma característica comum: o desejo de jogar basquete, mesmo sem lugar para jogar.\n\n Nomiya, tendo abandonado o ensino médio, não tem expectativa de futuro. Togawa, tendo uma personalidade difícil, encontra-se constantemente brigando com seus próprios companheiros de equipe. Takahashi, antes um popular líder de equipe, agora se vê incapaz de se mover do peito para baixo\n\n Real também lida com a realidade das deficiências físicas e a inferioridade psicológica contra a qual os personagens lutam. Os personagens rompem suas próprias barreiras psicológicas pouco a pouco"
+const nomeRoteirista2= "Takehiko Inoue"
+const nomeArtista2 = "Takehiko Inoue"
+const genero2 = "Drama, esporte, slice of life"
+const concluido2 = false
+const numeroVolumes2= 15
+const anoLancamento2 = 1999
+let anoEncerramento2 ="em andamento"
+const revistaPublicada2="Shuukan Young Jump"
+const editora2 = "Shueisha"
+const mangasRelacionados2 = "nenhum"
+
+const manga3 = "Frieren e a jornada para o alem"
+const nomeOriginal3 = "Sousou no frieren"
+const sinopse3 = "A jornada acabou mas a vida continua para uma elfa que apenas começou a aprender sobre o que é a vida\n\n A elfa Frieren e seus corajosos companheiros de viagem derroataram o Rei demônio e trouxeram paz para sua terra. após o termino de sua jornada, eles se separam e vão viver suas pacatas vidas. Porém, sendo uma elfa, Frieren é praticamente imortal, e viverá milhares de anos mais que seus companheiros de jornada. Como Frieren lidará com a morte de seus amigos? Seria ela capaz de encontrar satisfação em sua prórpia vida e entender o que significa a vida para os humanos ao seu redor? Frieren começa uma nova jornada para encontrar essas respostas "
+const nomeRoteirista3 = "Yamada Kanehito"
+const nomeArtista3 = "Abe Tsukasa"
+const genero3 = "Aventura, comédia, drama, fantasia, Slice of Life"
+const concluido3 = false
+const numeroVolumes3= 9
+const anoLancamento3 = 2020
+let anoEncerramento3 = "em andamento"
+const revistaPublicada3 = "Shuukan Shonen Sunday"
+const editora3 = "Shogakukan"
+const mangasRelacionados3 = "nenhum"
+
+//3
+
+const mediaVolumes = (numeroVolumes + numeroVolumes2 + numeroVolumes3)/3
+console.log (mediaVolumes)
+
+
+//4
+
+const verificaConcluidos = concluido && concluido2 && concluido3
+console.log (verificaConcluidos)
+
+console.log(mangasRelacionados3)
