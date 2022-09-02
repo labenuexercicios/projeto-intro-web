3b2555ff7cf95222fb7df6217be5a311cb1453d1
JayGovinda13 <jaygovinda13@gmail.com>
2022-09-02 14:01:41 -0300

    Semana-1

---
+++ index.js
@@ -0,0 +1,32 @@
+//Livros 
+/*const nome = prompt("Olá, qual seu nome?")
+const idade = prompt("Quantos anos voce tem?")
+const altura = prompt("Qual sua altura?")
+const pessoa = `Nome: ${nome} \nIdade: ${idade} \nAltura: ${altura}`
+console.log(pessoa)*/
+
+
+const tituloLivro1 = "Bhagavad-Gita: Como ele é"
+const autorLivro1 = "Swami Maharaj"
+const anoDeEdicao1 = 1997
+const adiquirido1 = true
+const sequencias1 = [0]
+console.log('livro'.toUpperCase())
+console.log(`Tituto: \"${tituloLivro1}\" \nAutor: ${autorLivro1} \nAno da Edição: ${anoDeEdicao1} \nSequencias: ${sequencias1}`)
+
+const tituloLivro2 = "Pedra no Céu"
+const autorLivro2 = "Isaac Azimov"
+const anoDeEdicao2 = 2019
+const adiquirido2 = true
+const sequencias2 = [5, "Saga Fundação"]
+console.log(`Tituto: \"${tituloLivro2}\" \nAutor: ${autorLivro2} \nAno da Edição: ${anoDeEdicao2} \nSequencias: ${sequencias2}`)
+
+const tituloLivro3 = "Jurasic Park"
+const autorLivro3 = "Michael Crichton"
+const anoDeEdicao3 = 2020
+const adiquirido3 = false
+const sequencias3 = [1, "Jurasic World"]
+console.log(`Tituto: \"${tituloLivro3}\" \nAutor: ${autorLivro3} \nAno da Edição: ${anoDeEdicao3} \nSequencias: ${sequencias3}`)
+
+//const anoMedio = (anoDeEdicao1 + anoDeEdicao2 + anoDeEdicao3) / 3
+//console.log(anoMedio)
