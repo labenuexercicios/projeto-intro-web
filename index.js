950961815b96ef941f046ea304154b876338dc53
slvgs <gabriellasilveriosantana15@gmail.com>
2022-09-02 22:02:42 -0300

    Modifiquei o console.log

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const Artista1 = "Banda Zimbra"
 const QuantosAlbuns = 04
 const TemMaisDe5anos = 12 > 5 
 const AlgunsAlbuns1 = ["Azul", "Sala Dois", "Verniz", "O tudo, o Nada e o Mundo"]
-console.log(Artista1.toUpperCase() +"\nQuantos Albuns: " + QuantosAlbuns + "\nTem mais de 5 anos? "  + TemMaisDe5anos +  "\nAlguns Albums: " + AlgunsAlbuns1)
+console.log (Artista1.toUpperCase(), "\nQuantos Albuns:",  QuantosAlbuns,  "\nTem mais de 5 anos?", TemMaisDe5anos, "\nAlguns Albums:", AlgunsAlbuns1)
 
 const Artista2 = "Miley Cyrus"
 const QuantosAlbuns2 = 09
 const TemMaisDe5anos2 = 21 > 5 
 const AlgunsAlbuns2 = ["Bangerz", "Can't Be Tamed", "Plastic Hearts" ]
-console.log(Artista2.toUpperCase() +"\nQuantos Albuns: " + QuantosAlbuns2 + "\nTem mais de 5 anos? "  + TemMaisDe5anos2 + "\nAlguns Albums: " + AlgunsAlbuns2)
+console.log (Artista2.toUpperCase(), "\nQuantos Albuns:",  QuantosAlbuns2,  "\nTem mais de 5 anos?", TemMaisDe5anos2, "\nAlguns Albums:", AlgunsAlbuns2)
 
 const  Artista3 = "Pedro Sampaio"
 const QuantosAlbuns3 = 02
 const TemMaisDe5anos3 = 3 > 5 
 const AlgunsAlbuns3 = ["Chama meu Nome", "REMIXES"]
-console.log(Artista3.toUpperCase() +"\nQuantos Albuns: " + QuantosAlbuns3 + "\nTem mais de 5 anos? "  + TemMaisDe5anos3 + "\nAlguns Albums: " + AlgunsAlbuns3)
+console.log (Artista3.toUpperCase(), "\nQuantos Albuns:",  QuantosAlbuns3,  "\nTem mais de 5 anos?", TemMaisDe5anos3, "\nAlguns Albums:", AlgunsAlbuns3)
 
 const MediaAlbuns = 22 / 3
 console.log(MediaAlbuns)
