42d484d330a1e1b8edcf5c82ba4d3d6538d22475
Bruno Alves de Araujo <88730176+brunodev21@users.noreply.github.com>
2022-09-04 21:59:06 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,84 @@
+// DRAGON BALL PERSONAGENS
+
+// Personagem 01 -------------------------------------------------------------------------------------------
+
+const nomePersonagem1 = "GOKU"
+console.log(nomePersonagem1)
+
+let idadePersonagem1 = 42
+console.log(`A idade de ${nomePersonagem1} é: ${idadePersonagem1} anos.`)
+
+let kiPersonagem1 = 150
+console.log(`O KI do ${nomePersonagem1} é de ${kiPersonagem1} bilhões.`)
+
+const personagemPoderoso1 = kiPersonagem1 >= 100
+console.log(`O ${nomePersonagem1} é poderoso? ${personagemPoderoso1}`)
+
+const caractePersonagem1 = [" Coragem" , " Resiliência" , " Força"]
+console.log(`As características de ${nomePersonagem1} são: ${caractePersonagem1}`)
+
+
+// Personagem 02 -------------------------------------------------------------------------------------------
+
+const nomePersonagem2 = "VEGETA"
+console.log(nomePersonagem2)
+
+let idadePersonagem2 = 48
+console.log(`A idade de ${nomePersonagem2} é: ${idadePersonagem2} anos.`)
+
+let kiPersonagem2 = 145
+console.log(`O KI do ${nomePersonagem2} é de ${kiPersonagem2} bilhões.`)
+
+const personagemPoderoso2 = kiPersonagem2 >= 100
+console.log(`O ${nomePersonagem2} é poderoso? ${personagemPoderoso2}`)
+
+const caractePersonagem2 = [" Agressividade" , " Coragem" , " Agilidade"]
+console.log(`As características de ${nomePersonagem2} são: ${caractePersonagem2}`)
+
+
+// Personagem 03 -------------------------------------------------------------------------------------------
+
+const nomePersonagem3 = "GOHAN"
+console.log(nomePersonagem3)
+
+let idadePersonagem3 = 23
+console.log(`A idade de ${nomePersonagem3} é: ${idadePersonagem3} anos.`)
+
+let kiPersonagem3 = 130
+console.log(`O KI do ${nomePersonagem3} é de ${kiPersonagem3} bilhões.`)
+
+const personagemPoderoso3 = kiPersonagem3 >= 100
+console.log(`O ${nomePersonagem3} é poderoso? ${personagemPoderoso3}`)
+
+
+const caractePersonagem3 = [" Velocidade" , " Coragem" , " Força"]
+console.log(`As características de ${nomePersonagem3} são: ${caractePersonagem3}`)
+
+
+// Personagem 04 -------------------------------------------------------------------------------------------
+
+const nomePersonagem4 = "FREEZA"
+console.log(nomePersonagem4)
+
+let idadePersonagem4 = 70
+console.log(`A idade de ${nomePersonagem4} é: ${idadePersonagem4} anos.`)
+
+let kiPersonagem4 = 50
+console.log(`O KI do ${nomePersonagem4} é de ${kiPersonagem4} bilhões.`)
+
+const personagemPoderoso4 = kiPersonagem4 >= 95
+console.log(`O ${nomePersonagem4} é poderoso? ${personagemPoderoso4}`)
+
+
+const caractePersonagem4 = [" Renascimento" , " Resistência" , " Maldade"]
+console.log(`As características de ${nomePersonagem4} são: ${caractePersonagem4}`)
+
+
+// Médias dos Personagens -------------------------------------------------------------------------------------
+
+const idadeMediaPersonagens = (idadePersonagem1 + idadePersonagem2 + idadePersonagem3 + idadePersonagem4) / 4
+console.log(`A média de idade entre os personagens é de: ${idadeMediaPersonagens} anos.`)
+
+const kiMedioPersonagens = (kiPersonagem1 + kiPersonagem2 + kiPersonagem3 + kiPersonagem4) / 4
+console.log(`A média do KI entre os personagens é de: ${kiMedioPersonagens} bilhões.`)
+
