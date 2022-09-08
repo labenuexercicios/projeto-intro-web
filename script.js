120e7500cb940584c8b8b722792a7ad9bceb7fbf
Adriano Silva <adrianowic0101@gmail.com>
2022-09-08 14:34:56 -0300

    Feito da semana 1 até a semana 7.

--- script.js
+++ script.js
@@ -1,31 +1,266 @@
-const nome1 = 'Sandman - Preludios e Noturnos';
-const arco1 = 1;
-const lista1 = true
-const cap1 = ['o sono dos justos', 'anfitriões imperfeitos', 'sonhe um breve sonho comigo', 'uma esperança no inferno', 'passageiros', '24 horas', 'som e furia', 'o som de suas asas']
-console.log(`${nome1.toUpperCase()} \nNúmero do arco: #${arco1} \nConsta na lista: ${lista1} \nNome dos capitulos: ${cap1.join(`\n- `)}`)
+//Criando um array vazio
+let arcosSandman = [];
 
-console.log('--------------------------------------------------------------------------------------------')
+//Adicionando os itens criados na semana 2 dentro de um objeto
+obj1 = {
+    nome: 'Sandman'.toUpperCase(),
+    nomeArco: 'preludios e noturnos',
+    arco: 1,
+    estaNaLista: true,
+    caminhoImagem: 'imagens/capa-arco-1.jpg',
+    cap: ['o sono dos justos', 'anfitriões imperfeitos', 'sonhe um breve sonho comigo', 'uma esperança no inferno', 'passageiros', '24 horas', 'som e furia', 'o som de suas asas']
+};
+//Criando uma variavel, percorrendo o obj ate o array e concatenando os elementos do array em uma unica string e armazenando dentro da variavel criada.
+let valorObj1
+for(i of obj1.cap){
+    valorObj1 = obj1.cap.join(', ')  
+    
+}; 
+// console.log(valorObj1)
+// console.log(Object.values(obj1))
+// console.log(JSON.stringify(obj1))
 
-const nome2 = 'Sandman - Casa de Bonecas';
-const arco2 = 2;
-const lista2 = true;
-const cap2 = ['contos na areia', 'cada de bonecas', 'mudanças', 'brincando de casinha', 'homens de boa fortuna', 'os colecionadores', 'noite adentro', 'corações perdidos']
-console.log(`${nome2.toUpperCase()} \nNúmero do arco: #${arco2} \nConsta na lista: ${lista2} \nNome dos capitulos: ${cap2.join(`\n- `)}`)
+ //let qualquer = 'obj1: {'
+// Object.entries(obj1).forEach(([key, value]) => {
+//     if(key != 'cap'){
+//     console.log(key+': '+value)
+//     //qualquer += key+': '+' \''+value+'\', '
+//     }
+//     else{
+//     console.log(key+': '+valorObj1)
+//     //qualquer += key+': '+' \''+valorObj1+'\''
+//     }
+// });
+// //qualquer += '}'
+// console.log(qualquer)
 
-console.log('--------------------------------------------------------------------------------------------')
+//Adicionando os itens criados na semana 2 dentro de um objeto
+obj2 = {
+    nome: 'Sandman'.toUpperCase(),
+    nomeArco: 'casa de bonecas',
+    arco: 2,
+    estaNaLista: true,
+    caminhoImagem: 'imagens/capa-arco-2.jpg',
+    cap: ['contos na areia', 'casa de bonecas', 'mudanças', 'brincando de casinha', 'homens de boa fortuna', 'os colecionadores', 'noite adentro', 'corações perdidos']
+};
+//Criando uma variavel, percorrendo o obj ate o array e concatenando os elementos do array em uma unica string e armazenando dentro da variavel criada.
+let valorObj2 
 
-const nome3 = 'Sandman - Terra dos Sonhos';
-const arco3 = 0;
-const lista3 = true;
-const cap3 = ['caliope', 'um sonho de mil gatos', 'sonho de uma noite de verão', 'fachada']
-console.log(`${nome3.toUpperCase()} \nNúmero do arco: #${arco3} \nConsta na lista: ${lista3} \nNome dos capitulos: - ${cap3.join(`\n- `)}`)
+for(i of obj2.cap){
+    valorObj2 = obj2.cap.join(', ')
+};
+
+//Adicionando os itens criados na semana 2 dentro de um objeto
+obj3 = {
+    nome: 'Sandman'.toUpperCase(),
+    nomeArco: 'terra dos sonhos',
+    arco: 3,
+    estaNaLista: true,
+    caminhoImagem: 'imagens/capa-arco-3.jpg',
+    cap: ['caliope', 'um sonho de mil gatos', 'sonho de uma noite de verão', 'fachada']
+};
+//Criando uma variavel, percorrendo o obj ate o array e concatenando os elementos do array em uma unica string e armazenando dentro da variavel criada.
+let valorObj3
+for(i of obj3.cap){
+    valorObj3 = obj3.cap.join(', ') 
+};
+
+//Adicionando os itens criados na semana 2 dentro de um objeto
+obj4 = {
+    nome: 'Sandman'.toUpperCase(),
+    nomeArco: 'estação das brumas',
+    arco: 4,
+    estaNaLista: true,
+    caminhoImagem: 'imagens/capa-arco-4.jpg',
+    cap: ['Estação das brumas: Um Prólogo','Estação das Brumas: Capítulo 1', 'Estação das Brumas: Capítulo 2', 'Estação das Brumas: Capítulo 3', 'Estação das Brumas: Capítulo 4', 'Estação das Brumas: Capítulo 5', 'Estação das Brumas: Capítulo 6', 'Estação das Brumas: Epílogo']
+};
+//Criando uma variavel, percorrendo o obj até o array e concatenando os elementos do array em uma unica string e armazenando dentro da variavel criada
+let valorObj4
+for(i of obj4.cap){
+    valorObj4 = obj4.cap.join(', ')
+};
+
+//Adicionando os itens criados na semana 2 dentro de um objeto
+obj5 = {
+    nome: 'Sandman'.toUpperCase(),
+    nomeArco: 'um jogo de você',
+    arco: 5,
+    estaNaLista: true,
+    caminhoImagem: 'imagens/capa-arco-5.jpg',
+    cap: ['Chacina na Quinta Avenida', 'Cantigas de Ninar da Broadway', 'O Nascer da Lua Má', 'Começando a Ver a Luz', 'Sobre o Mar, Rumo ao Céu', 'Eu acordei e Um de Nós Estava Chorando']
+};
+//Criando uma variavel, percorrendo o obj até o array e concatenando os elementos do array em uma unica string e armazenando dentro da variavel criada
+let valorObj5
+for(i of obj5.cap){
+    valorObj5 = obj5.cap.join(', ')
+};
+
+//Adicionando os itens criados na semana 2 dentro de um objeto
+obj6 = {
+    nome: 'Sandman'.toUpperCase(),
+    nomeArco: 'fabulas e reflexões',
+    arco: 6,
+    estaNaLista: true,
+    caminhoImagem: 'imagens/capa-arco-6.jpg',
+    cap: ['Espelhos Distantes: Termidor', 'Espelhos Distantes: Augustus', 'Espelhos Distantes: Três Setembros e Um Janeiro', 'Convergência: Caçada', 'Convergência: Lugares Maleáveis', 'Convergência: O Parlamento das Gralhas', 'Espelhos Distantes: Ramadã', 'A Canção de Orpheus', 'Medo de Cair']
+};
+//Criando uma variavel, percorrendo o obj até o array e concatenando os elementos do array em uma unica string e armazenando dentro da variavel criada
+let valorObj6
+for(i of obj6.cap){
+    valorObj6 = obj6.cap.join(', ')
+};
+
+//Adicionando os itens criados na semana 2 dentro de um objeto
+obj7 = {
+    nome: 'Sandman'.toUpperCase(),
+    nomeArco: 'vidas breves',
+    arco: 7,
+    estaNaLista: true,
+    caminhoImagem: 'imagens/capa-arco-7.jpg',
+    cap: ['Vidas Breves: Capítulo 1', 'Vidas Breves: Capítulo 2', 'Vidas Breves: Capítulo 3', 'Vidas Breves: Capítulo 4', 'Vidas Breves: Capítulo 5', 'Vidas Breves: Capítulo 6', 'Vidas Breves: Capítulo 7', 'Vidas Breves: Capítulo 8', 'Vidas Breves: Capítulo 9']
+};
+//Criando uma variavel, percorrendo o obj até o array e concatenando os elementos do array em uma unica string e armazenando dentro da variavel criada
+let valorObj7
+for(i of obj7.cap){
+    valorObj7 = obj7.cap.join(', ')
+};
+
+//Adicionando os itens criados na semana 2 dentro de um objeto
+obj8 = {
+    nome: 'Sandman'.toUpperCase(),
+    nomeArco: 'fim dos mundos',
+    arco: 8,
+    estaNaLista: true,
+    caminhoImagem: 'imagens/capa-arco-8.jpg',
+    cap: ['Um Conto de Duas Cidades', 'O Conto de Cluracan', 'o Leviatã de Hob', 'O Menino de Ouro', 'Mortalha', 'Fim dos Mundos']
+};
+//Criando uma variavel, percorrendo o obj até o array e concatenando os elementos do array em uma unica string e armazenando dentro da variavel criada
+let valorObj8
+for(i of obj8.cap){
+    valorObj8 = obj8.cap.join(', ')
+};
+
+//Adicionando os itens criados na semana 2 dentro de um objeto
+obj9 = {
+    nome: 'Sandman'.toUpperCase(),
+    nomeArco: 'entes queridos',
+    arco: 9,
+    estaNaLista: true,
+    caminhoImagem: 'imagens/capa-arco-9.jpg',
+    cap: ['As Bondosas: Capítulo 1', 'As Bondosas: Capítulo 2', 'As Bondosas: Capítulo 3', 'As Bondosas: Capítulo 4', 'As Bondosas: Capítulo 5', 'As Bondosas: Capítulo 6', 'As Bondosas: Capítulo 7', 'As Bondosas: Capítulo 8', 'As Bondosas: Capítulo 9', 'As Bondosas: Capítulo 10', 'As Bondosas: Capítulo 11', 'As Bondosas: Capítulo 12', 'As Bondosas: Capítulo 13']
+};
+//Criando uma variavel, percorrendo o obj até o array e concatenando os elementos do array em uma unica string e armazenando dentro da variavel criada
+let valorObj9
+for(i of obj9.cap){
+    valorObj9 = obj9.cap.join(', ')
+};
+
+//Adicionando os itens criados na semana 2 dentro de um objeto
+obj10 = {
+    nome: 'Sandman'.toUpperCase(),
+    nomeArco: 'o despertar',
+    arco: 10,
+    estaNaLista: true,
+    caminhoImagem: 'imagens/capa-arco-10.jpg',
+    cap: ['O Qual Ocorre no Rastro do Que Se Passou', 'No Qual um Vélorio se Realiza', 'No Qual o Velorio se Encerra e Nós Despertamos', 'Um Epilogo - Luto Dominical', 'Exilados', 'A Tempestade']
+};
+//Criando uma variavel, percorrendo o obj até o array e concatenando os elementos do array em uma unica string e armazenando dentro da variavel criada
+let valorObj10
+for(i of obj10.cap){
+    valorObj10 = obj10.cap.join(', ')
+};
+
+//Adicionando os itens criados na semana 2 dentro de um objeto
+obj11 = {
+    nome: 'Sandman'.toUpperCase(),
+    nomeArco: 'Preludio',
+    arco: 11,
+    estaNaLista: true,
+    caminhoImagem: 'imagens/capa-arco-11.jpg',
+    cap: ['Capitulo 1', 'Capitulo 2', 'Capitulo 3']
+};
+//Criando uma variavel, percorrendo o obj até o array e concatenando os elementos do array em uma unica string e armazenando dentro da variavel criada
+let valorObj11
+for(i of obj11.cap){
+    valorObj11 = obj11.cap.join(', ')
+};
+
+//Criando um array e adicionando as variaveis com os arrays dos objs concatenados. E mostrando um relatorio desse array usando o laço. 
+let valorObjTotal = [valorObj1, valorObj2, valorObj3, valorObj4, valorObj5, valorObj6, valorObj7, valorObj8, valorObj9, valorObj10, valorObj11]
 
 
-console.log('---------Calculando a média e Verificando a lista--------- ')
-//Calculando a media
-const media = (arco1+arco2+arco3)/3
-console.log(media)
 
-//Verificando a lista
-const verificarLista = lista1 && lista2 && lista3
-console.log(verificarLista)
+//Criando uma condição para adicionar o obj no array "arcosSandman"
+if(obj1.estaNaLista === true ? arcosSandman.push(obj1) : alert(`O arco "${obj1.nomeArco}" não foi adicionado`))
+if(obj2.estaNaLista === true ? arcosSandman.push(obj2) : alert(`O arco "${obj2.nomeArco}" não foi adicionado`))
+if(obj3.estaNaLista === true ? arcosSandman.push(obj3) : alert(`O arco "${obj3.nomeArco}" não foi adicionado`))
+if(obj4.estaNaLista === true ? arcosSandman.push(obj4) : alert(`O arco "${obj4.nomeArco}" não foi adicionado`))
+if(obj5.estaNaLista === true ? arcosSandman.push(obj5) : alert(`O arco "${obj5.nomeArco}" não foi adicionado`))
+if(obj6.estaNaLista === true ? arcosSandman.push(obj6) : alert(`O arco "${obj6.nomeArco}" não foi adicionado`))
+if(obj7.estaNaLista === true ? arcosSandman.push(obj7) : alert(`O arco "${obj7.nomeArco}" não foi adicionado`))
+if(obj8.estaNaLista === true ? arcosSandman.push(obj8) : alert(`O arco "${obj8.nomeArco}" não foi adicionado`))
+if(obj9.estaNaLista === true ? arcosSandman.push(obj9) : alert(`O arco "${obj9.nomeArco}" não foi adicionado`))
+if(obj10.estaNaLista === true ? arcosSandman.push(obj10) : alert(`O arco "${obj10.nomeArco}" não foi adicionado`))
+if(obj11.estaNaLista === true ? arcosSandman.push(obj11) : alert(`O arco "${obj11.nomeArco}" não foi adicionado`))
+
+console.log(arcosSandman)
+
+//Devolvendo array de objetos em string
+
+const relatorioObjetos = arcosSandman.map((x) => {
+    return{
+        nome: x.nome,
+        nomeArco: x.nomeArco,
+        arco: x.arco,
+        estaNaLista: x.estaNaLista,
+        cap: x.cap.toString()
+    
+    }
+    
+}); 
+console.log(relatorioObjetos)
+
+
+
+//--------------------------Semana 6-------------------------------------
+let relatorio
+const funcaoObjeto = (obj) =>{
+    
+    for(i in obj){
+        
+        relatorio = console.log(`${i}: ${obj[i]}`)     
+    }
+    return relatorio
+     
+};
+ //console.log(funcaoObjeto(obj1))
+
+//  const retornoObjeto = arcosSandman.find(elemento => elemento.nome === 'Sandman')
+
+//  console.log(retornoObjeto)
+
+// let n1 = prompt('digite o nome')
+// const retornoObjeto2 = arcosSandman.filter((numero, indice, array) => {
+//        if(numero.nomeArco === n1){
+//         return retornoObjeto2
+//        } else{
+//         console.log('item não encontrado')
+//        };
+//return numero.nomeArco === n1 || alert('Esse item não foi encontrado !!') 
+// })
+
+//Buscador
+
+let filtrados = []
+function procurar (array,arco){
+     filtrados = array.filter((i) => i.nomeArco.toUpperCase() === arco.toUpperCase());
+        if(filtrados.length >= 1){
+            return filtrados
+        } else{
+            console.log('Item não encontrado')
+        };
+};
+
+console.log(procurar(arcosSandman, prompt('Digite aqui o arco que procura...')))
+
+
