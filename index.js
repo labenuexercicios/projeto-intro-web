709199d7b473ee5dbbb89accdcbf186998174532
Bruno Alves de Araujo <88730176+brunodev21@users.noreply.github.com>
2022-09-09 17:24:15 -0300

    segundo commit

--- index.js
+++ index.js
@@ -2,83 +2,86 @@
 
 // Personagem 01 -------------------------------------------------------------------------------------------
 
-const nomePersonagem1 = "GOKU"
-console.log(nomePersonagem1)
-
-let idadePersonagem1 = 42
-console.log(`A idade de ${nomePersonagem1} é: ${idadePersonagem1} anos.`)
-
-let kiPersonagem1 = 150
-console.log(`O KI do ${nomePersonagem1} é de ${kiPersonagem1} bilhões.`)
-
-const personagemPoderoso1 = kiPersonagem1 >= 100
-console.log(`O ${nomePersonagem1} é poderoso? ${personagemPoderoso1}`)
-
-const caractePersonagem1 = [" Coragem" , " Resiliência" , " Força"]
-console.log(`As características de ${nomePersonagem1} são: ${caractePersonagem1}`)
-
+const personagem1 = { 
+    nome : "Goku",
+    idade : Number(42),
+    ki : Number(150),
+    poder : true,
+    caracteristicas : [" Coragem" , " Resiliência" , " Força"],
+}
+    // console.log(`Nome do personagem: ${personagem1.nome.toUpperCase()}
+    // Idade: ${personagem1.idade} anos
+    // Esse personagem tem o KI de: ${personagem1.ki} bilhões
+    // Ele é poderoso? ${personagem1.poder}
+    // Suas características: ${personagem1.caracteristicas}`)
+    
 
 // Personagem 02 -------------------------------------------------------------------------------------------
 
-const nomePersonagem2 = "VEGETA"
-console.log(nomePersonagem2)
-
-let idadePersonagem2 = 48
-console.log(`A idade de ${nomePersonagem2} é: ${idadePersonagem2} anos.`)
-
-let kiPersonagem2 = 145
-console.log(`O KI do ${nomePersonagem2} é de ${kiPersonagem2} bilhões.`)
-
-const personagemPoderoso2 = kiPersonagem2 >= 100
-console.log(`O ${nomePersonagem2} é poderoso? ${personagemPoderoso2}`)
-
-const caractePersonagem2 = [" Agressividade" , " Coragem" , " Agilidade"]
-console.log(`As características de ${nomePersonagem2} são: ${caractePersonagem2}`)
-
+const personagem2 = { 
+    nome : "Vegeta",
+    idade : Number(48),
+    ki : Number(145),
+    poder : true,
+    caracteristicas : [" Agressividade" , " Coragem" , " Agilidade"],
+}
+    // console.log(`Nome do personagem: ${personagem2.nome.toUpperCase()}
+    // Idade: ${personagem2.idade} anos
+    // Esse personagem tem o KI de: ${personagem2.ki} bilhões
+    // Ele é poderoso? ${personagem2.poder}
+    // Suas características: ${personagem2.caracteristicas}`)
+    
 
 // Personagem 03 -------------------------------------------------------------------------------------------
-
-const nomePersonagem3 = "GOHAN"
-console.log(nomePersonagem3)
-
-let idadePersonagem3 = 23
-console.log(`A idade de ${nomePersonagem3} é: ${idadePersonagem3} anos.`)
-
-let kiPersonagem3 = 130
-console.log(`O KI do ${nomePersonagem3} é de ${kiPersonagem3} bilhões.`)
-
-const personagemPoderoso3 = kiPersonagem3 >= 100
-console.log(`O ${nomePersonagem3} é poderoso? ${personagemPoderoso3}`)
-
-
-const caractePersonagem3 = [" Velocidade" , " Coragem" , " Força"]
-console.log(`As características de ${nomePersonagem3} são: ${caractePersonagem3}`)
+ 
+const personagem3 = { 
+    nome : "Gohan",
+    idade : Number(23),
+    ki : Number(130),
+    poder : true,
+    caracteristicas : [" Velocidade" , " Coragem" , " Força"],
+}
+    // console.log(`Nome do personagem: ${personagem3.nome.toUpperCase()}
+    // Idade: ${personagem3.idade} anos
+    // Esse personagem tem o KI de: ${personagem3.ki} bilhões
+    // Ele é poderoso? ${personagem3.poder}
+    // Suas características: ${personagem3.caracteristicas}`)
 
 
 // Personagem 04 -------------------------------------------------------------------------------------------
 
-const nomePersonagem4 = "FREEZA"
-console.log(nomePersonagem4)
-
-let idadePersonagem4 = 70
-console.log(`A idade de ${nomePersonagem4} é: ${idadePersonagem4} anos.`)
-
-let kiPersonagem4 = 50
-console.log(`O KI do ${nomePersonagem4} é de ${kiPersonagem4} bilhões.`)
-
-const personagemPoderoso4 = kiPersonagem4 >= 95
-console.log(`O ${nomePersonagem4} é poderoso? ${personagemPoderoso4}`)
-
-
-const caractePersonagem4 = [" Renascimento" , " Resistência" , " Maldade"]
-console.log(`As características de ${nomePersonagem4} são: ${caractePersonagem4}`)
+const personagem4 = { 
+    nome : "Freeza",
+    idade : Number(70),
+    ki : Number(50),
+    poder : false,
+    caracteristicas : [" Renascimento" , " Resistência" , " Maldade"],
+}
+    // console.log(`Nome do personagem: ${personagem4.nome.toUpperCase()}
+    // Idade: ${personagem4.idade} anos
+    // Esse personagem tem o KI de: ${personagem4.ki} bilhões
+    // Ele é poderoso? ${personagem4.poder}
+    // Suas características: ${personagem4.caracteristicas}`)
 
 
 // Médias dos Personagens -------------------------------------------------------------------------------------
 
-const idadeMediaPersonagens = (idadePersonagem1 + idadePersonagem2 + idadePersonagem3 + idadePersonagem4) / 4
-console.log(`A média de idade entre os personagens é de: ${idadeMediaPersonagens} anos.`)
+// const idadeMediaPersonagens = (personagem1.idade + personagem2.idade  + personagem3.idade  + personagem4.idade) / 4 
+// console.log(`A média de idade entre os personagens é de: ${idadeMediaPersonagens} anos.`)
 
-const kiMedioPersonagens = (kiPersonagem1 + kiPersonagem2 + kiPersonagem3 + kiPersonagem4) / 4
-console.log(`A média do KI entre os personagens é de: ${kiMedioPersonagens} bilhões.`)
+// const kiMedioPersonagens = (personagem1.ki + personagem2.ki + personagem3.ki + personagem4.ki) / 4
+// console.log(`A média do KI entre os personagens é de: ${kiMedioPersonagens} bilhões.`)
 
+
+// Array vazio ------------------------------------------------------------------------------------------------
+
+const personagens  = []
+
+// Verificações -----------------------------------------------------------------------------------------------
+
+if(personagem1.poder === true && personagem2.poder === true && personagem3.poder === true && personagem4.poder === false){
+    personagens.push(personagem1, personagem2, personagem3, personagem4)
+    console.log(personagens)
+} else {
+    alert("O item não foi adicionado!")
+}
