aedf0999fcba75d1dd243749a56b71464665757c
Lucas Calabria <lucascalabria86@gmail.com>
2022-09-09 11:05:13 -0300

    Commit Semana 2

--- Yes-Sir/index.js
+++ Yes-Sir/index.js
@@ -2,37 +2,54 @@
 
 // ITEM: TÊNIS||SNEAKERS.
 
-let nomeTenis = "Air force 1";   //Exercício 1 e 2
-let cor = ["Branco"];
-let marca = "Nike";
-let tamanho = [38, 39, 40, 41, 42, 43, 44];
-let temEstoque = true;
-let preco = 950;
+let tenis1 = {
+    nome: "Air force 1",                //Adicionando Objetos
+    cor : "Branco",
+    marca: "Nike",
+    tamanho: [38, 39, 40, 41, 42, 43, 44],
+    temEstoque: true,
+    preco: 950
+};
 
-let nomeTenis2 = "Air Jordan 1 Chicago";   
-let cor2 = ["Branco", "Vermelho", "Preto"];    //Exercício 5
-let marca2 = "Nike";
-let tamanho2 = [38, 39, 40, 41, 42, 43, 44];
-let temEstoque2 = true;
-let preco2 = 1300;
+let tenis2 = {
+    nome : "Air Jordan 1 Chicago",   
+    cor : ["Branco", "Vermelho", "Preto"],    
+    marca : "Nike",
+    tamanho : [38, 39, 40, 41, 42, 43, 44],
+    temEstoque : true,
+    preco : 1300
+};
 
-let nomeTenis3 = "Dunk Low Retro";   
-let cor3 = ["Branco", "Preto"];
-let marca3 = "Nike";
-let tamanho3 = [38, 39, 40, 41, 42, 43, 44];
-let temEstoque3 = true;
-let preco3 = 1000;      
+let tenis3 = {
+    nome: "Dunk Low Retro",   
+    cor: ["Branco", "Preto"],
+    marca : "Nike",
+    tamanho : [38, 39, 40, 41, 42, 43, 44],
+    temEstoque : true,
+    preco : 1000
+};
 
-let mediaPreco = (preco + preco2 + preco3) / 3;     
-console.log("A média do preço é: ", mediaPreco);    ////Exercício 3
+let meusObjetos = [];               //Adicionando objetos ao array verificando booleanos
 
-console.log("Temos os 3 tênis em estoque ?", temEstoque && temEstoque2 && temEstoque3);  //Exercício 4
+if(tenis1.temEstoque){
+    meusObjetos.push(tenis1)
+} else{
+    alert(`O produto ${tenis1.nome} está em falta no estoque!`)
+};
 
-nomeUpper1 = nomeTenis.toUpperCase();
-console.log(`${nomeUpper1} \nCor: ${cor} \nMarca: ${marca} \nTamanhos: ${tamanho} \nTem no estoque? ${temEstoque} \nPreço: ${preco}`);
+if(tenis2.temEstoque){
+    meusObjetos.push(tenis2)
+} else{
+    alert(`O produto ${tenis2.nome} está em falta no estoque!`)
+};
 
-nomeUpper2 = nomeTenis2.toUpperCase();
-console.log(`${nomeUpper2} \nCor: ${cor2} \nMarca: ${marca2} \nTemanhos: ${tamanho2} \nTem no estoque? ${temEstoque2} \nPreço: ${preco2}`);
+if(tenis3.temEstoque){
+    meusObjetos.push(tenis3)
+} else{
+    alert(`O produto ${tenis3.nome} está em falta no estoque!`)
+};
 
-nomeUpper3 = nomeTenis3.toUpperCase();
-console.log(`${nomeUpper3} \nCor: ${cor3} \nMarca: ${marca3} \nTamanhos: ${tamanho3} \nTem no estoque? ${temEstoque3} \nPreço: ${preco3}`);
+console.log(meusObjetos);
+
+let mediaPreco = (tenis1.preco + tenis2.preco + tenis3.preco) / meusObjetos.length
+console.log("A média de preço dos tênis é", mediaPreco);   //Atualizando Média de Preço do produto
