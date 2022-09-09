f646481588b10ed4ddc100f790915f62824a8230
giova <giovanaftiburtino@gmail.com>
2022-09-09 14:29:36 -0300

    Semana 1 feita

--- projeto intro web/script.js
+++ projeto intro web/script.js
@@ -1,40 +1,68 @@
 //filme
 
-const titulo1 = "Parasita"
-const duracao1 = 132
-const ganhouOscar1 = true
-const atores1 = ["Song Kang-ho", "Sun-kyun Lee", "Cho Yeo-jeong"] 
+let filme1 = {
+titulo1: "Parasita",
+duracao1: 132,
+ganhouOscar1: true,
+atores1: ["Song Kang-ho", "Sun-kyun Lee", "Cho Yeo-jeong"]
+}
 
-const titulo2 = "Moonlight"
-const duracao2 = 111
-const ganhouOscar2 = true
-const atores2 = ["Mahersala Ali", "Naomie Harris", "Trevante Rhodes"]
+let filme2 = {
+titulo2: "Moonlight",
+duracao2: 111,
+ganhouOscar2: true,
+atores2: ["Mahersala Ali", "Naomie Harris", "Trevante Rhodes"]
+}
 
-const titulo3 = "Green book"
-const duracao3 = 130
-const ganhouOscar3 = true
-const atores3 = ["Viggo Mortensen", "Mahersala Ali", "Linda Cardellini"]
+let filme3 = {
+titulo3: "Green book",
+duracao3: 130,
+ganhouOscar3: true,
+atores3: ["Viggo Mortensen", "Mahersala Ali", "Linda Cardellini"]
+}
 
-const media = (duracao1 + duracao2 + duracao3) / 3
+const media = (filme1.duracao1 + filme2.duracao2 + filme3.duracao3) / 3
 console.log("A média de duração dos filmes é de:",(media), "minutos")
 
-const temOscar = ganhouOscar1 && ganhouOscar2 && ganhouOscar3
+const temOscar = filme1.ganhouOscar1 && filme2.ganhouOscar2 && filme3.ganhouOscar3
 console.log("Os filmes ganharam Oscars?", temOscar)
 
-console.log(`Título do filme: ${titulo1.toUpperCase()}
-Duração do filme: ${duracao1} minutos
-O filme tem oscar? ${ganhouOscar1}
-Nome dos atores: ${atores1}.
+console.log(`Título do filme: ${filme1.titulo1.toUpperCase()}
+Duração do filme: ${filme1.duracao1} minutos
+O filme tem oscar? ${filme1.ganhouOscar1}
+Nome dos atores: ${filme1.atores1}.
 `)
 
-console.log(`Título do filme: ${titulo2.toUpperCase()}
-Duração do filme: ${duracao2} minutos
-O filme tem oscar? ${ganhouOscar2}
-Nome dos Atores: ${atores2}.
+console.log(`Título do filme: ${filme2.titulo2.toUpperCase()}
+Duração do filme: ${filme2.duracao2} minutos
+O filme tem oscar? ${filme2.ganhouOscar2}
+Nome dos Atores: ${filme2.atores2}.
 `)
 
-console.log(`Título do filme: ${titulo3.toUpperCase()}
-Duração do filme: ${duracao3} minutos
-O filme tem oscar? ${ganhouOscar3}
-Nome dos Atores: ${atores3}.
+console.log(`Título do filme: ${filme3.titulo3.toUpperCase()}
+Duração do filme: ${filme3.duracao3} minutos
+O filme tem oscar? ${filme3.ganhouOscar3}
+Nome dos Atores: ${filme3.atores3}.
 `)
+
+const arrayFilmes = []
+
+if(filme1.ganhouOscar1 === true){
+    arrayFilmes.push(filme1)
+} else {
+    alert("Erro! O item não foi adicionado")
+}
+
+if(filme2.ganhouOscar2 === true){
+    arrayFilmes.push(filme2)
+} else {
+    alert("Erro! O item não foi adicionado")
+}
+
+if(filme3.ganhouOscar3 === true){
+    arrayFilmes.push(filme3)
+} else {
+    alert("Erro! O item não foi adicionado")
+}
+
+console.log("Lista com filmes que ganharam Oscars:", arrayFilmes)
