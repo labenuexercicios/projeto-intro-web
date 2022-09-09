79600baf882252773738190d34beafc477702924
LucasMarks8 <lucas.marks8@gmail.com>
2022-09-09 17:07:18 -0300

    semana 2 do projeto finalizada

--- index.js
+++ index.js
@@ -20,15 +20,14 @@ console.log(`O preço médio é: ${precoMedio.toFixed(2)}`);
 const temNoEstoque = disponivelEmEstoque1 && disponivelEmEstoque2 && disponivelEmEstoque3;
 console.log(`A verificação é: ${temNoEstoque}`);
 
-//semana 2
-//1)
+//5)
 const arrayreceitas1 = ["sorvete", " bolo", " panqueca", " pudim"]
 
 const arrayreceitas2 = ["mojito", " caipirinha", " batida", " margarita"]
 
 const arrayreceitas3 = ["sopa", " sorvete", " smoothie", " picolé"]
 
-//2)
+//6)
 console.log(`
     ${nome1.toUpperCase()}
     Preço em São Paulo: ${preco1} reais o kg
@@ -49,3 +48,51 @@ console.log(`
     É a mais consumida no Brasil? ${disponivelEmEstoque3}
     Possíveis receitas: ${arrayreceitas3}
 `)
+
+//semana 2
+//1)
+objeto1 = {
+    nome: "banana",
+    preço: 3.70,
+    disponivelEmEstoque: true
+}
+
+objeto2 = {
+    nome: "melancia",
+    preço: 4.00,
+    disponivelEmEstoque: false
+}
+
+objeto3 = {
+    nome: "melão",
+    preço: 6.00,
+    disponivelEmEstoque: false
+}
+
+//2)
+const arrayDeObjetos = []
+
+//3)
+arrayDeObjetos.push(objeto1, objeto2, objeto3)
+console.log(arrayDeObjetos)
+
+//4 e 5)
+if(objeto1.disponivelEmEstoque === true){
+    arrayDeObjetos.push(objeto1)
+} else {
+    alert("Item 1 não adicionado")
+}
+
+if(objeto2.disponivelEmEstoque === true){
+    arrayDeObjetos.push(objeto2)
+} else {
+    alert("Item 2 não adicionado")
+}
+
+if(objeto3.disponivelEmEstoque === true){
+    arrayDeObjetos.push(objeto3)
+} else {
+    alert("Item 3 não adicionado")
+}
+
+console.log(arrayDeObjetos)
