3b7bd2617ca78443996bf64b71c7a4d5b76343b5
Jessica <jessica.thamiresnunes@gmail.com>
2022-09-09 13:14:22 -0300

    Semana 1 ok

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+
+
+// Comida
+
+const suco1= 'Demolidor'
+const descricao1= "Produto 100% natural, embalado a vácuo e congelado."
+const ingrediente1=['Beterraba, cenoura, maça, gengibre, limão e chia.']
+const preco1= 6
+const isdetox1= true
+
+const suco2= "Mais Bela"
+const descricao2= "Produto 100% natural, embalado a vácuo e congelado."
+const ingrediente2=['Couve, maça, gengibre, limão e chia.']
+const preco2= 7
+const isdetox2= true
+
+const suco3= "Maria bonita"
+const descricao3= "Produto 100% natural, embalado a vácuo e congelado."
+const ingrediente3=['Couve, maça, pepino, limão e chia.']
+const preco3= 8
+const isdetox3= true
+
+const mediapreco = console.log(preco1+preco2+preco3/3)
+const boolAltaCaloria = console.log(isdetox1&&isdetox2&&isdetox3)
+
+console.log(suco1.toUpperCase())
+console.log(descricao1)
+console.log(ingrediente1)
+console.log(preco1)
+console.log(isdetox1)
+
+console.log(suco2.toUpperCase())
+console.log(descricao2)
+console.log(ingrediente2)
+console.log(preco2)
+console.log(isdetox2)
+
+console.log(suco3.toUpperCase())
+console.log(descricao3)
+console.log(ingrediente3)
+console.log(preco3)
+console.log(isdetox3)
