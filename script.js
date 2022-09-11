db91b828dc56709c7bb334c745109a1062aaca17
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2022-09-11 19:31:43 -0300

    semana1e2-BarbosaC-Sanderson

---
+++ script.js
@@ -0,0 +1,33 @@
+const nome1 = "Seya"
+const idade1 = 16
+const cavaleiroAtena1 = true 
+const golpes1 = ["meteoro de pagaso", " cometa de pegaso", " turbilhão de pagaso"]
+
+const nome2 = "Hyoga"
+const idade2 = 16
+const cavaleiroAtena2 = true
+const golpes2 = ["círculo de gelo", " trovão aurora", " pó de diamante"]
+
+const nome3 = "Shiryu"
+const idade3 = 15
+const cavaleiroAtena3 = true 
+const golpes3 = ["cólera do dragão", " dragão voador", " dragão nascente"]
+
+const cavaleirosDeAthena = cavaleiroAtena1 && cavaleiroAtena2 && cavaleiroAtena3
+
+// console.log(cavaleirosDeAthena);
+
+// console.log(`Nome = ${nome1.toUpperCase()}
+// Idade = ${idade1}
+// é um cavaleiro de atena = ${cavaleiroAtena1}
+// Golpes = ${golpes1}`);
+
+// console.log(`Nome = ${nome2.toUpperCase()}
+// Idade = ${idade2}
+// é um cavaleiro de atena = ${cavaleiroAtena2}
+// Golpes = ${golpes2}`);
+
+// console.log(`Nome = ${nome3.toUpperCase()}
+// Idade = ${idade3}
+// é um cavaleiro de atena = ${cavaleiroAtena3}
+// Golpes = ${golpes3}`);
