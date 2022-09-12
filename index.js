158f9794a26c39d5f2d5b2829b00023de770205b
MariaEugenia <eugeniialima@gmail.com>
2022-09-11 21:30:16 -0300

    Segunda semana projeto

--- index.js
+++ index.js
@@ -1,46 +1,65 @@
-const comida = "Hambúrguer de costela"
-let preco = 29.90
-const ingredientes = ["Pão australiano", "Costelinha", "Bacon", "Queijo cheddar", "Cebola caramelizada"]
-const vegano = false
 
-const comida2 = "Hambúrguer gourmet"
-let preco2 = 28.90
-const vegano2 = false
-const ingredientes2 = ["Pão brioche", "Carne bovina", "Bacon", "Queijo cheddar", "Cebola caramelizada"]
+addEventListener('load', (event) => {
+    const menu = [
+        {
+            'comida': "Hambúrguer de costela",
+            'preco': 29.90,
+            'ingredientes': ["Pão australiano", "Costelinha", "Bacon", "Queijo cheddar", "Cebola caramelizada"],
+            'vegano': false
+        }, {
+            'comida': "Hambúrguer gourmet",
+            'preco': 28.90,
+            'ingredientes': ["Pão brioche", "Carne bovina", "Bacon", "Queijo cheddar", "Cebola caramelizada"],
+            'vegano': false
+        }, {
+            'comida': "Hambúrguer de frango",
+            'preco': 25.90,
+            'ingredientes': ["Pão brioche", "Filé de frango", "Alface", "Tomate", "Queijo prato"],
+            'vegano': false
+        }, {
+            'comida': "Veggie hambúrguer",
+            'preco': 26.90,
+            'ingredientes': ["Pão brioche", "Hambúrguer de falafel", "Alface", "Tomate", "Cebola"],
+            'vegano': true
+        }]
 
-const comida3 = "Hambúrguer de frango"
-let preco3 = 25.50
-let ingredientes3 = ["Pão brioche", "Filé de frango", "Alface", "Tomate", "Queijo prato"]
-const vegano3 = false
+    const vegano = []
+    menu.forEach(item => {
+        if (item.vegano)
+            vegano.push(item)
+        else
+            alert(`O item ${item.comida} não foi adicionado`)
 
-const comida4 = "Veggie hambúrguer"
-let preco4 = 24.90
-let ingredientes4 = ["Pão brioche", "Hambúrguer de falafel", "Alface", "Tomate", "Cebola"]
-const vegano4 = true
+    });
 
-const mediaPreco = (preco + preco2 + preco3 + preco4) / 4
-console.log(`A média dos preço da lanchonete é R$${mediaPreco}`)
+    console.log(vegano)
 
-const verificarVegano = (vegano && vegano2 && vegano3 && vegano4)
-console.log(`Todos são veganos? ${verificarVegano}.`)
+});
 
-console.log(`${comida.toUpperCase()} 
-R$ ${preco}
-Ingredientes:${ingredientes}
-É vegano?${vegano}`)
 
-console.log(`${comida2.toUpperCase()} 
-R$${preco2}
-Ingredientes:${ingredientes2}
-É vegano?${vegano2}`)
+// const mediaPreco = (preco + preco2 + preco3 + preco4) / 4
+// console.log(`A média dos preço da lanchonete é R$${ mediaPreco }`)
 
-console.log(`${comida3.toUpperCase()} 
-R$${preco3}
-Ingredientes:${ingredientes3}
-É vegano?${vegano3}`)
+// const verificarVegano = (vegano && vegano2 && vegano3 && vegano4)
+// console.log(`Todos são veganos ? ${ verificarVegano }.`)
 
-console.log(`${comida4.toUpperCase()} 
-R$${preco4}
-Ingredientes:${ingredientes4}
-É vegano?${vegano4}`)
+// console.log(`${ comida.toUpperCase() }
+// R$ ${preco}
+// Ingredientes:${ingredientes}
+// É vegano?${vegano}`)
+
+// console.log(`${comida2.toUpperCase()}
+// R$${preco2}
+// Ingredientes:${ingredientes2}
+// É vegano?${vegano2}`)
+
+// console.log(`${comida3.toUpperCase()}
+// R$${preco3}
+// Ingredientes:${ingredientes3}
+// É vegano?${vegano3}`)
+
+// console.log(`${comida4.toUpperCase()}
+// R$${preco4}
+// Ingredientes:${ingredientes4}
+// É vegano?${vegano4}`)
 
