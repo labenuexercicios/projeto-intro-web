17edd0cffc3c597035fc56e7da7eeddb65248ce0
Emerson-M-de-Lira <emerson.lira.monteiro@gmail.com>
2022-09-11 21:33:29 -0300

    introweb

---
+++ script.js
@@ -0,0 +1,64 @@
+﻿
+
+const nome1 = 'manjubinha';
+const descrição1 = 'empanado na farinha de rosca';
+const  ingredientes1 = 'manjubinha, leite, ovo, farinha de rosca';
+const categoria1 = 'Tira gosto';
+const vegetariano1 = false;
+let preco1 = 35;
+const menu1=[]
+
+
+const nome2 = 'Catumarão';
+const descrição2 =' camarão empanado com catupery';
+const ingredientes2 = 'camarao rosinha, catupery, leite, ovo,farinha de rosca';
+const categoria2 = 'Petisco';
+const vegetariano2 = false;
+let preco2 = 50;
+const menu2=[]
+
+
+const nome3 = 'mandioca';
+const descrição3 ='  mandioca';
+const ingredientes3 = 'mandioca';
+const categoria3 = 'Porçao';
+const vegetariano3 = true;
+let preco3 = 45;
+const menu3=[]
+
+const totalDePreco = preco1 && preco2 && preco3;
+console.log(totalDePreco)
+
+
+let menu = 
+[{
+    nome1: 'manjubinha',    
+    descrição1: 'empanado na farinha de rosca',
+    ingredientes1: 'manjubinha, leite, ovo, farinha de rosca',
+    categoria1: 'Tira gosto',
+    vegetariano1: false,
+    preco1: 35,
+},
+    {
+    nome2: 'Catumarão',
+    descrição2: 'camarão empanado com catupery',
+    ingredientes2: 'camarao rosinha, catupery, leite, ovo,farinha de rosca',
+    categoria2: 'Petisco',
+    vegetariano2: false,
+    preco2: 50,
+    },
+{
+    nome3: 'mandioca',
+    Descrição3: 'mandioca',
+    ingredientes3: 'mandioca',
+    categoria3: 'Porçao',
+    vegetariano3: true,
+    preco3: 45
+}]
+
+function divisor (params=30){
+    console.log()
+}
+*/
+(n1,n2,n3)=>n1*n2/n3
+console.log()
