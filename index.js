b9474e3eb20d0f55a795915a86ec5f37316bd967
Aureana <111321782+Aureana@users.noreply.github.com>
2022-09-13 17:18:53 -0300

    Adicionando Projeto - Semana 1

--- index.js
+++ index.js
@@ -20,26 +20,27 @@ const verificaMARCA = verificouMarca1 && verificouMarca2 && verificouMarca3   //
 console.log("Verificou marca verdadeiro?", verificaMARCA)   //que checa se todos os valores de variáveis booleanas criadas até aqui são verdadeiras.
 
 
-const copiaMarca1 = marca1.slice()
-const copiaMarca2 = marca2.slice() 
-const copiaMarca3 = marca3.slice()   
+const copiaMarca1 = marca1.slice()       // criei os slices p mexer nos arrays de cópia, ñ nos originais
+const copiaMarca2 = marca2.slice()       //neste caso eu mostro os arrays originais pq o projeto ñ pede,
+const copiaMarca3 = marca3.slice()       //porém eu fiz a cópia p exibir os resultados pedidos.
 //console.log("Copia do marca:", marca1)
 //console.log("Copia do marca:", marca2)
 //console.log("Copia do marca:", marca3)
-console.log("Tipo de roupa que escolheu: "+tipoRoupaAcademia1.toLocaleUpperCase()+"\nQuantidade que deseja: "+quantidade1+" \nMarca desejada: "+verificouMarca1+" \nQual marca que pediu: "+copiaMarca1.splice(0,1)+" ") 
+console.log(tipoRoupaAcademia1.toUpperCase() + "\nQuantidade que deseja: "+quantidade1+" \nMarca desejada: "+verificouMarca1+" \nQual marca que pediu: "+copiaMarca1.splice(0,1)+" ") 
 console.log("Marcas Existentes:", marca1)
 
-console.log("Tipo de roupa que escolheu: "+tipoRoupaAcademia2.toLocaleUpperCase()+"\nQuantidade que deseja: "+quantidade2+" \nMarca desejada: "+verificouMarca2+" ") 
+console.log(tipoRoupaAcademia2.toUpperCase() + "\nQuantidade que deseja: "+quantidade2+" \nMarca desejada: "+verificouMarca2+" ") 
 console.log("Marcas Existentes:", marca2)
 
-console.log("Tipo de roupa que escolheu: "+tipoRoupaAcademia3.toLocaleUpperCase()+"\nQuantidade que deseja: "+quantidade3+" \nMarca desejada: "+verificouMarca3+" ") 
+console.log(tipoRoupaAcademia3.toUpperCase() + "\nQuantidade que deseja: "+quantidade3+" \nMarca desejada: "+verificouMarca3+" ") 
 console.log("Marcas Existentes:", marca3)
 
-//.toLocaleUpperCase()
-/*  sobre a avariavel COPIA MARCA 1
-tem que fazer uma copia para mostrar só uma valor de marca, quando perguntar qual marcar escolheu
-tem que mostrar só uma, ai chama a copia, pq vai excluir as outras com o splice
-pq senão quando der um splice, ele modifica a variavel original e depois n mostra ela completa,
-quando pedi p perguntar quais marcas existem
 
+/*  sobre a avariavel COPIA MARCA 1
+tem que fazer uma copia para mostrar só um valor de marca, quando perguntar qual marcar escolheu
+tem que mostrar só uma, ai chama a copia, pq vai excluir as outras com o slice
+pq senão quando der um slice, ele modifica a variavel original e depois n mostra ela completa,
+quando pedi p perguntar quais marcas existem
 */
+
+
