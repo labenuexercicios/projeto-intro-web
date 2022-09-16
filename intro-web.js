d7c76af95eb3bc1858d6852119b5d5ed902e437d
LuizHenriqueMarinho <luizhenriquemarinhofga@gmail.com>
2022-09-16 12:08:34 -0300

    projeto atualizado

--- intro-web.js
+++ intro-web.js
@@ -1,5 +1,5 @@
 // time semana 1
-
+//item 1 e 2 
 const time_1 = "flamengo"
 const maiorRival_1 = "vasco"
 const temMundial_1 = true
@@ -27,6 +27,7 @@ const brasileirao_3 = 1
 const copaDoBrasil_3 = 1
 const titulos_3 = ["libertadores: 0", "brasileirão: 1", "copa do brasil: 1"] //=> na ordem: liberta, brasileirão e copa do brasil 
 
+//semana 1 item 3
 //total de títulos
 const total_1 = libertadores_1 + brasileirao_1 + copaDoBrasil_1
 const total_2 = libertadores_2 + brasileirao_2 + copaDoBrasil_2
@@ -34,8 +35,11 @@ const total_3 = libertadores_3 + brasileirao_3 + copaDoBrasil_3
 
 //media dos títulos dos 3 times 
 const media = (total_1 + total_2 + total_3) / 3
+console.log("SEMANA 1 ITEM 3:")
 console.log("a média de títulos dos times é: ", media)
 
+// semana 1 item 4
+console.log("SEMANA 1 ITEM 4:")
 //verifica se todos já foram rebaixados
 const todosRebaixados = (foiRebaixado_1 && foiRebaixado_2 && foiRebaixado_3)
 console.log("todos os times já forma rebaixados?", todosRebaixados)
@@ -45,6 +49,8 @@ const todosTemMundial = (temMundial_1 && temMundial_2 && temMundial_3)
 console.log("todos os times têm mundial?", todosTemMundial)
 
 //resumo dos times
+//semana 1 item 5 e 6
+console.log("SEMANA 1 ITEM 5 e 6:")
 console.log(
     "time: " + time_1.toUpperCase() +
     "\nmaior rival: " + maiorRival_1.toUpperCase() +
@@ -75,7 +81,8 @@ console.log(
     "\nquantas copas do brasil o time tem? " + copaDoBrasil_3 +
     "\nresumo de títulos time: " + titulos_3)
 
-//// transformar em objeto semana 2
+//// transformar em objeto 
+//semana 2 item 1
 const objeto_1 =
 {
     time: "flamengo",
@@ -112,13 +119,19 @@ const objeto_3 =
     titulos: ["libertadores: 0", "brasileirão: 1", "copa do brasil: 1"]
 }
 
+// semana 2 item 2
+
 const arrayObjetos = [] // = [...objeto_1.titulos , ...objeto_2.titulos , ...objeto_3.titulos]
+// semana 2 item 3
+console.log("SEMANA 2 ITEM 3:")
 arrayObjetos.push(objeto_1)
 arrayObjetos.push(objeto_2)
 arrayObjetos.push(objeto_3)
 console.log(arrayObjetos)
 
 // condição tem mundial
+// semana 2 item 4 e 5
+console.log("SEMANA 2 ITEM 4 e 5:")
 let arrayObjetosIfElse = []
 console.log("adicione somente os times que tem mundial: ")
 if (temMundial_1 === true) {
@@ -126,7 +139,7 @@ if (temMundial_1 === true) {
     console.log(" o " + objeto_1.time + " tem mundial")
 }
 else {
-    console.log("**ALERT** o " + objeto_1.time + " não tem mundial")
+    alert("**ALERT** o " + objeto_1.time + " não tem mundial")
 }
 
 if (temMundial_2 === true) {
@@ -134,7 +147,7 @@ if (temMundial_2 === true) {
     console.log(" o " + objeto_2.time + " tem mundial")
 }
 else {
-    console.log("**ALERT** o " + objeto_2.time + " não tem mundial")
+    alert("**ALERT** o " + objeto_2.time + " não tem mundial")
 }
 
 if (temMundial_3 === true) {
@@ -142,67 +155,88 @@ if (temMundial_3 === true) {
     console.log(" o " + objeto_3.time + " tem mundial")
 }
 else {
-    console.log("**ALERT** o " + objeto_3.time + " não tem mundial")
+    alert("**ALERT** o " + objeto_3.time + " não tem mundial")
 }
 
 console.log("informações dos times que possuem mundial: ")
 console.log(arrayObjetosIfElse)
 
-/////semana 3 loop que guarda os array dos objetos em uma única variável
-/////1
+/////semana 3 
+////item 1 loop que guarda os array dos objetos em uma única variável
+console.log("SEMANA 3 ITEM 1:")
 let i = 0 
 let novaTitulos_1 = "" //variavel que vai receber os arrays no loop
-for (i = 0; i < 3; i++) {
-    novaTitulos_1 = novaTitulos_1 + objeto_1.titulos[i]
-    if (i < 2)
-        novaTitulos_1 = novaTitulos_1 + ", " //condição para não acrescentar virgula no fim
+for (i in objeto_1) 
+{
+    novaTitulos_1 = `${objeto_1.titulos}` //condição para não acrescentar virgula no fim
 }
 console.log("\"" + novaTitulos_1 + "\"\n" + "tipo da variável: " + typeof (novaTitulos_1))
 
 /////2 verificar se é isso mesmo
-
-for (i = 0; i < titulos_1.length; i++) ////// se for para fazer a iteração apenas pro array
+console.log("SEMANA 3 ITEM 2:")
+let iteracao = 1
+for (let elementos in objeto_1) ////// se for para fazer a iteração apenas pro array
 {
-    console.log(titulos_1[i])
-    console.log("iteraçao" + (i + 1))
+    console.log(`${elementos}: ${objeto_1[elementos]}`)
+    console.log("iteraçao " + iteracao++)
 }
 
-for (i = 0; i < titulos_2.length; i++) ////// se for para fazer a iteração apenas pro array
-{
-    console.log(titulos_2[i])
-    console.log("iteraçao" + (i + 1))
+iteracao = 1
+
+for (elementos in objeto_2) ////// se for para fazer a iteração apenas pro array
+{ 
+    console.log(`${elementos}: ${objeto_2[elementos]}`)
+    console.log("iteraçao " + iteracao++)
 }
 
-for (i = 0; i < titulos_3.length; i++) ////// se for para fazer a iteração apenas pro array
+iteracao = 1
+
+for (elementos in objeto_3) ////// se for para fazer a iteração apenas pro array
 {
-    console.log(titulos_3[i])
-    console.log("iteraçao" + (i + 1))
+    console.log(`${elementos}: ${objeto_3[elementos]}`)
+    console.log("iteraçao " + iteracao++)
 }
 
 //////3 função 
+console.log("SEMANA 3 ITEM 3:")
+
 function objetoString(objeto)
-{
-    return console.log(objeto);
+{   
+    text = []
+    for(elementos in objeto)
+    { 
+        text += `${elementos}: ${objeto[elementos]}, ` 
+    }
+    return text
 }
 
-objetoString(objeto_1)
-objetoString(objeto_2)
-objetoString(objeto_3)
+console.log(objetoString(objeto_1))
+console.log(objetoString(objeto_2))
+console.log(objetoString(objeto_3))
+
 
 //// 4 (ctrl + f) pesquisa no site
-
+console.log("SEMANA 3 ITEM 4:")
 let pesquisa = Number(0) // caso n encontre resultado, retorna um alert
 function arrayObjetosToStrings(arrayObjetosFunction, stringFunction)
 {
+    pesquisa = +0
     for(i=0; i<arrayObjetosFunction.length; i++) 
     {    
         if(arrayObjetosFunction[i].time === stringFunction)
         {
-            return console.log("o resultado da pesquisa foi: ", arrayObjetosFunction[i])
-            pesquisa === Number(1)
+            pesquisa = Number(1)
+            console.log("o resultado da pesquisa foi: ", arrayObjetosFunction[i])
+            
         }
     }
-    return console.log("ALERT item não encontrado")
+    if(pesquisa !== 1)
+    {
+    return alert("ALERT item não encontrado")// depois do retunr, a função para de ser executada 
+    }
 }
+
 let string = prompt("digite a string")
 arrayObjetosToStrings(arrayObjetos, string)
+
+
