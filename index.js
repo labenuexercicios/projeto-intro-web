1a4272cf878aa6a0047916d33453d02a9f78a9dd
Mjfreedom <mbuzina@gmail.com>
2022-09-17 14:55:18 -0300

    Finalmente consegui o EX1 e 2! Uhuuuuuuuuuuuu

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Título ---------------------------
-console.log("Lista de Pets da Casa")
+//console.log("Lista de Pets da Casa")
 
 // Brisa ----------------------------
 const nomePet1 = {
@@ -8,7 +8,6 @@ const nomePet1 = {
     especie: "Canina",
     raca: "Pastor Alemão",
     vacina: confirm(`Brisa tomou vacina?`),
-    vermifugo: confirm(`Brisa tomou vermifugo?`),
     arrayBriquedos: ["Bolinha", " ossinho."]
 };
 
@@ -21,7 +20,6 @@ const nomePet2 = {
     especie: "Felina",
     raca: "SRD",
     vacina: confirm(`Janis tomou vacina?`),
-    vermifugo: confirm(`Janis tomou vermifugo?`),
     arrayBriquedos: ["Caixa de papelão", " arranhador de tecido", " ervinha de gato."]
 };
 
@@ -34,7 +32,6 @@ const nomePet3 = {
     especie: "Canina",
     raca: "Shin Tzu",
     vacina: confirm(`Lua tomou vacina?`),
-    vermifugo: confirm(`Lua tomou vermifugo?`),
     arrayBriquedos: ["Bolinha", " ossinho", " orelha de boi", " ursinho."]
 }; 
 
@@ -46,8 +43,7 @@ const nomePet4 = {
     idade: 7,
     especie: "Felina",
     raca: "SRD",
-    vacina: confirm(`Marilyn tomou vacina?`),
-    vermifugo: confirm(`Marilyn tomou vermifugo?`),   
+    vacina: confirm(`Marilyn tomou vacina?`), 
     arrayBriquedos: ["Caixa de papelão", " barbante enrolado", " erva de gato", " bolinha de lã."]
 };
 
@@ -60,37 +56,93 @@ console.log(`Média de Idade dos meus pets: ${mediaPets}`)
 
 //verificar booleanos
 const verificarVacinas = nomePet1.vacina && nomePet2.vacina && nomePet3.vacina && nomePet4.vacina
-const verificarVermifugo = nomePet1.vermifugo && nomePet2.vermifugo && nomePet3.vermifugo && nomePet4.vacina
-
-console.log(`Todas vacinadas? ${verificarVacinas}
-Todas vermifugadas? ${verificarVermifugo}
-`)
+console.log(`Todas vacinadas? ${verificarVacinas}`)
 
 //array pra guardar os objetos
 
 const todosPets = []
 
-if ((verificarVacinas === true) && (verificarVermifugo === true)) {
-    todosPets.push(nomePet1, nomePet2, nomePet3, nomePet4)
-    console.log(`Todos os pets foram vacinados e vermifugados: ${todosPets}`)
-} else if ((verificarVacinas === false && verificarVermifugo === false)) {
-    alert(`Os pets não foram vacinados e nem vermifugados!`)
-    console.log(`Os pets não foram vacinados e nem vermifugados!`)
+if (nomePet1.vacina) {
+    todosPets.push(nomePet1)
+    //console.log(`Brisa está vacinada.`)
 } else {
-    alert(`Os pets não foram vacinados e nem vermifugados!`)
-    console.log(`Os pets não foram vacinados e nem vermifugados!`)
+    alert(`Brisa não foi vacinada!`)
 }
+
+if (nomePet2.vacina) {
+    todosPets.push(nomePet2)
+    //console.log(`Janis está vacinada.`)
+} else {
+    alert(`Janis não foi vacinada!`)
+}
+
+if (nomePet3.vacina) {
+    todosPets.push(nomePet3)
+    //console.log(`Lua está vacinada.`)
+} else {
+    alert(`Lua não foi vacinada!`)
+}
+
+if (nomePet4.vacina) {
+    todosPets.push(nomePet4)
+    //console.log(`Marilyn está vacinada.`)
+} else {
+    alert(`Marilyn não foi vacinada!`)
+}
+
+console.log(todosPets)
 
 //valores da propriedade array do objeto em uma mesma string
 
-let stringPet1 = nomePet1.arrayBriquedos.join()
-let stringPet2 = nomePet2.arrayBriquedos.join()
-let stringPet3 = nomePet3.arrayBriquedos.join()
-let stringPet4 = nomePet4.arrayBriquedos.join()
+console.log("=====EX01 - NOVO ARRAY STRING=====")
 
-console.log(stringPet1)
-console.log(stringPet2)
-console.log(stringPet3)
-console.log(stringPet4)
+// let stringPet1 = nomePet1.arrayBriquedos.join()
+// let stringPet2 = nomePet2.arrayBriquedos.join()
+// let stringPet3 = nomePet3.arrayBriquedos.join()
+// let stringPet4 = nomePet4.arrayBriquedos.join()
+
+// console.log(stringPet1)
+// console.log(stringPet2)
+// console.log(stringPet3)
+// console.log(stringPet4)
+
+let arrayStringPets = []
+
+for (let i = 0; i< todosPets.length; i++){
+    arrayStringPets.push(todosPets[i].nome) 
+    arrayStringPets.push(todosPets[i].idade)
+    arrayStringPets.push(todosPets[i].especie)
+    arrayStringPets.push(todosPets[i].raca)
+    arrayStringPets.push(todosPets[i].vacina)
+    arrayStringPets.push(todosPets[i].arrayBriquedos.join())
+}
+
+console.log(arrayStringPets)
+
+console.log("=====EX02 - LOOP ARRAY STRING=====")
+
+//Ainda no relatório, altere-o para que ele seja criado utilizando um laço. Ou seja, você não deve mais imprimir 
+// individualmente cada item do relatório. Cada item deve ser exibido a partir de uma iteração do laço. 
+// Para testar, adicione mais um item ao array de objetos, e verifique se ele é exibido corretamente.
+
+let stringPets = []
+
+    for (let i in todosPets){
+        stringPets.push(`${Number(i)+1}: ${todosPets[i].arrayBriquedos}`)
+        console.log("Nome:",todosPets[i].nome.toUpperCase(),"\nIdade:", 
+        todosPets[i].idade, "\nEspécie:",todosPets[i].especie,"\nRaça:", 
+        todosPets[i].raca,"\nBrinquedos:", todosPets[i].arrayBriquedos.join())
+    }
+
+//Crie uma função que receba como parâmetro um objeto, e devolva a string 
+//do relatório com os dados do objeto.
+
+console.log("=====EX03 - LOOP ARRAY STRING=====")
 
 
+
+
+
+//Crie uma função que recebe um array de objetos e uma string. Esta função deve retornar um objeto, e o objeto 
+// retornado deve possuir apenas os itens que tenham o nome/título igual à string passada como parâmetro. 
+// Caso não exista um item, exiba um ALERT indicando que nenhum item foi encontrado.
