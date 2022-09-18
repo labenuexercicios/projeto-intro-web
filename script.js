dc4e8df0159e74596c015a16cfda3d0162619842
PedroMagCoder <pedromagno999@gmail.com>
2022-09-18 19:44:04 -0300

    Semana2-PedroMagno-Barbosa-D

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-// semana 1 //
+// semana 2 //
 
 // Pet's
 
-const pet = "Ozzy";
+
+const nome = "Ozzy";
 let idade = 8;
 const dog = true;
+let alimentacao =  [ "Ração" , "Arroz" , "linguiça" ] ;
 
-const pet2 = "Erick";
+const nome2 = "Erick";
 let idade2 = 15;
 const dog2 = true;
+let alimentacao2 =  [ "Ração" , "Arroz" , "linguiça" , "Palitinhos" ] ;
 
-const pet3 = "Alice";
+const nome3 = "Alice" ;
 let idade3 = 5;
 const dog3 = false;
+let alimentacao3 =  [ "Ração"] ;
 
-let media = (idade + idade2 + idade3) / 3;
 
-console.log(media)
+console.log (`nome: ${nome.toLocaleUpperCase()} \n idade:${idade} \n é um cachorro: ${dog} \n gosta de comer: ${alimentacao}`);
+console.log (`nome: ${nome2.toLocaleUpperCase()} \n idade:${idade2} \n é um cachorro: ${dog2} \n gosta de comer: ${alimentacao2}`);
+console.log (`nome: ${nome3.toLocaleUpperCase()} \n idade:${idade3} \n é um cachorro: ${dog3} \n gosta de comer: ${alimentacao3}`);
 
-const verificarDog = dog && dog2 && dog3;
-console.log(verificarDog)
 
