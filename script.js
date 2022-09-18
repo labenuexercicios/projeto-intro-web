7062c297c885706a72ba59f92974610872d2172b
PedroMagCoder <pedromagno999@gmail.com>
2022-09-18 18:37:36 -0300

    Semana1-PedroMagno-Bardosa-D

---
+++ script.js
@@ -0,0 +1,23 @@
+// semana 1 //
+
+// Pet's
+
+const pet = "Ozzy";
+let idade = 8;
+const dog = true;
+
+const pet2 = "Erick";
+let idade2 = 15;
+const dog2 = true;
+
+const pet3 = "Alice";
+let idade3 = 5;
+const dog3 = false;
+
+let media = (idade + idade2 + idade3) / 3;
+
+console.log(media)
+
+const verificarDog = dog && dog2 && dog3;
+console.log(verificarDog)
+
