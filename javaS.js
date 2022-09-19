c5ec1981f68a93427fb6d611d48b8667063ee911
Will <acamedhf@gmail.com>
2022-09-19 18:50:26 -0300

    semana 4 OK!

--- javaS.js
+++ javaS.js
@@ -19,10 +19,19 @@ const cachorro = {
     array: ["O olfato de um cachorro é 1 milhão de vezes melhor do que o dos humanos", "se não for castrada, uma pode ter 66 filhotes em 6 anos", "são onívoros, eles precisam comer mais do que apenas carne"]
 }
 
-const adc = []
+// const escolha = prompt("Digite " + "\"Sim\"" + " pra adcionar os objetos ao array de objetos ou " + "\"Não\"" + " para não adcionar os objeotos no array de objetos").toUpperCase()
 
-adc.push(leao)
-adc.push(urso)
-adc.push(cachorro)
+// const variavel = ("SIM" === escolha)  //solução mais legal 
 
-console.log(adc)
+const variavel = true
+
+if (variavel) {
+    const adc = []
+    adc.push(leao)
+    adc.push(urso)
+    adc.push(cachorro)
+    console.log(adc)
+} else {
+    alert("Itens não foram adcionados")
+}
+
