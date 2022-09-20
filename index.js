2f7baa3962f5cdc0e882eed8cf9562aa6d0560de
Mjfreedom <mbuzina@gmail.com>
2022-09-20 17:04:19 -0300

    Projeto 3 Finalizo

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const nomePet1 = {
     especie: "Canina",
     raca: "Pastor Alemão",
     vacina: confirm(`Brisa tomou vacina?`),
-    arrayBriquedos: ["Bolinha", " ossinho."]
+    arrayBriquedos: ["Bolinha", "orelha de boi", " ossinho."]
 };
 
 //console.log(nomePet1)
@@ -106,18 +106,27 @@ console.log("=====EX01 - NOVO ARRAY STRING=====")
 // console.log(stringPet3)
 // console.log(stringPet4)
 
-let arrayStringPets = []
-
 for (let i = 0; i< todosPets.length; i++){
-    arrayStringPets.push(todosPets[i].nome) 
-    arrayStringPets.push(todosPets[i].idade)
-    arrayStringPets.push(todosPets[i].especie)
-    arrayStringPets.push(todosPets[i].raca)
-    arrayStringPets.push(todosPets[i].vacina)
-    arrayStringPets.push(todosPets[i].arrayBriquedos.join())
+    console.log(todosPets[i].nome)
+    console.log(todosPets[i].idade)
+    console.log(todosPets[i].especie)
+    console.log(todosPets[i].raca)
+    console.log(todosPets[i].vacina)
+    console.log(todosPets[i].arrayBriquedos.join())
 }
 
-console.log(arrayStringPets)
+// let arrayStringPets = []
+
+// for (let i = 0; i< todosPets.length; i++){
+//     arrayStringPets.push(todosPets[i].nome) 
+//     arrayStringPets.push(todosPets[i].idade)
+//     arrayStringPets.push(todosPets[i].especie)
+//     arrayStringPets.push(todosPets[i].raca)
+//     arrayStringPets.push(todosPets[i].vacina)
+//     arrayStringPets.push(todosPets[i].arrayBriquedos.join())
+// }
+
+// console.log(arrayStringPets)
 
 console.log("=====EX02 - LOOP ARRAY STRING=====")
 
@@ -125,24 +134,75 @@ console.log("=====EX02 - LOOP ARRAY STRING=====")
 // individualmente cada item do relatório. Cada item deve ser exibido a partir de uma iteração do laço. 
 // Para testar, adicione mais um item ao array de objetos, e verifique se ele é exibido corretamente.
 
-let stringPets = []
-
-    for (let i in todosPets){
-        stringPets.push(`${Number(i)+1}: ${todosPets[i].arrayBriquedos}`)
-        console.log("Nome:",todosPets[i].nome.toUpperCase(),"\nIdade:", 
-        todosPets[i].idade, "\nEspécie:",todosPets[i].especie,"\nRaça:", 
-        todosPets[i].raca,"\nBrinquedos:", todosPets[i].arrayBriquedos.join())
+for(pet of todosPets){
+    for (let i in pet){
+        console.log(`${i}: ${pet[i]}`)
     }
+    //console.log("----------------------");
+}
+
+// let stringPets = []
+
+//     for (let i in todosPets){
+//         stringPets.push(`${Number(i)+1}: ${todosPets[i].arrayBriquedos}`)
+//         console.log("Nome:",todosPets[i].nome.toUpperCase(),"\nIdade:", 
+//         todosPets[i].idade, "\nEspécie:",todosPets[i].especie,"\nRaça:", 
+//         todosPets[i].raca,"\nBrinquedos:", todosPets[i].arrayBriquedos.join())
+//     }
 
 //Crie uma função que receba como parâmetro um objeto, e devolva a string 
 //do relatório com os dados do objeto.
 
-console.log("=====EX03 - LOOP ARRAY STRING=====")
+console.log("=====EX03 - FUNÇÃO RECEBE OBJETO=====")
 
+//         console.log("Nome:",todosPets[i].nome.toUpperCase(),"\nIdade:", 
+//         todosPets[i].idade, "\nEspécie:",todosPets[i].especie,"\nRaça:", 
+//         todosPets[i].raca,"\nBrinquedos:", todosPets[i].arrayBriquedos.join())
+//     }
 
+// const comMap = copiaTodosPets.map((novoPetz) => {
+//     return novoPetz.join()
+// })
 
+// console.log(copiaTodosPets(todosPets))
 
+const funcaoPets = (novoPets) => {
+    for (let i = 0; i <todosPets.length; i++){
+        console.log(`${novoPets[i].nome}, ${novoPets[i].idade}, ${novoPets[i].especie}, ${novoPets[i].raca}, ${novoPets[i].vacina}, ${novoPets[i].arrayBriquedos}`)
+    }  
+}
 
-//Crie uma função que recebe um array de objetos e uma string. Esta função deve retornar um objeto, e o objeto 
-// retornado deve possuir apenas os itens que tenham o nome/título igual à string passada como parâmetro. 
-// Caso não exista um item, exiba um ALERT indicando que nenhum item foi encontrado.
+funcaoPets(todosPets)
+
+//Crie uma função que recebe um array de objetos e uma string. 
+//Esta função deve retornar um objeto, e o objeto 
+// retornado deve possuir apenas os itens que tenham o nome/título igual 
+//à string passada como parâmetro. Caso não exista um item, exiba um 
+//ALERT indicando que nenhum item foi encontrado.
+
+console.log("=====EX04 - FUNÇÃO RECEBE OBJETO=====")
+
+const funcaoDupla = (objeto, buscaItem) =>{
+    for (let i in todosPets){
+        if (todosPets[i].nome === buscaItem){
+            return objeto[i]
+            // Vazio
+        // }else if(todosPets[i].idade === buscaItem){
+        //     //Vazio
+        // }else if(todosPets[i].especie === buscaItem){ 
+        //     //Vazio
+        // }else if(todosPets[i].raca === buscaItem){
+        //     //Vazio
+        // }else if(todosPets[i].vacina === buscaItem){
+        //     //Vazio
+        // }else if(todosPets[i].arrayBriquedos === buscaItem){
+        //     //Vazio
+        // }else{
+            
+        }}
+        return alert(`Nenhum pet foi encontrado!`) 
+    }
+
+    console.log(funcaoDupla(todosPets, "BRISA"))
+
+    
