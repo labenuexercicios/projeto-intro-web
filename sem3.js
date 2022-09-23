51216a25ae74e39a884cee19cd3d73af44b8421d
marcoshamil76 <marcoshamilton1976@gmail.com>
2022-09-23 12:44:31 -0300

    Atualização Semana3

--- sem3.js
+++ sem3.js
@@ -1,68 +1,68 @@
 //const listaProdutos = []
 const listaProdutos = [ // lista de produtos adicionados os produtos a uma array, onde cada produto é um
-                // objeto
- {
-    nome: "brigadeiro",
-    descricao: "docinho",
-    estoque: false,
-    valor: 1,
-    ingredientes: ["Chocolate em pó","Leite Condensado","Chocolate Granulado"]
-},
-{
-    nome: "CupCake",
-    descricao: "Bolos",
-    estoque: true,
-    valor: 3,
-    ingredientes: ["Farinha de Trigo","Açucar","Fermento","Chocolate"]
-},
-{
-    nome: "Cone",
-    descricao: "Cones",
-    estoque: true,
-    valor: 5,
-    ingredientes: ["Cone","Confete","Ganache","Leite em pó"]
-},
-{
-    nome: "Trufa",
-    descricao: "Trufas",
-    estoque: true,
-    valor: 2,
-    ingredientes: ["Chocolate","Leite Condensado"]
-},
-{
-    nome: "Beijinho",
-    descricao: "docinho",
-    estoque: false,
-    valor: 2,
-    ingredientes: ["Leite Condensado","Leite em pó","coco granulado"]
-},
-{
-    nome: "Nozinho",
-    descricao: "panificados",
-    estoque: false,
-    valor: 5,
-    ingredientes: ["Farinha de Trigo","Óleo","Açúcar","Leite","Ovo"]
-}
+    // objeto
+    {
+        nome: "brigadeiro",
+        descricao: "docinho",
+        estoque: false,
+        valor: 1,
+        ingredientes: ["Chocolate em pó", "Leite Condensado", "Chocolate Granulado"]
+    },
+    {
+        nome: "CupCake",
+        descricao: "Bolos",
+        estoque: true,
+        valor: 3,
+        ingredientes: ["Farinha de Trigo", "Açucar", "Fermento", "Chocolate"]
+    },
+    {
+        nome: "Cone",
+        descricao: "Cones",
+        estoque: true,
+        valor: 5,
+        ingredientes: ["Cone", "Confete", "Ganache", "Leite em pó"]
+    },
+    {
+        nome: "Trufa",
+        descricao: "Trufas",
+        estoque: true,
+        valor: 2,
+        ingredientes: ["Chocolate", "Leite Condensado"]
+    },
+    {
+        nome: "Beijinho",
+        descricao: "docinho",
+        estoque: false,
+        valor: 2,
+        ingredientes: ["Leite Condensado", "Leite em pó", "coco granulado"]
+    },
+    {
+        nome: "Nozinho",
+        descricao: "panificados",
+        estoque: false,
+        valor: 5,
+        ingredientes: ["Farinha de Trigo", "Óleo", "Açúcar", "Leite", "Ovo"]
+    }
 ]
 // checando se o produto constar em estoque é adicionado ao array
-function checarEstoque (produtosLista){ // função que inclue no array produtosEmFalta os produtos em falta
-    const produtosEmFalta =[]
-    for(i of produtosLista){
-        if(i.estoque !== true){
-            console.log (i.nome)
+function checarEstoque(produtosLista) { // função que inclue no array produtosEmFalta os produtos em falta
+    const produtosEmFalta = []
+    for (i of produtosLista) {
+        if (i.estoque !== true) {
+            console.log(i.nome)
             produtosEmFalta.push(i.nome)
         }
-    }    return produtosEmFalta
-    }
+    } return produtosEmFalta
+}
 
-console.log(checarEstoque(listaProdutos))
+//console.log(checarEstoque(listaProdutos))
 
 // segunda maneira de adicionar o produto utilizando o if ternario
 
 //produto2.estoque ? listaProdutos.push(produto2):alert("Produto em falta")
 // array listaProdutos está com produtos que constam em estoque
 //console.log (produto2.nome)
-console.log (listaProdutos)
+//console.log(listaProdutos)
 
 // for(let i of produto1.ingredientes){
 //     let junta = i.ingredientes
@@ -72,23 +72,25 @@ console.log (listaProdutos)
 // }
 // console.log (listaProd)
 
-const alteraParaString = (arrayEnviado)=>{
-    let string =[]
-    for (i in arrayEnviado){
+
+// comando para alterar para string os ingredientes
+const alteraParaString = (arrayEnviado) => {
+    let string = []
+    for (i in arrayEnviado) {
         string.push(`${arrayEnviado[i].ingredientes.join(" ")}`)
-        arrayEnviado[i].ingredientes=arrayEnviado[i].ingredientes.join()
+        arrayEnviado[i].ingredientes = arrayEnviado[i].ingredientes.join()
     }
     return string
-    
+
 }
 console.log(alteraParaString(listaProdutos))
 //console.log(listaProdutos)
-for (let i of listaProdutos){
-    console.log(i)
+for (let i of listaProdutos) {
+    //console.log(i)
 }
 
-for (let i in listaProdutos){
-    console.log (listaProdutos[i].nome.toUpperCase(),listaProdutos[i].descricao,listaProdutos[i].estoque,listaProdutos[i].valor,listaProdutos[i].ingredientes)
+for (let i in listaProdutos) {
+   // console.log(listaProdutos[i].nome.toUpperCase(), listaProdutos[i].descricao, listaProdutos[i].estoque, listaProdutos[i].valor, listaProdutos[i].ingredientes)
 }
 
 // function devolveString (parametro){
@@ -101,12 +103,39 @@ for (let i in listaProdutos){
 //     return stringDevolvida
 // }
 // console.log(devolveString(listaProdutos))
-function devolveString (parametro){
-    for(const i in parametro){
-        for(const j in i){
-            console.log (parametro[i][j])
-        }
-       
-}}
-    
-console.log(devolveString(listaProdutos))
+//function devolveString (parametro){
+//     for(const i of parametro){
+//         for(const j in i){
+//             console.log (parametro[i][j])
+//         }
+
+// }
+
+// objeto listaProdutos, dentro deste objeto na propriedade ingredientes tenho um array
+
+// For abaixo mostra somente o nome
+for (let i = 0; i < listaProdutos.length; i++) {
+    //console.log("lista de produtos da Maria Ganache  " + listaProdutos[i].nome + listaProdutos[i].ingredientes)
+}
+
+for (let i of listaProdutos) {
+    //console.log("Lista de Produtos da Maria Ganache " + i)
+    //console.log(i)
+
+
+}
+
+const recebeArrayObjetos = (arrayInformado, stringInformada) => {
+    const resultado = arrayInformado.filter((pedevela) => {
+        
+        return pedevela.nome === stringInformada
+    })
+    if (resultado.length > 0) {
+        console.log(resultado)
+    } else {
+        alert("Nenhum ingrediente encontrado")
+    }
+}
+
+recebeArrayObjetos(listaProdutos, "Nozinho")
+
---
+++ sem3teste.js
@@ -0,0 +1,168 @@
+//const listaProdutos = []
+const listaProdutos = [ // lista de produtos adicionados os produtos a uma array, onde cada produto é um
+                // objeto
+ {
+    nome: "brigadeiro",
+    descricao: "docinho",
+    estoque: false,
+    valor: 1,
+    ingredientes: ["Chocolate em pó","Leite Condensado","Chocolate Granulado"]
+},
+{
+    nome: "CupCake",
+    descricao: "Bolos",
+    estoque: true,
+    valor: 3,
+    ingredientes:[]
+    //ingredientes: ["Farinha de Trigo","Açucar","Fermento","Chocolate"]
+},
+{
+    nome: "Cone",
+    descricao: "Cones",
+    estoque: true,
+    valor: 5,
+    ingredientes: ["Cone","Confete","Ganache","Leite em pó"]
+},
+{
+    nome: "Trufa",
+    descricao: "Trufas",
+    estoque: false,
+    valor: 2,
+    ingredientes: ["Chocolate","Leite Condensado"]
+},
+{
+    nome: "Beijinho",
+    descricao: "docinho",
+    estoque: false,
+    valor: 2,
+    ingredientes: ["Leite Condensado","Leite em pó","coco granulado"]
+},
+{
+    nome: "Nozinho",
+    descricao: "panificados",
+    estoque: false,
+    valor: 5,
+    ingredientes: [,"Óleo","Açúcar","Leite","Ovo"]
+}
+]
+// checando se o produto constar em estoque é adicionado ao array
+function checarEstoque (produtosLista){ // função que inclue no array produtosEmFalta os produtos em falta
+    const produtosEmFalta =[]
+    for(i of produtosLista){
+        if(i.estoque !== true){
+            console.log (i.nome)
+            produtosEmFalta.push(i.nome)
+        }
+    }    return produtosEmFalta
+    }
+
+//console.log(checarEstoque(listaProdutos))
+
+for (let i =0; i<listaProdutos.length; i++){
+    console.log ("Os ingredientes para  " + listaProdutos[i].nome +  "  são: ")
+    const itensProduto = listaProdutos[i].ingredientes
+    //console.table(itensProduto)
+    if (itensProduto.length){
+    for (let k =0; k< itensProduto.length; k++){
+        console.log(itensProduto[k])
+    }
+    }else {
+    console.log("Item sem ingredientes cadastrados")
+    }
+
+}
+
+// const recebeArrayObjetos = (arrayInformado,stringInformada)=>{
+//     const resultado = arrayInformado.filter((objeto)=>{
+//         return objeto.nome === stringInformada
+//     })
+//     if(resultado.length >0){
+//         console.log(resultado)
+//     }else { alert ("Nenhum ingrediente encontrado")
+
+//     }
+// }
+
+
+
+// segunda maneira de adicionar o produto utilizando o if ternario
+
+//produto2.estoque ? listaProdutos.push(produto2):alert("Produto em falta")
+// array listaProdutos está com produtos que constam em estoque
+//console.log (produto2.nome)
+//console.log (listaProdutos)
+
+// for(let i of produto1.ingredientes){
+//     let junta = i.ingredientes
+//     junta += i.ingredientes
+//     console.log(i.ingredientes)
+//     console.log(junta)
+// }
+// console.log (listaProd)
+
+
+// comando para alterar para string os ingredientes
+// const alteraParaString = (arrayEnviado)=>{
+//     let string =[]
+//     for (i in arrayEnviado){
+//         string.push(`${arrayEnviado[i].ingredientes.join(" ")}`)
+//         arrayEnviado[i].ingredientes=arrayEnviado[i].ingredientes.join()
+//     }
+//     return string
+    
+// }
+// console.log(alteraParaString(listaProdutos))
+// //console.log(listaProdutos)
+// for (let i of listaProdutos){
+//     console.log(i)
+// }
+
+// for (let i in listaProdutos){
+//     console.log (listaProdutos[i].nome.toUpperCase(),listaProdutos[i].descricao,listaProdutos[i].estoque,listaProdutos[i].valor,listaProdutos[i].ingredientes)
+// }
+
+// function devolveString (parametro){
+//     let  stringDevolvida 
+//     for(let i in parametro){
+//         for(let k of i){
+//             console.log(k.parametro[i])
+//             stringDevolvida += (`${String(k.parametro[i])}`) 
+//            }}
+//     return stringDevolvida
+// }
+// console.log(devolveString(listaProdutos))
+//function devolveString (parametro){
+//     for(const i of parametro){
+//         for(const j in i){
+//             console.log (parametro[i][j])
+//         }
+       
+// }
+
+// objeto listaProdutos, dentro deste objeto na propriedade ingredientes tenho um array
+    
+// For abaixo mostra somente o nome
+// for (let i = 0; i <listaProdutos.length; i++){
+//     console.log("lista de produtos da Maria Ganache  " + listaProdutos[i].nome +listaProdutos[i].ingredientes)
+//     }
+
+// for(let i of listaProdutos){
+//     console.log("Lista de Produtos da Maria Ganache " + i)
+//     console.log(i)
+
+
+//  }
+const recebeArrayObjetos = (arrayInformado,stringInformada)=>{
+const resultado = arrayInformado.filter((pedevela)=>{
+    console.log(pedevela)
+    console.log(pedevela.nome)
+    return pedevela.nome === stringInformada
+})
+if(resultado.length >0){
+    console.log(resultado)
+}else { alert ("Nenhum ingrediente encontrado")
+}
+}
+
+recebeArrayObjetos(listaProdutos,"Cone")
+console.log(listaProdutos)
