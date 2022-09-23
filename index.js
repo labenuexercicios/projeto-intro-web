7e99974ff38362e5fc43fa736583084f067b4fda
Arthuramaral100 <arthuramaral57@gmail.com>
2022-09-23 16:14:21 -0300

    Semana 3 do projeto-intro-web

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ sinopse: "Esta versão americana de \"The Office\" é uma comédia que gira em t
 temps: 9, 
 episodios: 201, 
 temNetflix: false, 
-elenco :["Steve Carrell", "Rainn Wilson", "John Krasinski", "Jenna Fischer", "B. J. Novak", "Angela Kinsey", "Brian Baumgartner", "Oscar Nunez", "Leslie David Baker", "Creed Bratton", "Phyllis Smith", "MIndy Kaling", "Kate Flannery", "Paul Lieberstein", "Craig Robinson", "Ed Helms", "Ellie Kemper"]
+elenco: ["Steve Carrell", "Rainn Wilson", "John Krasinski", "Jenna Fischer", "B. J. Novak", "Angela Kinsey", "Brian Baumgartner", "Oscar Nunez", "Leslie David Baker", "Creed Bratton", "Phyllis Smith", "MIndy Kaling", "Kate Flannery", "Paul Lieberstein", "Craig Robinson", "Ed Helms", "Ellie Kemper"]
 }
 
 let serie2 = {nome: "Brooklyn 99", 
@@ -25,44 +25,30 @@ elenco: ["Claire Foy", "Olivia Colman", "Matt Smith", "Tobias Menzies", "Vanessa
 // Criar um array vazio e verificar se o booleano de cada objeto é true para poder adicionar ao array vazio 
 
 const arraySeries = []
-
-if(serie1.temNetflix){
-    arraySeries.push(serie1) 
-}else{
-    alert(`A série ${serie1.nome} não está presente no catálogo da Netflix`)
+function verificaNetflix(serie) {
+    if(serie.temNetflix){
+        arraySeries.push(serie)
+    }else {
+        //alert(`A série ${serie.nome} não está presente no catálogo da Netflix`)
+    }
 }
-
-if (serie2.temNetflix) {
-    arraySeries.push(serie2)
-}else{
-    alert(`A série ${serie2.nome} não está presente no catálogo da Netflix`)
-}
-
-    if(serie3.temNetflix){
-    arraySeries.push(serie3)
-}else{
-    alert(`A série ${serie3.nome} não está presente no catálogo da Netflix`)
-}
+verificaNetflix(serie1)
+verificaNetflix(serie2)
+verificaNetflix(serie3)
 
 // Tirando média do número de episodios das 3 séries
 
 const numEpisodios = (serie1.episodios + serie2.episodios + serie3.episodios) / 3
-console.log(` Média de episódios das 3 séries: ${numEpisodios.toFixed(0)}`);
+//console.log(`Média de episódios das 3 séries: ${numEpisodios.toFixed(0)}`);
 
 // Tirando média de episódios por temporada de cada série
-// const mediaTemps1 = serie1.episodios / serie1.temps
-// console.log(mediaTemps1.toFixed(0));
 
-// const mediaTemps2 = serie2.episodios / serie2.temps
-// console.log(mediaTemps2.toFixed(0));
-
-// const mediaTemps3 = serie3.episodios / serie3.temps
-// console.log(mediaTemps3.toFixed(0));
-
-// Verificando se todas as séries estão na Netflix
-
-const temNetflix1 = serie1.temNetflix && serie2.temNetflix && serie3.temNetflix
-console.log(`Todas as séries estãos na Netflix? ${temNetflix1}`)
+function mediaTemp(serie){
+        return (`Média de episódios por temporada de ${serie.nome}: ${(serie.episodios / serie.temps).toFixed(0)}`);
+}
+mediaTemp(serie1)
+mediaTemp(serie2)
+mediaTemp(serie3)
 
 // Alterando o nome ds séries para maiúsculo
 
@@ -70,10 +56,59 @@ serie1.nome = serie1.nome.toUpperCase()
 serie2.nome = serie2.nome.toUpperCase()
 serie3.nome = serie3.nome.toUpperCase()
 
-// Imprimindo os objetos e o array com os objetos dentro
+// Alterando o array "elenco" para uma string em todas as series
 
-console.log(serie1);
-console.log(serie2);
-console.log(serie3);
+let novoArray = [serie1, serie2, serie3]
+// for(let ator of novoArray){
+//      ator.elenco = ator.elenco.toString()
+// }
 
-console.log(arraySeries);
+// console.log(novoArray);
+
+// Transformei o objeto em uma string
+
+
+for(let serie of novoArray){
+    console.log(`
+    ${serie.nome}
+    SINOPSE: ${serie.sinopse}
+    TEMPORADAS: ${serie.temps}
+    QUANTIDADE DE EPISÓDIOS: ${serie.episodios}
+    DISPONIBILIDADE NA NETFLIX: ${serie.temNetflix}
+    ELENCO: ${serie.elenco}`);
+}
+
+// 
+
+for (elemento of novoArray) {
+    for (indice in elemento) {
+        console.log(`${indice}: ${elemento[indice]}`)
+    }
+    console.log('--------------------')
+}
+
+// function objetoParaString(objeto){
+//     for(let i in objeto){
+//         console.log(objeto[i].toString())
+//     };
+//     console.log("=============================================");
+// }
+// objetoParaString(serie1)
+// objetoParaString(serie2)
+// objetoParaString(serie3)
+
+// Código que verifica se existe série com o mesmo nome da string retornada, verifica dentro do array
+
+function encontraSerie(array, string){
+    const buscaSerie = array.filter((elemento) => {
+        if(elemento.nome === string){
+            return elemento
+    }
+})
+    if(buscaSerie.length === 0){
+        alert("Série não encontrada")
+    }else{
+        console.log(buscaSerie);
+    }
+}
+encontraSerie(novoArray, "BROOKLYN 99")
