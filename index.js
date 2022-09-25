8587ed5296986f1d44a94ce7972e654c3641ec15
adrianomikio <adrianouge@gmail.com>
2022-09-25 14:44:20 -0300

    5. pronto

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ const nomeAnimal1 = "Shiba Inu"
 const origemAnimal1 = "Japão"
 const patasAnimal1 = 4;
 const voaOuNaoAnimal1 = false 
-
+const coresComunsAnimal1 = ["Sésamo Vermelho", "Preto e canela", "Creme", "Preto sésamo", "Sésame", "Vermelho"]
 
 const nomeAnimal2 = "Arara"
 const origemAnimal2 = "America do Sul"
 const patasAnimal2 = 2
 const voaOuNaoAnimal2 = true
-
+const coresComunsAnimal2 = ["Azul e Amarela", "Vermelha e Verde", "Amarela, Azul e Vermelho"]
 
 const nomeAnimal3 = "Foca"
 const origemAnimal3 = "Hemisfério do Norte"
 const patasAnimal3 = 0
 const voaOuNaoAnimal3 = false
+const coresComunsAnimal3 = ["Branca", "Acizentada", "Marrom Escuro"]
 
 const mediaDePatas = (patasAnimal1+patasAnimal2+patasAnimal3)/3
 console.log(mediaDePatas)
