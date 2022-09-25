e55da402a94f6801508de4746a303d61a0ebb9db
adrianomikio <adrianouge@gmail.com>
2022-09-25 14:30:55 -0300

    2. pronto

---
+++ index.js
@@ -0,0 +1,20 @@
+// Item escolhido foi animais,
+// características dos animais: nome, origem, quantas patas, é voador ou não
+
+
+const nomeAnimal1 = "Shiba Inu"
+const origemAnimal1 = "Japão"
+const patasAnimal1 = 4;
+const voaOuNaoAnimal1 = false 
+
+
+const nomeAnimal2 = "Arara"
+const origemAnimal2 = "America do Sul"
+const patasAnimal2 = 2
+const voaOuNaoAnimal2 = true
+
+
+const nomeAnimal3 = "Foca"
+const origemAnimal3 = "Hemisfério do Norte"
+const patasAnimal3 = 0
+const voaOuNaoAnimal3 = false
