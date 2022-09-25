35fd46ce4450d8417af9efc1df56343bfd2f2738
adrianomikio <adrianouge@gmail.com>
2022-09-25 14:51:22 -0300

    6. pronto

--- index.js
+++ index.js
@@ -6,22 +6,41 @@ const nomeAnimal1 = "Shiba Inu"
 const origemAnimal1 = "Japão"
 const patasAnimal1 = 4;
 const voaOuNaoAnimal1 = false 
-const coresComunsAnimal1 = ["Sésamo Vermelho", "Preto e canela", "Creme", "Preto sésamo", "Sésame", "Vermelho"]
+const coresComunsAnimal1 = ["Sésamo Vermelho", " Preto e canela", " Creme", " Preto sésamo", " Sésame", " Vermelho"]
 
 const nomeAnimal2 = "Arara"
 const origemAnimal2 = "America do Sul"
 const patasAnimal2 = 2
 const voaOuNaoAnimal2 = true
-const coresComunsAnimal2 = ["Azul e Amarela", "Vermelha e Verde", "Amarela, Azul e Vermelho"]
+const coresComunsAnimal2 = ["Azul e Amarela", " Vermelha e Verde", " Amarela, Azul e Vermelho"]
 
 const nomeAnimal3 = "Foca"
 const origemAnimal3 = "Hemisfério do Norte"
 const patasAnimal3 = 0
 const voaOuNaoAnimal3 = false
-const coresComunsAnimal3 = ["Branca", "Acizentada", "Marrom Escuro"]
+const coresComunsAnimal3 = ["Branca", " Acizentada", " Marrom Escuro"]
 
 const mediaDePatas = (patasAnimal1+patasAnimal2+patasAnimal3)/3
 console.log(mediaDePatas)
 
 const verificarTodosAnimaisVoadoresOuNao = voaOuNaoAnimal1 && voaOuNaoAnimal2 && voaOuNaoAnimal3
 console.log(verificarTodosAnimaisVoadoresOuNao)
+
+console.log(
+    `Informações sobre animais:
+    
+    Nome: ${nomeAnimal1.toLocaleUpperCase()}
+    Origem: ${origemAnimal1}
+    Quantas patas tem? ${patasAnimal1}
+    Quais são as cores comuns do animal? ${coresComunsAnimal1}
+    
+    Nome: ${nomeAnimal2.toLocaleUpperCase()}
+    Origem: ${origemAnimal2}
+    Quantas patas tem? ${patasAnimal2}
+    Quais são as cores comuns do animal? ${coresComunsAnimal2}
+    
+    Nome: ${nomeAnimal3.toLocaleUpperCase()}
+    Origem: ${origemAnimal3}
+    Quantas patas tem? ${patasAnimal3}
+    Quais são as cores comuns do animal? ${coresComunsAnimal3}`
+)
