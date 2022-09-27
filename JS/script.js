1904ff472813710a46eaf46d8de1111203ecf607
Gabriel Max <gabrielmaxcerqueira@gmail.com>
2022-09-27 17:39:32 -0300

    semana 1, 2 e 3 concluidas

---
+++ JS/script.js
@@ -0,0 +1,66 @@
+
+const nome1 = "Reyna"
+const idade1 = 25
+const classe1 = "Duelista"
+const hype1 = true
+const habilidade1 = ["Comedora de Alma", "Intagibilidade rsrs", "Olho que Cega", "Mulé fico brava"]
+
+
+const nome2 = "Viper"
+const idade2 = 30
+const classe2 = "Controladora"
+const hype2 = true
+const habilidade2 = ["Peido Tenebroso", "Parede de fumaça", "Veneninho Slime", "Bob Marley"]
+
+const nome3 = "Sova"
+const idade3 = 33
+const classe3 = "Iniciador"
+const hype3 = false
+const habilidade3 = ["Flecha ricochete", "Flecha (não vai subir ninguém)", "Drone Abelha", "Não Existe Fusca!!!"]
+
+
+const media = (idade1 + idade2 + idade3) / 3
+const hype = hype1 && hype2 && hype3
+
+console.log(media)
+console.log(hype)
+
+console.log(nome1.toUpperCase(), idade1, classe1, hype1, habilidade1)
+console.log(nome2.toUpperCase(), idade2, classe2, hype2, habilidade2)
+console.log(nome3.toUpperCase(), idade3, classe3, hype3, habilidade3)
+
+
+const allAgentes = [] 
+
+ reyna = {
+    nome: ("Reyna"),
+    idade: (25),
+    classe: ("Duelista"),
+    hype: (true),
+    habilidades: ["Comedora de Alma", "Intagibilidade rsrs", "Olho que Cega", "Mulé fico brava"]
+}
+
+viper = {
+    nome: ("Viper"),
+    idade: (30),
+    classe: ("Controladora"),
+    hype: (false),
+    habilidades2: ["Peido Tenebroso", "Parede de fumaça", "Veneninho Slime", "Bob Marley"],
+}
+
+sova = {
+    nome: ("Sova"),
+    idade: (33),
+    classe: ("Iniciador"),
+    hype: (false),
+    habilidades: ["Flecha ricochete", "Flecha (não vai subir ninguém)", "Drone Abelha", "Não Existe Fusca!!!"]
+}
+
+
+
+
+
+
+
+
+
