5d58f3b583794420cf6bf74bd7526c244c85240f
Astastroth <raphaelaloisiodiniz@gmail.com>
2022-09-29 08:21:57 -0300

    semana10

---
+++ script.js
@@ -0,0 +1,15 @@
+function pesquisar(){
+    var input,filtro,menu,menuItens,links;
+    input = document.getElementById("pesquisa");
+    filtro = input.value.toUpperCase();
+    menu = document.getElementById("menu");
+    menuItens = menu.getElementsByTagName("li");
+    for(var i=0;i<menuItens.length;i++){
+        links = menuItens[i].getElementsByTagName("a")[0];
+        if(links.innerHTML.toUpperCase().indexOf(filtro)>-1){
+            menuItens[i].style.display="";
+        }else{
+            menuItens[i].style.display="none";
+        }
+    }
+}
