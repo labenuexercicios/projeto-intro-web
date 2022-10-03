8a455363dd26067c02d3f59a8a5dc62fd2fbf52b
lecjxx <leticiacorreia2500@gmail.com>
2022-10-02 22:54:07 -0300

    Semana 01 - Projeto-desenvolvimento-web

---
+++ script.js
@@ -0,0 +1,47 @@
+
+// SEMANA 01
+
+const filme01 = "Meu amigo Totoro";
+const anoDeLancamento01 = 1988
+const duracao01 = "86 min";
+const RoteiroDeHayaoMiyazaki01 = true
+
+const filme02 = "O Serviço de Entregas da Kiki";
+const anoDeLancamento02 = 1989
+const duracao02 = "103 min";
+const RoteiroDeHayaoMiyazaki02 = true
+
+const filme03 = "Sussurros do Coração";
+const anoDeLancamento03 = 1995
+const duracao03 = "111 min";
+const RoteiroDeHayaoMiyazaki03 = true
+
+const filme04 = "Mononoke Hime";
+const anoDeLancamento04 = 1997
+const duracao04 = "133 min";
+const RoteiroDeHayaoMiyazaki04 = true
+
+const filme05 = "A Viagem de Chihiro";
+const anoDeLancamento05 = 2001
+const duracao05 = "125 min";
+const RoteiroDeHayaoMiyazaki05 = true
+
+const filme06 = "O Reino dos Gatos";
+const anoDeLancamento06 = 2002
+const duracao06 = "75 min";
+const RoteiroDeHayaoMiyazaki06 = true
+
+const filme07 = "O Castelo Animado";
+const anoDeLancamento07 = 2004
+const duracao07 = "119 min";
+const RoteiroDeHayaoMiyazaki07 = true
+
+const filme08 = "Ponyo – Uma Amizade Que Veio do Mar";
+const anoDeLancamento08 = 2008
+const duracao08 = "101 min";
+const RoteiroDeHayaoMiyazaki08 = true
+
+const filme09 = "Ponyo – Uma Amizade Que Veio do Mar";
+const anoDeLancamento09 = 2013
+const duracao09 = "126 min";
+const RoteiroDeHayaoMiyazaki09 = true
