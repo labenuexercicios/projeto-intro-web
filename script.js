8f2d3bce04f368e52fd37455a31a73e84010016f
Atreus66 <brunoragnar06@gmail.com>
2022-10-03 15:58:39 -0300

    Semana 1

---
+++ script.js
@@ -0,0 +1,45 @@
+const personagem = "Monkey D. Luffy";
+const idade = 19;
+const procurado = true;
+
+const personagem1 = "Roronoa Zoro";
+const idade1 = 21;
+const procurado1 = true;
+
+const personagem2 = "God Usopp";
+const idade2 = 18;
+const procurado2 = true;
+
+const personagem3 = "Vinsmoke Sanji";
+const idade3 = 20;
+const procurado3 = true;
+
+const personagem4 = "Nami";
+const idade4 = 20;
+const procurado4 = true;
+
+const personagem5 = "Tony Tony Chopper";
+const idade5 = 17;
+const procurado5 = true;
+
+const personagem6 = "Nico Robin";
+const idade6 = 30;
+const procurado6 = true;
+
+const personagem7= "Franky";
+const idade7 = 36;
+const procurado7 = true;
+
+const personagem8 = "Brook";
+const idade8 = 90;
+const procurado8 = true;
+
+const personagem9 = "Jinbei";
+const idade9 = 46;
+const procurado9 = true;
+
+console.log(procurado && procurado1 && procurado2 && procurado3 && procurado4 && procurado5 && procurado6 && procurado7 && procurado8 && procurado9)
+
+console.log(personagem, idade, procurado)
+
+//------------------------------------------------------------------
