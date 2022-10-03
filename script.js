79221d2f9ec28eea835d78742a5ae57c8c5e79c0
lecjxx <leticiacorreia2500@gmail.com>
2022-10-03 00:23:41 -0300

    Semana - 02

--- script.js
+++ script.js
@@ -1,53 +1,77 @@
 
 // SEMANA 01
 
-const filme01 = "Meu amigo Totoro";
+const filme01 = "Meu amigo Totoro".toUpperCase();
 const anoDeLancamento01 = 1988
 const duracaoEmMinutos01 = 86;
 const RoteiroDeHayaoMiyazaki01 = true
+const elenco01 = ["Chika Sakamoto", "Noriko Hidaka", "Hitoshi Takagi"]
 
-const filme02 = "O Serviço de Entregas da Kiki";
+const filme02 = "O Serviço de Entregas da Kiki".toUpperCase();
 const anoDeLancamento02 = 1989
 const duracaoEmMinutos02 = 103;
 const RoteiroDeHayaoMiyazaki02 = true
+const elenco02 = ["Minami Takayama", "Rei Sakuma", "Kappei Yamaguchi"]
 
-const filme03 = "Sussurros do Coração";
+const filme03 = "Sussurros do Coração".toUpperCase();
 const anoDeLancamento03 = 1995
 const duracaoEmMinutos03 = 111;
 const RoteiroDeHayaoMiyazaki03 = true
+const elenco03 = ["Yoko Honna","Issei Takahashi", "Takashi Tachibana", "Shigeru Muroi", "Shigeru Tsuyuguchi", "Keiju Kobayashi"]
 
-const filme04 = "Mononoke Hime";
+const filme04 = "Mononoke Hime".toUpperCase();
 const anoDeLancamento04 = 1997
 const duracaoEmMinutos04 = 133;
 const RoteiroDeHayaoMiyazaki04 = true
+const elenco04 = ["Yoji Matsuda", "Yuriko Ishida","Yuko Tanaka","Kaoru Kobayashi",
+    "Masahiko Nishimura","Tsunehiko Kamijo","Akihiro Miwa"," Mitsuko Mori","Hisaya Morishige"]
 
-const filme05 = "A Viagem de Chihiro";
+const filme05 = "A Viagem de Chihiro".toUpperCase();
 const anoDeLancamento05 = 2001
 const duracaoEmMinutos05 = 125;
 const RoteiroDeHayaoMiyazaki05 = true
+const elenco05 = [	"Rumi Hiiragi","Miyu Irino","Mari Natsuki","Takeshi Naito",
+    "Yasuko Sawaguchi","Tsunehiko Kamijo","Takehiko Ono","Bunta Sugawara"]
 
-const filme06 = "O Reino dos Gatos";
+const filme06 = "O Reino dos Gatos".toUpperCase();
 const anoDeLancamento06 = 2002
 const duracaoEmMinutos06 = 75;
 const RoteiroDeHayaoMiyazaki06 = true
+const elenco06 = ["	Chizuru Ikewaki","Yoshihiko Hakamada","Aki Maeda","Takayuki Yamada",
+    "Hitomi Sato",'Kenta Satoi',"Mari Hamada","Tetsu Watanabe"]
 
-const filme07 = "O Castelo Animado";
+const filme07 = "O Castelo Animado".toUpperCase();
 const anoDeLancamento07 = 2004
 const duracaoEmMinutos07 = 119;
 const RoteiroDeHayaoMiyazaki07 = true
+const elenco07 = ["Chieko Baisho", "Takuya Kimura", "Akihiro Miwa"]
 
-const filme08 = "Ponyo – Uma Amizade Que Veio do Mar";
+const filme08 = "Ponyo – Uma Amizade Que Veio do Mar".toUpperCase();
 const anoDeLancamento08 = 2008
 const duracaoEmMinutos08 = 101 ;
 const RoteiroDeHayaoMiyazaki08 = true
+const elenco08 = ["Tomoko Yamaguchi","Kazushige Nagashima","Yuki Amami","George Tokoro",
+    "Yuria Nara", "Hiroki Doi", "Rumi Hiiragi", "Akiko Yano", "Kazuko Yoshiyuki", "Tomoko Naraoka"]
 
-const filme09 = "Ponyo – Uma Amizade Que Veio do Mar";
+const filme09 = "Vidas ao Vento".toUpperCase();
 const anoDeLancamento09 = 2013
 const duracaoEmMinutos09 = 126 ;
 const RoteiroDeHayaoMiyazaki09 = true
+const elenco09 = ["Hideaki Anno", "Miori Takimoto", "Hidetoshi Nishijima","Masahiko Nishimura", "Morio Kazama", "Keiko Takeshita",
+    "Mirai Shida", "Jun Kunimura", "Shinobu Otake", "Mansai Nomura"]
 
-const media = duracaoEmMinutos01 + duracaoEmMinutos02 + duracaoEmMinutos03 + duracaoEmMinutos04 + duracaoEmMinutos05 + duracaoEmMinutos06 + duracaoEmMinutos07 + duracaoEmMinutos08 + duracaoEmMinutos09 / 9
+const media = (duracaoEmMinutos01 + duracaoEmMinutos02 + duracaoEmMinutos03 + duracaoEmMinutos04 + duracaoEmMinutos05 + duracaoEmMinutos06 + duracaoEmMinutos07 + duracaoEmMinutos08 + duracaoEmMinutos09) % 9
 console.log(media, "minutos");
 
-const verificarRoterista = RoteiroDeHayaoMiyazaki01 && RoteiroDeHayaoMiyazaki02 && RoteiroDeHayaoMiyazaki03 && RoteiroDeHayaoMiyazaki04 && RoteiroDeHayaoMiyazaki05 && RoteiroDeHayaoMiyazaki06 && RoteiroDeHayaoMiyazaki07 && RoteiroDeHayaoMiyazaki08 && RoteiroDeHayaoMiyazaki09 
+const verificarRoterista = (RoteiroDeHayaoMiyazaki01 && RoteiroDeHayaoMiyazaki02 && RoteiroDeHayaoMiyazaki03 && RoteiroDeHayaoMiyazaki04 && RoteiroDeHayaoMiyazaki05 && RoteiroDeHayaoMiyazaki06 && RoteiroDeHayaoMiyazaki07 && RoteiroDeHayaoMiyazaki08 && RoteiroDeHayaoMiyazaki09) 
 console.log(verificarRoterista);
+
+console.log(filme01, "\n", anoDeLancamento01,"\n", duracaoEmMinutos01, "\n", RoteiroDeHayaoMiyazaki01, "\n", elenco01);
+console.log(filme02, "\n", anoDeLancamento02,"\n", duracaoEmMinutos02, "\n", RoteiroDeHayaoMiyazaki02, "\n", elenco02);
+console.log(filme03, "\n", anoDeLancamento03,"\n", duracaoEmMinutos03, "\n", RoteiroDeHayaoMiyazaki03, "\n", elenco03);
+console.log(filme04, "\n", anoDeLancamento04,"\n", duracaoEmMinutos04, "\n", RoteiroDeHayaoMiyazaki04, "\n", elenco04);
+console.log(filme05, "\n", anoDeLancamento05,"\n", duracaoEmMinutos05, "\n", RoteiroDeHayaoMiyazaki05, "\n", elenco05);
+console.log(filme06, "\n", anoDeLancamento06,"\n", duracaoEmMinutos06, "\n", RoteiroDeHayaoMiyazaki06, "\n", elenco06);
+console.log(filme07, "\n", anoDeLancamento07,"\n", duracaoEmMinutos07, "\n", RoteiroDeHayaoMiyazaki07, "\n", elenco07);
+console.log(filme08, "\n", anoDeLancamento08,"\n", duracaoEmMinutos08, "\n", RoteiroDeHayaoMiyazaki08, "\n", elenco08);
+console.log(filme09, "\n", anoDeLancamento09,"\n", duracaoEmMinutos09, "\n", RoteiroDeHayaoMiyazaki09, "\n", elenco09);
