700cd4dff02fb8ffc0250be39e5d4baee754870c
lecjxx <leticiacorreia2500@gmail.com>
2022-10-02 23:43:37 -0300

    Semana 01

---
+++ script.js
@@ -0,0 +1,53 @@
+
+// SEMANA 01
+
+const filme01 = "Meu amigo Totoro";
+const anoDeLancamento01 = 1988
+const duracaoEmMinutos01 = 86;
+const RoteiroDeHayaoMiyazaki01 = true
+
+const filme02 = "O Serviço de Entregas da Kiki";
+const anoDeLancamento02 = 1989
+const duracaoEmMinutos02 = 103;
+const RoteiroDeHayaoMiyazaki02 = true
+
+const filme03 = "Sussurros do Coração";
+const anoDeLancamento03 = 1995
+const duracaoEmMinutos03 = 111;
+const RoteiroDeHayaoMiyazaki03 = true
+
+const filme04 = "Mononoke Hime";
+const anoDeLancamento04 = 1997
+const duracaoEmMinutos04 = 133;
+const RoteiroDeHayaoMiyazaki04 = true
+
+const filme05 = "A Viagem de Chihiro";
+const anoDeLancamento05 = 2001
+const duracaoEmMinutos05 = 125;
+const RoteiroDeHayaoMiyazaki05 = true
+
+const filme06 = "O Reino dos Gatos";
+const anoDeLancamento06 = 2002
+const duracaoEmMinutos06 = 75;
+const RoteiroDeHayaoMiyazaki06 = true
+
+const filme07 = "O Castelo Animado";
+const anoDeLancamento07 = 2004
+const duracaoEmMinutos07 = 119;
+const RoteiroDeHayaoMiyazaki07 = true
+
+const filme08 = "Ponyo – Uma Amizade Que Veio do Mar";
+const anoDeLancamento08 = 2008
+const duracaoEmMinutos08 = 101 ;
+const RoteiroDeHayaoMiyazaki08 = true
+
+const filme09 = "Ponyo – Uma Amizade Que Veio do Mar";
+const anoDeLancamento09 = 2013
+const duracaoEmMinutos09 = 126 ;
+const RoteiroDeHayaoMiyazaki09 = true
+
+const media = duracaoEmMinutos01 + duracaoEmMinutos02 + duracaoEmMinutos03 + duracaoEmMinutos04 + duracaoEmMinutos05 + duracaoEmMinutos06 + duracaoEmMinutos07 + duracaoEmMinutos08 + duracaoEmMinutos09 / 9
+console.log(media, "minutos");
+
+const verificarRoterista = RoteiroDeHayaoMiyazaki01 && RoteiroDeHayaoMiyazaki02 && RoteiroDeHayaoMiyazaki03 && RoteiroDeHayaoMiyazaki04 && RoteiroDeHayaoMiyazaki05 && RoteiroDeHayaoMiyazaki06 && RoteiroDeHayaoMiyazaki07 && RoteiroDeHayaoMiyazaki08 && RoteiroDeHayaoMiyazaki09 
+console.log(verificarRoterista);
