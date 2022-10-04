79c036a05228edd382775dc27848fd402c832c71
Matheus Mendes <matheusmendesreis@outlook.com>
2022-10-04 19:26:10 -0300

    parte do dom resolvido

--- script.js
+++ script.js
@@ -7,16 +7,18 @@ const series = {
     elencoPrincipal: []
 }
 
-const greysAnatomy = {...series,
+const greysAnatomy = {
+    ...series,
     nome: 'greys anatomy',
-    anoLancamento: Number(2005),
+    anoLancamento: 2005,
     isSerie: true,
-    totTemporadas: Number(19),
+    totTemporadas: 19,
     genero: ['drama', 'ficção medica', 'romance', 'comedia dramatica'],
     elencoPrincipal: ['ellen pompeo', 'patrick dempsey', 'sandra oh']
 }
 
-const laCasaDePapel = {...series,
+const laCasaDePapel = {
+    ...series,
     nome: 'lá casa de papel',
     anoLancamento: Number(2017),
     isSerie: true,
@@ -24,7 +26,8 @@ const laCasaDePapel = {...series,
     genero: ['assalto', 'drama', 'suspense', 'crime'],
     elencoPrincipal: ['alvaro morte', 'alba flores', 'miguel herran']
 }
-const vikings = {...series,
+const vikings = {
+    ...series,
     nome: 'vikings',
     anoLancamento: Number(2013),
     isSerie: true,
@@ -35,23 +38,23 @@ const vikings = {...series,
 // Exercicio 2
 const bdSeries = []
 // Exercicio 3
-if(laCasaDePapel.isSerie){
+if (laCasaDePapel.isSerie) {
     bdSeries.push(laCasaDePapel)
-}else{
-    console.log('A serie ',laCasaDePapel.nome,'não foi adicionada.')
-    
+} else {
+    console.log('A serie ', laCasaDePapel.nome, 'não foi adicionada.')
+
 }
 
-if(greysAnatomy.isSerie){
+if (greysAnatomy.isSerie) {
     bdSeries.push(greysAnatomy)
-}else{
-    console.log('A serie ',greysAnatomy.nome,' não foi adicionado')
+} else {
+    console.log('A serie ', greysAnatomy.nome, ' não foi adicionado')
 }
 
 if (vikings.isSerie) {
     bdSeries.push(vikings)
 } else {
-    console.log('A serie ',vikings.nome,' não foi adicionado')
+    console.log('A serie ', vikings.nome, ' não foi adicionado')
 }
 // console.log(teste)
 
@@ -60,25 +63,43 @@ let txtGreysAnatomy = greysAnatomy.elencoPrincipal.toString()
 let txtLaCasaDePapel = laCasaDePapel.elencoPrincipal.toString()
 
 let = stringGreysAnatomy = ""
-for(let i in greysAnatomy.elencoPrincipal){
+for (let i in greysAnatomy.elencoPrincipal) {
     stringGreysAnatomy += greysAnatomy.elencoPrincipal[i] + " "
 }
 
 let = stringLaCasaDePapel = ""
-for(let i in laCasaDePapel.elencoPrincipal){
+for (let i in laCasaDePapel.elencoPrincipal) {
     stringLaCasaDePapel += laCasaDePapel.elencoPrincipal[i] + " "
 }
 
 let = stringVikings = ""
-for(let i in vikings.elencoPrincipal){
+for (let i in vikings.elencoPrincipal) {
     stringVikings += vikings.elencoPrincipal[i] + " "
 }
 
 function relatorio(objeto) {
     let txt = ""
-    for(keys in objeto){
-        txt +=keys +": "+ objeto[keys] + "\n"
+    for (keys in objeto) {
+        txt += keys + ": " + objeto[keys] + "\n"
     }
     return txt.toString()
 }
 
+
+function addElem() {
+
+    const main = document.getElementById('main');
+    // Criando elemento section
+    const section = document.createElement('section')
+    // Inserindo elemento SECTION no HTML
+    main.appendChild(section)
+    const ul = document.createElement('ul')
+    for (prop in greysAnatomy) {
+        //  Criando elemento lista
+        const li = document.createElement('li')
+        li.innerHTML += greysAnatomy[prop]
+        section.appendChild(li)
+        
+    }
+}
+addElem()
