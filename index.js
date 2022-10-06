cfdef9afc1d1cc07855cb3f49ae1ca0009efcc85
jessicathamires <jessica.thamiresnunes@gmail.com>
2022-10-06 19:00:47 -0300

    Projeto parte 2

---
+++ index.js
@@ -0,0 +1,78 @@
+// const item1 = {
+//     suco1:'Demolidor',
+//     descricao1:"Produto 100% natural, embalado a vácuo e congelado.",
+//     ingrediente1: ['Beterraba, cenoura, maça, gengibre, limão e chia.'],
+//     preco1:6,
+//     isverde1:false,
+//     }
+    
+//     const item2 = {
+//     suco2:"Mais Bela",
+//     descricao2:"Produto 100% natural, embalado a vácuo e congelado.",
+//     ingrediente2: ['Couve, maça, gengibre, limão e chia.'],
+//     preco2:7,
+//     isverde2:true,
+//     }
+    
+//     const item3 = {
+//     suco3:"Maria bonita",
+//     descricao3:"Produto 100% natural, embalado a vácuo e congelado.",
+//     ingrediente3: ['Couve, maça, pepino, limão e chia.'],
+//     preco3:8,
+//     isverde3:true,
+//     }
+    
+//     let itensDoSuco = []
+    
+    
+    
+//     // 3 Adicione os objetos criados no item 1 ao array de
+//     //  objetos criado no item 2, utilizando o push()
+    
+//     if (item1.isverde1 === true){
+//       itensDoSuco.push(item1)
+//     }else{
+//         alert("Item NÃO adicionado ao carrinho!")
+//     }
+    
+//     if (item2.isverde2 === true){
+//         itensDoSuco.push(item2)
+//     }else{
+//         alert("Item NÃO adicionado ao carrinho!")
+//     }
+    
+//     if (item3.isverde3 === true){
+//         itensDoSuco.push(item3)
+//     }else{
+//         alert("Item NÃO adicionado ao carrinho!")
+//     }
+    
+
+//     console.log(itensDoSuco)
+//     console.log(item1,item2, item3)
+    
+    
+    
+    // console.log(arrayItens)
+    
+    
+    // mediapreco :console.log(preco1+preco2+preco3/3),
+    // boolAltaCaloria :console.log(isverde1&&isverde2&&isverde3),
+    
+    // console.log(suco1.toUpperCase())
+    // console.log(descricao1)
+    // console.log(ingrediente1)
+    // console.log(preco1)
+    // console.log(isverde1)
+    
+    // console.log(suco2.toUpperCase())
+    // console.log(descricao2)
+    // console.log(ingrediente2)
+    // console.log(preco2)
+    // console.log(isverde2)
+    
+    // console.log(suco3.toUpperCase())
+    // console.log(descricao3)
+    // console.log(ingrediente3)
+    // console.log(preco3)
+    // console.log(isverde3)
