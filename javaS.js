163aa5a351dbbf62fc35b022fd6a99a54012c331
Will <acamedhf@gmail.com>
2022-10-08 18:33:49 -0300

    semana 6 Ok!

--- javaS.js
+++ javaS.js
@@ -2,14 +2,14 @@ const leao = {
     nome: "leão".toUpperCase(),
     tempoMedioDeVida: 15,
     classe: true,
-    array: "Rei da selva, dono da mordida que tem uma potencia de 600 a 650 PSI, sempre é o primeiro a comer"
+    array:[ "Rei da selva", "dono da mordida que tem uma potencia de 600 a 650 PSI", "sempre é o primeiro a comer"]
 }
 
 const urso = {
     nome: "urso-pardo".toUpperCase(),
     tempoMedioDeVida: 26,
     classe: true,
-    array: "Têm pelagens grossas e em camadas, são grandes, fortes e rápidos, podem andar apenas com as patas traseiras"
+    array:[ "Têm pelagens grossas e em camadas", "são grandes, fortes e rápidos", "podem andar apenas com as patas traseiras"]
 
 }
 
@@ -17,16 +17,16 @@ const cachorro = {
     nome: "cachorro".toUpperCase(),
     tempoMedioDeVida: 13,
     classe: true,
-    array: "O olfato de um cachorro é 1 milhão de vezes melhor do que o dos humanos, se não for castrada, uma pode ter 66 filhotes em 6 anos, são onívoros, eles precisam comer mais do que apenas carne"
+    array: ["O olfato de um cachorro é 1 milhão de vezes melhor do que o dos humanos", "se não for castrada", "uma pode ter 66 filhotes em 6 anos", "são onívoros", "eles precisam comer mais do que apenas carne"]
 }
 
-// const escolha = prompt("Digite " + "\"Sim\"" + " pra adcionar os objetos ao array de objetos ou " + "\"Não\"" + " para não adcionar os objeotos no array de objetos").toUpperCase()
+const escolha = prompt("Digite " + "\"Sim\"" + " pra adcionar os objetos ao array de objetos ou " + "\"Não\"" + " para não adcionar os objeotos no array de objetos").toUpperCase()
 
-// const variavel = ("SIM" === escolha)  //solução mais legal 
+const variavel = ("SIM" === escolha)  //solução mais legal 
 
-const adc = []
+const adc = [] //array de obj
 
-const variavel = true
+// const variavel = true
 
 if (variavel) {
     adc.push(leao)
@@ -37,12 +37,30 @@ if (variavel) {
     alert("Itens não foram adcionados")
 }
 
-for (let j = 0; j < adc.length; j++) {
-    console.log(`Nome ${adc[j].nome.toUpperCase()}\nTempo médio de vida ${adc[j].tempoMedioDeVida}\n${adc[j].classe}\n${adc[j].array}`)
+// for (let i = 0; i < leao.array.length; i++) {
+//     console.log (leao.array[i])
+// }
+
+// for (let j = 0; j < leao.length; j++) {
+//     console.log(`Nome ${leao[j].nome.toUpperCase()}\nTempo médio de vidsssssa ${leao[j].tempoMedioDeVida}\n${leao[j].classe}\n${leao[j].array}`)
+// }
+
+// function queRetorna(leaorapa) {
+//     let string = ""
+//     string = leaorapa
+//     console.log(string)
+// }
+
+// queRetorna(leao)
+
+function nan (celular, sust) {
+    let fofo = {}
+    for (let i in celular){
+       if (celular[i] === sust){
+         fofo = celular[i]
+         }
+    }
+    console.log (fofo)
 }
 
-function queRetorna(leao){
-    console.log(leao)
-}
-
-queRetorna(leao)
+nan(adc, leao)
