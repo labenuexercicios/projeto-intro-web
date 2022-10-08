54d141953bf900c69137d0f36c33a3d6768415b4
Atreus66 <brunoragnar06@gmail.com>
2022-10-08 20:14:19 -0300

    Semana 3

--- script.js
+++ script.js
@@ -1,79 +1,80 @@
-const personagem = "Monkey D. Luffy";
-const idade = 19;
-const procurado = true;
-const Recompensa = "B$: 3.000.000,00"
-const Atributos = ["Capitão dos Chapeús de Palha" ,"Gomu Gomu no MI" ,"Haki Do conquistador" ,"Haki da observação" ,"Força incomum"]
+const Mugiwaras = []
 
-console.log(`${personagem.toUpperCase()}\n ${idade} \n ${procurado} \n ${Recompensa} \n ${Atributos}`)
+const MugiwarasBando = [{
+    Pirata: "Monkey D. Luffy",
+    idade: 19,
+    procurado: true,
+    Recompensa: "B$: 3.000.000,00",
+    Atributos: ["Capitão dos Chapeús de Palha" ,"Gomu Gomu no MI" ,"Haki Do conquistador" ,"Haki da observação" ,"Força incomum"]
+},
+    {
 
-const personagem1 = "Roronoa Zoro";
-const idade1 = 21;
-const procurado1 = true;
-const Recompensa1 = "B$: 1.111.000,00"
-const Atributos1 = ["Espadachim" ,"Haki Da Observação" ,"Primeiro imediato dos Mugiwaras" ,"Velocidade e Agilidade"]
+    Pirata: "Roronoa Zoro",
+    idade: 21,
+    procurado:true,
+    Recompensa: "B$: 1.111.000,00",
+    Atributos: ["Espadachim" ,"Haki Da Observação" ,"Primeiro imediato dos Mugiwaras" ,"Velocidade e Agilidade"]
+},
+    {
 
-console.log(`${personagem1.toUpperCase()}\n ${idade1} \n ${procurado1} \n ${Recompensa1} \n ${Atributos1}`)
+    Pirata: "Vinsmoke Sanji",
+    idade: 20,
+    procurado:true,
+    Recompensa: "B$: 1.032.000,00",
+    Atributos: ["Cozinheiro", "Força incomum", "Resistência", "velocidade", "Senso de cavalheirismo"]
+},
+    {
 
-const personagem2 = "God Usopp";
-const idade2 = 18;
-const procurado2 = true;
-const Recompensa2 = "B$: 500.000,00"
-const Atributos2 = ["Atirador do Bando", "Velocidade", "Haki da Observação"]
+    Pirata: "Nami",
+    idade: 20,
+    procurado:true,
+    Recompensa: "B$: 366.000,00",
+    Atributos: ["Navegadora do bando", "Navegação e Cartografia", "Ladra e batedora de carteiras"]
+},
+    {
+    
+    Pirata: "Tony Tony Chopper",
+    idade: 17,
+    procurado:true,
+    Recompensa: "B$: 1.000,00",
+    Atributos: ["médico do bando", "Hito Hito no Mi", "Fruta tipo Zoan", "Inteligência"]
+},
+    {
 
-console.log(`${personagem2.toUpperCase()}\n ${idade2} \n ${procurado2} \n ${Recompensa2} \n ${Atributos2}`)
+    Pirata: "Nico Robin",
+    idade: 30,
+    procurado:true,
+    Recompensa: "B$: 930.000,00",
+    Atributos: ["Inteligência e conhecimento vasto", "Arqueológa", "Fruta: Hana Hana no Mi", "Espionagem"]
+},
+    {
 
-const personagem3 = "Vinsmoke Sanji";
-const idade3 = 20;
-const procurado3 = true;
-const Recompensa3 = "B$: 1.032.000,00"
-const Atributos3 = ["Cozinheiro", "Força incomum", "Resistência", "velocidade", "Senso de cavalheirismo"]
-console.log(`${personagem3.toUpperCase()}\n ${idade3} \n ${procurado3} \n ${Recompensa3} \n ${Atributos3}`)
+    Pirata: "Franky",
+    idade: 36,
+    procurado:true,
+    Recompensa: "B$: 394.000,00",
+    Atributos: ["Cyborg", "Construtor e Engenheiro Naval dos Mugiwaras", "Super força"]
+},
+    {
 
-const personagem4 = "Nami";
-const idade4 = 20;
-const procurado4 = true;
-const Recompensa4 = "B$: 366.000,00"
-const Atributos4 = ["Navegadora do bando", "Navegação e Cartografia", "Ladra e batedora de carteiras"]
-console.log(`${personagem4.toUpperCase()}\n ${idade4} \n ${procurado4} \n ${Recompensa4} \n ${Atributos4}`)
+    Pirata: "Brook",
+    idade: 90,
+    procurado:true,
+    Recompensa: "B$: 383.000,00",
+    Atributos: ["Músico", "Fruta: Yomi Yomi no Mi", "Inteligência Tática"]
+},
+    {
 
-const personagem5 = "Tony Tony Chopper";
-const idade5 = 17;
-const procurado5 = true;
-const Recompensa5 = "B$: 1.000,00"
-const Atributos5 = ["médico do bando", "Hito Hito no Mi", "Fruta tipo Zoan", "Inteligência"]
-console.log(`${personagem5.toUpperCase()}\n ${idade5} \n ${procurado5} \n ${Recompensa5} \n ${Atributos5}`)
+    Pirata: "Jinbei",
+    idade: 46,
+    procurado:true,
+    Recompensa: "B$: 1.100.000,00",
+    Atributos: ["Homem-Peixe", "Gyojin Karatê", "Super força"]
+}]
 
-const personagem6 = "Nico Robin";
-const idade6 = 30;
-const procurado6 = true;
-const Recompensa6 = "B$: 930.000,00"
-const Atributos6 = ["Inteligência e conhecimento vasto", "Arqueológa", "Fruta: Hana Hana no Mi", "Espionagem"]
-console.log(`${personagem6.toUpperCase()}\n ${idade6} \n ${procurado6} \n ${Recompensa6} \n ${Atributos6}`)
+Mugiwaras.push(MugiwarasBando)
+console.log(MugiwarasBando)
 
-const personagem7= "Franky";
-const idade7 = 36;
-const procurado7 = true;
-const Recompensa7 = "B$: 394.000,00"
-const Atributos7 = ["Cyborg", "Construtor e Engenheiro Naval dos Mugiwaras", "Super força"]
-console.log(`${personagem7.toUpperCase()}\n ${idade7} \n ${procurado7} \n ${Recompensa7} \n ${Atributos7}`)
 
-const personagem8 = "Brook";
-const idade8 = 90;
-const procurado8 = true;
-const Recompensa8 = "B$: 383.000,00"
-const Atributos8 = ["Músico", "Fruta: Yomi Yomi no Mi", "Inteligência Tática"]
-console.log(`${personagem8.toUpperCase()}\n ${idade8} \n ${procurado8} \n ${Recompensa8} \n ${Atributos5}`)
-
-const personagem9 = "Jinbei";
-const idade9 = 46;
-const procurado9 = true;
-const Recompensa9 = "B$: 1.100.000,00"
-const Atributos9 = ["Homem-Peixe", "Gyojin Karatê", "Super força"]
-console.log(`${personagem9.toUpperCase()}\n ${idade9} \n ${procurado9} \n ${Recompensa9} \n ${Atributos9}`)
-
-// const media = ((idade + idade1 + idade2 + idade3 + idade4 + idade5 + idade6 + idade7 + idade8 + idade9)/10);
-// console.log(media)
-
-console.log(procurado && procurado1 && procurado2 && procurado3 && procurado4 && procurado5 && procurado6 && procurado7 && procurado8 && procurado9)
 
 //------------------------------------------------------------------
