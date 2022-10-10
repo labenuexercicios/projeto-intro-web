6e28deb292588f90d3629c93b0ebc730c555be50
Bruno Hammes <bruno.hammes22@gmail.com>
2022-10-09 20:33:52 -0400

    inclusão do deploy

--- js/script.js
+++ js/script.js
@@ -42,22 +42,132 @@ function filtraPoker(nome, pokemons) {
 }
 const poke_container = document.getElementById('poke-container')
 
+function tabela() {
+    let table = `
+    <table class="vitals-table">
+<tbody>
+    <tr>
+        <th>
+            <font>HP</font>
+        </th>
+        <td class="cell-num">
+            <font>80</font>
+        </td>
+        <td class="cell-barchart">
+            <div style="width:100%;" class="barchart-bar barchart-rank-3 "></div>
+        </td>
+
+    </tr>
+    <tr>
+        <th>
+
+            <font>Ataque</font>
+
+        </th>
+        <td class="cell-num">
+
+            <font>82</font>
+
+        </td>
+        <td class="cell-barchart">
+            <div style="width:45.56%;" class="barchart-bar barchart-rank-3 "></div>
+        </td>
+
+    </tr>
+    <tr>
+        <th>
+            <font>Defesa</font>
+        </th>
+        <td class="cell-num">
+            <font style="vertical-align: inherit;">83</font>
+        </td>
+        <td class="cell-barchart">
+            <div style="width:46.11%;" class="barchart-bar barchart-rank-3 "></div>
+        </td>
+
+    </tr>
+    <tr>
+        <th>
+            <font>Sp.Atk </font>
+        </th>
+        <td class="cell-num">
+            <font>100</font>
+        </td>
+        <td class="cell-barchart">
+            <div style="width:55.56%;" class="barchart-bar barchart-rank-4 "></div>
+        </td>
+    </tr>
+    <tr>
+        <th>
+            <font style="vertical-align: inherit;">Sp. Def </font>
+        </th>
+        <td class="cell-num">
+            <font>100</font>
+        </td>
+        <td class="cell-barchart">
+            <div style="width:55.56%;" class="barchart-bar barchart-rank-4 "></div>
+        </td>
+    </tr>
+    <tr>
+        <th>
+            <font>Velocidade</font>
+        </th>
+        <td class="cell-num">
+            <font>80</font>
+        </td>
+        <td class="cell-barchart">
+            <div style="width:44.44%;" class="barchart-bar barchart-rank-3 "></div>
+        </td>
+
+</tbody>
+<tfoot>
+    <tr>
+        <th>
+            <font style="vertical-align: inherit;">
+                <font style="vertical-align: inherit;">Total</font>
+            </font>
+        </th>
+        <td class="cell-num cell-total">
+            <font style="vertical-align: inherit;">
+                <font style="vertical-align: inherit;">525</font>
+            </font>
+        </td>
+    </tr>
+</tfoot>
+</table>
+    `
+    return table
+}
+
+function tituloPoker(pokemon) {
+    let titulo = `
+<figure class="img-container">
+<img src=${pokemon.img} alt="${pokemon.name}">
+</figure>
+<article class="info">
+<span class="number">#${pokemon.num}</span>
+<h3 class="name">${pokemon.name}</h3>
+<small class="type">Tipo: <span>${pokemon.type.join(", ")}</span> </small><br/>
+
+</article>
+`
+    return titulo
+}
+
 const createPokemonCard = (pokemon) => {
     // console.log(pokemon)
     const pokemonEl = document.createElement('section')
     pokemonEl.classList.add('pokemon')
     pokemonEl.classList.add(pokemon.type[0].toLowerCase())
-    
-    const pokemonInnerHTML = `
-<figure class="img-container">
-    <img src=${pokemon.img} alt="${pokemon.name}">
-</figure>
-<article class="info">
-    <span class="number">#${pokemon.num}</span>
-    <h3 class="name">${pokemon.name}</h3>
-    <small class="type">Tipo: <span>${pokemon.type.join(", ")}</span> </small><br/>
 
-</article>`
+    const pokemonInnerHTML = `
+  <section class="tituloPoker">
+        ${tituloPoker(pokemon)}
+    </section>
+    <atricle >
+        ${tabela()}
+    </article>
+    `
     pokemonEl.innerHTML = pokemonInnerHTML
     poke_container.appendChild(pokemonEl)
 }
