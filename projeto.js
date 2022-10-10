b55baf1b846e1c87f823cf34f61e4ffca3980ec5
LABNUISA <araujo_isaias@outlook.com>
2022-10-09 23:12:10 -0300

    subindo

--- projeto.js
+++ projeto.js
@@ -29,132 +29,174 @@ console.log('Os animais são da mesma espécie? ',especie)*/
 //SEMANA 2
 
 //PROJETO WEB SEMANA 2
-/*console.log('Projeto Web - Semana 2')
-console.log('')
+// console.log('Projeto Web - Semana 2')
+// console.log('')
 
-const nome = "HAMSTER SIRIO"
-const tempoDeVida = 3
-const especieRoedor = true
-const alimento = ['racão extrusada', 'frutas', 'legumes', 'grãos']
+// const nome = "HAMSTER SIRIO"
+// const tempoDeVida = 3
+// const especieRoedor = true
+// const alimento = ['racão extrusada', 'frutas', 'legumes', 'grãos']
 
-console.log (`Nome: ${nome}\nTempo médio de vida: ${tempoDeVida}\nEspecie roedor? ${especieRoedor}\nAlimento: ${[alimento]}`.toLocaleUpperCase())
-console.log("")
+// console.log (`Nome: ${nome}\nTempo médio de vida: ${tempoDeVida}\nEspecie roedor? ${especieRoedor}\nAlimento: ${[alimento]}`.toLocaleUpperCase())
+// console.log("")
 
-const nome2 = "HAMSTER ANÃO RUSSO"
-const tempoDeVida2 = 3
-const especieRoedor2 = true
-const alimento2 = ['racão extrusada', 'frutas', 'legumes', 'grãos']
-console.log (`Nome: ${nome2}\nTempo médio de vida: ${tempoDeVida2}\nEspecie roedor? ${especieRoedor2}\nAlimento: ${alimento2}`.toUpperCase())
-console.log("")
+// const nome2 = "HAMSTER ANÃO RUSSO"
+// const tempoDeVida2 = 3
+// const especieRoedor2 = true
+// const alimento2 = ['racão extrusada', 'frutas', 'legumes', 'grãos']
+// console.log (`Nome: ${nome2}\nTempo médio de vida: ${tempoDeVida2}\nEspecie roedor? ${especieRoedor2}\nAlimento: ${alimento2}`.toUpperCase())
+// console.log("")
 
 
-const nome3 = "PORQUINHO DA INDIA"
-const tempoDeVida3 = 8
-const especieRoedor3 = true
-const alimento3 = ['racão própria', 'frutas', 'legumes', 'grãos', 'feno']
-console.log (`Nome: ${nome3}\nTempo médio de vida: ${tempoDeVida3}\nEspecie roedor? ${especieRoedor3}\nAlimento: ${alimento3}`.toUpperCase())
-console.log("")*/
+// const nome3 = "PORQUINHO DA INDIA"
+// const tempoDeVida3 = 8
+// const especieRoedor3 = true
+// const alimento3 = ['racão própria', 'frutas', 'legumes', 'grãos', 'feno']
+// console.log (`Nome: ${nome3}\nTempo médio de vida: ${tempoDeVida3}\nEspecie roedor? ${especieRoedor3}\nAlimento: ${alimento3}`.toUpperCase())
+// console.log("")
 
 //SEMANA 3
 
-/*console.log('Projeto Web - Semana 3')
+// console.log('Projeto Web - Semana 3')
 
-roedor={
-    nome: "HAMSTER SIRIO", 
-    mediaVida: 3,
-    especieRoedor: true,
-    alimento: 'racão extrusada, frutas, legumes e grãos'
-}
-roedor2={
-    nome: "HAMSTER ANAO RUSSO", 
-    mediaVida: 3,
-    especieRoedor: true,
-    alimento: 'racão extrusada, frutas, legumes e grãos'
-}
+// roedor={
+//     nome: "HAMSTER SIRIO", 
+//     mediaVida: 3,
+//     especieRoedor: true,
+//     alimento: 'racão extrusada, frutas, legumes e grãos'
+// }
+// roedor2={
+//     nome: "HAMSTER ANAO RUSSO", 
+//     mediaVida: 3,
+//     especieRoedor: true,
+//     alimento: 'racão extrusada, frutas, legumes e grãos'
+// }
 
-roedor3={
-    nome: "PORQUINHO DA INDIA INGLÊS", 
-    mediaVida: 8,
-    especieRoedor: true,
-    alimento: 'Feno, Capim fresco, frutas e verduras e ração própria'
-}
-//console.log(roedor, roedor2, roedor3)
+// roedor3={
+//     nome: "PORQUINHO DA INDIA INGLÊS", 
+//     mediaVida: 8,
+//     especieRoedor: true,
+//     alimento: 'Feno, Capim fresco, frutas e verduras e ração própria'
+// }
+// //console.log(roedor, roedor2, roedor3)
 
-const animais =[]
-animais.push(roedor, roedor2, roedor3)
-console.log(animais)*/
+// const animais =[]
+// animais.push(roedor, roedor2, roedor3)
+// console.log(animais)
 
 //SEMANA 4
 
-/*console.log('Projeto Web  - Semana 4')
+// console.log('Projeto Web  - Semana 4')
 
 
-roedor={
+// roedor={
+//     nome: "HAMSTER SIRIO", 
+//     mediaVida: 3,
+//     especieRoedor: true,
+//     alimento: 'racão extrusada, frutas, legumes e grãos'
+// }
+// roedor2={
+//     nome: "HAMSTER ANAO RUSSO", 
+//     mediaVida: 3,
+//     especieRoedor: true,
+//     alimento: 'racão extrusada, frutas, legumes e grãos'
+// }
+
+// roedor3={
+//     nome: "PORQUINHO DA INDIA INGLÊS", 
+//     mediaVida: 8,
+//     especieRoedor: true,
+//     alimento: 'Feno, Capim fresco, frutas e verduras e ração própria'
+// }
+// //console.log(roedor, roedor2, roedor3)
+
+// const animais = []
+
+// if (roedor.especieRoedor && roedor2.especieRoedor && roedor3.especieRoedor === true){
+//     animais.push(roedor, roedor2, roedor3)
+//     console.log('Push realizado:', animais)
+
+// }else{
+//     alert('Item não pôde ser adicionado. O psuh não será realizado')
+//     console.log("Push não realizado", animais)
+// }
+
+
+// //SEMANA 5
+
+// console.log('Projeto Web - Semana 5')
+// console.log('')
+
+// const roedores =[
+//     {
+//     nome: "HAMSTER SIRIO", 
+//     mediaVida: 3,
+//     especieRoedor: true,
+//     alimento: [' racão extrusada', ' frutas', ' legumes e grãos']
+// },
+
+// {
+//     nome: "HAMSTER ANAO RUSSO", 
+//     mediaVida: 3,
+//     especieRoedor: true,
+//     alimento: [' racão extrusada', ' frutas', ' legumes e grãos']
+// },
+
+// {
+//     nome: "PORQUINHO DA INDIA INGLÊS", 
+//     mediaVida: 8,
+//     especieRoedor: true,
+//     alimento: [' Feno', ' Capim fresco', ' frutas e verduras', ' ração própria']
+// },
+// ]
+
+
+
+// for(let mostra of roedores){
+//     console.log(`Nome: ${mostra.nome}\nMedia de vida: ${mostra.mediaVida}\nEspecie: ${mostra.especieRoedor}\nAlimento: ${mostra.alimento}` )
+// }
+
+//SEMANA 6
+console.log('Projeto Web  - Semana 6')
+console.log(" ")
+
+
+const roedores =[
+    {
     nome: "HAMSTER SIRIO", 
     mediaVida: 3,
     especieRoedor: true,
-    alimento: 'racão extrusada, frutas, legumes e grãos'
-}
-roedor2={
+    alimento: [' racão extrusada', ' frutas', ' legumes e grãos']
+},
+
+{
     nome: "HAMSTER ANAO RUSSO", 
     mediaVida: 3,
     especieRoedor: true,
-    alimento: 'racão extrusada, frutas, legumes e grãos'
-}
+    alimento: [' racão extrusada', ' frutas', ' legumes e grãos']
+},
 
-roedor3={
+{
     nome: "PORQUINHO DA INDIA INGLÊS", 
     mediaVida: 8,
     especieRoedor: true,
-    alimento: 'Feno, Capim fresco, frutas e verduras e ração própria'
-}
-//console.log(roedor, roedor2, roedor3)
+    alimento: [' Feno', ' Capim fresco', ' frutas e verduras', ' ração própria']
+},
+]
 
-const animais = []
+const retornaRoedor = roedores.map((roedor)=>{
+    return `O nome do roedore é ${roedor.nome}, seu tempo médio de vida é ${roedor.mediaVida}, a sua espécie é ${roedor.especieRoedor}, e seu alimento adequado é${roedor.alimento}`
+}) 
 
-if (roedor.especieRoedor, roedor2.especieRoedor, roedor3.especieRoedor === true){
-    animais.push(roedor, roedor2, roedor3)
-    console.log('Push realizado:', animais)
+console.log(retornaRoedor)
 
-}else{
-    alert('Item não pôde ser adicionado. O psuh não será realizado')
-    console.log("Push não realizado", animais)
-}*/
+const apenasHamster = roedores.filter((roedor)=>{
+     if(roedor.nome === "PORQUINHO DA INDIA INGLÊS"){
+        return roedor
+       
+    }
+  
 
+})
 
-//SEMANA 5
-console.log('Projeto Web - Semana 5')
-console.log('')
-
-const nome = "hamster sirio"
-const tempoDeVida = 3
-const especieRoedor = true
-const alimento = ['racão extrusada', 'frutas', 'legumes', 'grãos']
-
-console.log ('Nome:', nome.toUpperCase())
-console.log('Tempo de vida:', tempoDeVida)
-console.log('Especie roedor?', especieRoedor)
-console.log('Alimento:', alimento)
-console.log("")
-
-const nome2 = "hamster anão russo"
-const tempoDeVida2 = 3
-const especieRoedor2 = true
-const alimento2 = ['racão extrusada', 'frutas', 'legumes', 'grãos']
-console.log ('Nome:', nome2.toLocaleUpperCase())
-console.log('Tempo de vida:', tempoDeVida2)
-console.log('Especie roedor?',especieRoedor2)
-console.log(`Alimento: ${alimento2}`.toUpperCase())
-console.log("")
-console.log("")
-
-
-const nome3 = "porquinho da india"
-const tempoDeVida3 = 8
-const especieRoedor3 = true
-const alimento3 = ['racão própria', 'frutas', 'legumes', 'grãos', 'feno']
-console.log (`Nome: ${nome3}\nTempo médio de vida: ${tempoDeVida3}\nEspecie roedor? ${especieRoedor3}\nAlimento: ${alimento3}`.toUpperCase())
-console.log("")
-
-
-
+console.log(apenasHamster)
