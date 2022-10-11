808e7790c7ca4fb72641cb258a4021409d7c95df
mayaracgc <mayaracgcgmail.com>
2022-10-10 21:14:04 -0300

    semana 3 feita

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const genero3 = ["Romance" , " Ficção Histórica"]
 */
 
 
-
+//semana 2
 const novotitulo1 = {
     titulo: "'A Guerra dos Tronos: Crônicas de Gelo e Fogo' - Georg R. R. Martin " ,
     paginas: 600 , 
@@ -45,9 +45,21 @@ const novotitulo3 = {
 }
 //console.log("Livro 3:" , novotitulo3)
 
+const novotitulo5 = {
+    titulo: "'Entrevista com o Vampiro' - Anne Rice" ,
+    paginas: 320,
+    ebook: true, 
+    genero: ["Romance" , "Terror" , "Ficção gótica"] ,
+}
+
+//semana 3
 const objetos = []
-objetos.push(novotitulo1 , novotitulo2 , novotitulo3)
-console.log("Livros com ebook:" , objetos)
+objetos.push(novotitulo1 , novotitulo2 , novotitulo3, novotitulo5)
+//console.log("Livros com ebook:" , objetos)
+for(let i in objetos){
+    console.log(`Livro ${i}: `)
+    console.log(`Título: ${objetos[i].titulo}, Páginas: ${objetos[i].paginas}, Gênero: ${objetos[i].genero}`)
+}
 
 const novotitulo4 = {
     titulo: "'1984' - George Orwell" ,
@@ -57,22 +69,52 @@ const novotitulo4 = {
 
 }
 //console.log("Livro 4:" , novotitulo4)
+const livro4 = []
+livro4.push(novotitulo4)
 
 if(novotitulo4.ebook == true){
     objetos.push(novotitulo4)
     console.log(objetos)
 } else{
     alert("O Livro 4 não foi adicionado a pasta de Livros com ebook.")
-    console.log("Livro sem ebook:" , novotitulo4)
-}
+    for(let i in livro4){
+        console.log(`Livro sem e-book: ${i}`)
+        console.log(`Título: ${livro4[i].titulo}, Páginas: ${livro4[i].paginas}, Gênero: ${livro4[i].genero}`)
+    }
+    //console.log("Livro sem ebook:" , novotitulo4)
+} 
 
 const mediaPaginas = {
-    quantidade: novotitulo1.paginas + novotitulo2.paginas + novotitulo3.paginas + novotitulo4 , 
+    quantidade : novotitulo1.paginas + novotitulo2.paginas + novotitulo3.paginas + novotitulo4 
 }
 console.log("Média de páginas de todos os livros: " , mediaPaginas)
 
 
 const verificaEbook = {
-    verificacao : novotitulo1.ebook && novotitulo2.ebook && novotitulo3.ebook && novotitulo4.ebook ,
+    verificacao : novotitulo1.ebook && novotitulo2.ebook && novotitulo3.ebook && novotitulo4.ebook
 }
 console.log("Verificação de E-books de todos os livros: " , verificaEbook)
+
+
+const retornaString = (novotitulo1) => {
+    const titulo1 = String(novotitulo1.titulo + novotitulo1.paginas + novotitulo1.ebook + novotitulo1.genero)
+    return titulo1
+}
+console.log(retornaString(novotitulo1))
+
+const todosOsLivros = [novotitulo1 , novotitulo2 , novotitulo3 , novotitulo4 , novotitulo5]
+
+
+function retornaIgual(todosOsLivros){
+    let igual = {}
+    const titulo1 = String(novotitulo1.titulo + novotitulo1.paginas + novotitulo1.ebook + novotitulo1.genero)
+    for (let i = 0; i < todosOsLivros.length; i++){
+        if(titulo1 === todosOsLivros){
+            igual.push(todosOsLivros[i])
+            }else{
+                alert = "Nenhum item foi encontrado"
+            }
+    }
+    return igual
+}
+console.log(retornaIgual(todosOsLivros))
