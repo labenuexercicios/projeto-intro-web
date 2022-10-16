46dbc795fb3c3a1fd8d276168a22debbdcac31ad
falcaomm <manu.m.falcao@gmail.com>
2022-10-16 19:25:48 -0300

    semana 7 e mais

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//CARDÁPIO DE VERÃO CONSULADO CAFÉ
-
 const arrayDeObjetos = [
     objeto1 = {
         nome: "Frozen Café Tradicional",
@@ -7,7 +5,7 @@ const arrayDeObjetos = [
         disponivel: true,
         tamanho: 350,
         sabor: "Tradicional",
-        ingredientes: ["Frozem artesanal da casa, ", "expresso duplo e ", "leite cremoso"],
+        ingredientes: ["Frozen artesanal da casa, ", "expresso duplo e ", "leite cremoso."],
         img: "/midias/cafe2.png",
     },
 
@@ -17,7 +15,7 @@ const arrayDeObjetos = [
         disponivel: true,
         tamanho: 350,
         sabor: "Doce de Leite",
-        ingredientes: ["Frozem artesanal doce de leite, ", "expresso duplo e ", "leite cremoso"],
+        ingredientes: ["Frozen artesanal doce de leite, ", "expresso duplo e ", "leite cremoso."],
         img: "/midias/cafe4.png",
     },
 
@@ -27,147 +25,284 @@ const arrayDeObjetos = [
         disponivel: true,
         tamanho: 350,
         sabor: "Limão",
-        ingredientes: ["Frozem de limão siciliano e ", "capuccino tradicional"],
+        ingredientes: ["Frozen de limão siciliano e ", "capuccino tradicional."],
         img: "/midias/cafe1.png",
     },
 ]
 
-console.log(arrayDeObjetos);
 
-// semana 5 - A CADA INTERAÇÃO DO FOR IMPRIME UM DOS OBJETOS DO ARRAY COM AS ALTERAÇÕES PEDIDAS 
-// for (let objetos of arrayDeObjetos) {
-//     let string = ""
-//     for (let ingredientes of objetos.ingredientes) {
-//         string += ingredientes
-//     }
-//     objetos.ingredientes = string
-//     objetos.nome = objetos.nome.toUpperCase()
-//     console.log(objetos);
-// }
+let img = new Image(200, 200)
+img.src = "/midias/cafe2.png"
 
-// SEMANA 6 PARTE 1 - A FUNCAO IMPRIME O OBJETO CHAMADO COM AS ALTERAÇÕES PEDIDAS 
+let titulo = document.createElement("h2")
+titulo.innerHTML = "Frozen Café Tradicional"
 
-const imprimeObjComString = (objeto) => {
-    let string = ""
-    for (let j of objeto.ingredientes) {
-        string += j + ", "
+let preco = document.createElement("li")
+preco.innerHTML = "16R$"
+
+let botao = document.createElement("button")
+botao.innerHTML = "+"
+
+let listaVar = document.getElementById("lista1")
+listaVar.appendChild(img)
+listaVar.insertAdjacentElement("beforeend", titulo)
+listaVar.insertAdjacentElement("beforeend", preco)
+listaVar.insertAdjacentElement("beforeend", botao)
+
+///////////////////////////////////////////////////////////////////////
+
+let img2 = new Image(200, 200)
+img2.src = "/midias/cafe4.png"
+
+let titulo2 = document.createElement("h2")
+titulo2.innerHTML = "Frozen Café Doce de Leite"
+
+let preco2 = document.createElement("li")
+preco2.innerHTML = "17R$"
+
+let botao2 = document.createElement("button")
+botao2.innerHTML = "+"
+
+let lista2Var = document.getElementById("lista2")
+lista2Var.appendChild(img2)
+lista2Var.insertAdjacentElement("beforeend", titulo2)
+lista2Var.insertAdjacentElement("beforeend", preco2)
+lista2Var.insertAdjacentElement("beforeend", botao2)
+
+///////////////////////////////////////////////////////////////////////////
+
+let img3 = new Image(200, 200)
+img3.src = "/midias/cafe1.png"
+
+let titulo3 = document.createElement("h2")
+titulo3.innerHTML = "Frozen Café Limão"
+
+let preco3 = document.createElement("li")
+preco3.innerHTML = "18R$"
+
+let botao3 = document.createElement("button")
+botao3.innerHTML = "+"
+
+let lista3Var = document.getElementById("lista3")
+lista3Var.appendChild(img3)
+lista3Var.insertAdjacentElement("beforeend", titulo3)
+lista3Var.insertAdjacentElement("beforeend", preco3)
+lista3Var.insertAdjacentElement("beforeend", botao3)
+
+//////////////////////////////////////////////////////////////////////////
+
+function buscaItem(arrayDeObjetos) {
+    console.log(arrayDeObjetos.nome);
+    let inputVar = document.getElementById("input")
+    listaVar.innerHTML = ""
+    lista2Var.innerHTML = ""
+    lista3Var.innerHTML = ""
+    listaVar.classList.remove('ul')
+    lista2Var.classList.remove('ul')
+    lista3Var.classList.remove('ul')
+
+    for (let i in arrayDeObjetos) {
+        if (arrayDeObjetos[i].nome === inputVar.value) {
+            let img = new Image(200, 200)
+            img.src = arrayDeObjetos[i].img
+
+            let titulo = document.createElement("h2")
+            titulo.innerHTML = arrayDeObjetos[i].nome
+
+            let preco = document.createElement("li")
+            preco.setAttribute("id", "listaPreco")
+            preco.innerHTML = arrayDeObjetos[i].preco + "R$"
+
+            let tamanho = document.createElement("li")
+            tamanho.setAttribute("id", "listaTamanho")
+            tamanho.innerHTML = arrayDeObjetos[i].tamanho + "ml"
+
+            let string = "\nFeito com "
+            for (let j of arrayDeObjetos[i].ingredientes) {
+                string += `${j} `
+            }
+            let ingredientes = document.createElement("li")
+            ingredientes.innerHTML = string
+
+            let listaVar = document.getElementById("lista1")
+            listaVar.setAttribute("class", "classLista1")
+            listaVar.appendChild(img)
+            listaVar.insertAdjacentElement("beforeend", titulo)
+            listaVar.insertAdjacentElement("beforeend", ingredientes)
+            listaVar.insertAdjacentElement("beforeend", preco)
+            listaVar.insertAdjacentElement("beforeend", tamanho)
+        }
     }
-    return { ...objeto, nome: objeto.nome.toUpperCase(), ingredientes: string }
+
+    if (inputVar.value != "Frozen Café Doce de Leite" && inputVar.value != "Frozen Café Tradicional" && inputVar.value != "Frozen Café Limão") {
+        alert("Pedido não encontrado")
+        window.location.reload(true);
+    }
 }
-// console.log(imprimeObjComString(objeto1))
 
-// 
-// const imprimePedido = (arrayDeObjetos, pedido) => {
-//     for (let i in arrayDeObjetos) {
-//         if (arrayDeObjetos[i].nome === pedido) {
-//             return (arrayDeObjetos[i]);
-//         } else {
-//             return "pedido não encontrado"
-//         }
-//     }
-// }
-// console.log(imprimePedido(arrayDeObjetos, "Frozen Café Limão"));
+function pagInicial(event) {
+    window.location.reload(true);
+    event.preventDefault()
+}
 
-//tentativas semana 6 PART 2- com alert não deu nao funciona se são dois obj com mesmo nome
+function abreTradicional(arrayDeObjetos) {
+    listaVar.innerHTML = ""
+    lista2Var.innerHTML = ""
+    lista3Var.innerHTML = ""
+    listaVar.classList.remove('ul')
+    lista2Var.classList.remove('ul')
+    lista3Var.classList.remove('ul')
 
-// const imprimePedido = (arrayDeObjetos, pedido) => {
-//     const arrayFiltrado = arrayDeObjetos.filter((item) => {
-//         return item.nome === pedido})
-//     for (let i in arrayFiltrado) {
-//         if (arrayFiltrado[i].nome === pedido) {
-//             console.log(arrayFiltrado[i]);
-//         } else {
-//             alert('produto não encontrado')
-//         }
-//     }
-// }
+    for (let i in arrayDeObjetos) {
+        if (arrayDeObjetos[i].nome === titulo.innerHTML) {
+            let img = new Image(200, 200)
+            img.src = arrayDeObjetos[i].img
 
-// const imprimePedido = (arrayDeObjetos, pedido) => {
-//     const filtro = arrayDeObjetos.filter((item) => {
-//         return item.nome === pedido})
-//     return filtro[0]
-// }
+            let titulo = document.createElement("h2")
+            titulo.innerHTML = arrayDeObjetos[i].nome
 
-// SEMANA 6 PARTE 1 - A FUNCAO IMPRIME UMA STRING DO OBJETO COM AS ALTERAÇÕES PEDIDAS
-// const imprimeString = (objeto) => {
-//     let string
-//     string = objeto.nome.toUpperCase()
-//     string += "\nPreço: ", objeto.preco, "R$"
-//     string += "\nEstá Disponível: " + objeto.disponivel
-//     string += "\nTamanho: " + objeto.tamanho + "ml"
-//     string += "\nSabor:" + objeto.sabor
-//     string += "\nIngredientes: "
-//     for (let j of objeto.ingredientes) {
-//         string += `${j}, `
-//     }
-//     console.log(string);
-// }
+            let preco = document.createElement("li")
+            preco.setAttribute("id", "listaPreco")
+            preco.innerHTML = arrayDeObjetos[i].preco + "R$"
 
-// imprimeString(objeto1)
+            let tamanho = document.createElement("li")
+            tamanho.setAttribute("id", "listaTamanho")
+            tamanho.innerHTML = arrayDeObjetos[i].tamanho + "ml"
 
-// DIFERENTES MODOS SEMANA 5
+            let string = "\nFeito com "
+            for (let j of arrayDeObjetos[i].ingredientes) {
+                string += `${j} `
+            }
+            let ingredientes = document.createElement("li")
+            ingredientes.innerHTML = string
 
-// A CADA INTERAÇÃO DO FOR IMPRIME UMA STRING DE UM DOS OBJETOS DO ARRAY:
-// for (let i in arrayDeObjetos) {
-//     let string = ""
-//     string = `\n${arrayDeObjetos[i].nome.toUpperCase()}
-//     \nPreço:${arrayDeObjetos[i].preco}R$
-//     \nEstá Disponível:${arrayDeObjetos[i].disponivel}
-//     \nTamanho:${arrayDeObjetos[i].tamanho}ml
-//     \nSabor:${arrayDeObjetos[i].sabor}
-//     \nIngredientes:`
-//     for(let j of arrayDeObjetos[i].ingredientes) {
-//         string += j
-//     }
-//     console.log(string);
-// }
+            let listaVar = document.getElementById("lista1")
+            listaVar.setAttribute("class", "classLista1")
+            listaVar.appendChild(img)
+            listaVar.insertAdjacentElement("beforeend", titulo)
+            listaVar.insertAdjacentElement("beforeend", ingredientes)
+            listaVar.insertAdjacentElement("beforeend", preco)
+            listaVar.insertAdjacentElement("beforeend", tamanho)
+        }
+    }
+}
 
-// A CADA INTERAÇÃO DO FOR IMPRIME UM CONSOLE LOG DE UMA DAS PROPRIEDADES DOS OBJETOS DO ARRAY:
-// for (let objetos of arrayDeObjetos) {
-//     let string = ""
-//     for (let ingredientes of objetos.ingredientes) {
-//         string += ingredientes
-//     }
-//     objetos.ingredientes = string
-//     objetos.nome = objetos.nome.toUpperCase()
-//     for (let propriedades in objetos) {
-//         console.log(`${propriedades.toUpperCase()}:`, objetos[propriedades]);
-//     }
-// }
+function abreDoceDeLeite(arrayDeObjetos) {
+    listaVar.innerHTML = ""
+    lista2Var.innerHTML = ""
+    lista3Var.innerHTML = ""
+    listaVar.classList.remove('ul')
+    lista2Var.classList.remove('ul')
+    lista3Var.classList.remove('ul')
+    listaVar.setAttribute("onclick", "")
 
-//relatório semana 2
 
-// console.log("\n", objeto1.nome.toUpperCase());
-// console.log("Preço:", objeto1.preco, "R$");
-// console.log("Está disponível?", objeto1.disponivel);
-// console.log("Tamanho:", objeto1.tamanho, "ml");
-// console.log("Sabor:", objeto1.sabor);
+    for (let i in arrayDeObjetos) {
+        if (arrayDeObjetos[i].nome === titulo2.innerHTML) {
+            let img = new Image(200, 200)
+            img.src = arrayDeObjetos[i].img
 
-// console.log("\n",nome2.toUpperCase());
-// console.log("Preço:", preco2,"R$");
-// console.log("É gelado?", gelado2);
-// console.log("Tamanho:", tamanho2,"ml");
-// console.log("Sabor:", sabor2);
-// console.log("Ingredientes:", ingredientes2);
+            let titulo = document.createElement("h2")
+            titulo.innerHTML = arrayDeObjetos[i].nome
 
-// console.log("\n",nome3.toUpperCase());
-// console.log("Preço:", preco3,"R$");
-// console.log("É gelado?", gelado3);
-// console.log("Tamanho:", tamanho3,"ml");
-// console.log("Sabor:", sabor3);
-// console.log("Ingredientes:", ingredientes3);
+            let preco = document.createElement("li")
+            preco.setAttribute("id", "listaPreco")
+            preco.innerHTML = arrayDeObjetos[i].preco + "R$"
 
-//coisas semanas passadas
+            let tamanho = document.createElement("li")
+            tamanho.setAttribute("id", "listaTamanho")
+            tamanho.innerHTML = arrayDeObjetos[i].tamanho + "ml"
 
-// const arrayDeObjetos = []
-// if (objeto1.disponivel === objeto2.disponivel) {
-//     arrayDeObjetos.push(objeto1, objeto2, objeto3)
-// } else {
-//     alert("ITENS NÃO ADICIONADOS")
-// }
+            let string = "\nFeito com "
+            for (let j of arrayDeObjetos[i].ingredientes) {
+                string += `${j} `
+            }
+            let ingredientes = document.createElement("li")
+            ingredientes.innerHTML = string
 
-// const mediaPreco = (preco1 + preco2 + preco3) / 3;
-// console.log("\nMedia de Preço:", mediaPreco);
+            let listaVar = document.getElementById("lista1")
+            listaVar.setAttribute("class", "classLista1")
+            listaVar.appendChild(img)
+            listaVar.insertAdjacentElement("beforeend", titulo)
+            listaVar.insertAdjacentElement("beforeend", ingredientes)
+            listaVar.insertAdjacentElement("beforeend", preco)
+            listaVar.insertAdjacentElement("beforeend", tamanho)
+        }
+    }
+}
 
-// const saoGelados = gelado1 && gelado2 && gelado3;
-// console.log("Todos são gelados?", saoGelados);
+
+function abreLimao(arrayDeObjetos) {
+    listaVar.innerHTML = ""
+    lista2Var.innerHTML = ""
+    lista3Var.innerHTML = ""
+    listaVar.classList.remove('ul')
+    lista2Var.classList.remove('ul')
+    lista3Var.classList.remove('ul')
+    listaVar.setAttribute("onclick", "")
+
+    for (let i in arrayDeObjetos) {
+        if (arrayDeObjetos[i].nome === titulo3.innerHTML) {
+            let img = new Image(200, 200)
+            img.src = arrayDeObjetos[i].img
+
+            let titulo = document.createElement("h2")
+            titulo.innerHTML = arrayDeObjetos[i].nome
+
+            let preco = document.createElement("li")
+            preco.setAttribute("id", "listaPreco")
+            preco.innerHTML = arrayDeObjetos[i].preco + "R$"
+
+            let tamanho = document.createElement("li")
+            tamanho.setAttribute("id", "listaTamanho")
+            tamanho.innerHTML = arrayDeObjetos[i].tamanho + "ml"
+
+            let string = "\nFeito com "
+            for (let j of arrayDeObjetos[i].ingredientes) {
+                string += `${j} `
+            }
+            let ingredientes = document.createElement("li")
+            ingredientes.innerHTML = string
+
+            let listaVar = document.getElementById("lista1")
+            listaVar.setAttribute("class", "classLista1")
+            listaVar.appendChild(img)
+            listaVar.insertAdjacentElement("beforeend", titulo)
+            listaVar.insertAdjacentElement("beforeend", ingredientes)
+            listaVar.insertAdjacentElement("beforeend", preco)
+            listaVar.insertAdjacentElement("beforeend", tamanho)
+        }
+    }
+}
+
+
+for (let i in arrayDeObjetos) {
+    let string = ""
+    string = `\n${arrayDeObjetos[i].nome.toUpperCase()}
+    \nPreço:${arrayDeObjetos[i].preco}R$
+    \nEstá Disponível:${arrayDeObjetos[i].disponivel}
+    \nTamanho:${arrayDeObjetos[i].tamanho}ml
+    \nSabor:${arrayDeObjetos[i].sabor}
+    \nIngredientes:`
+    for (let j of arrayDeObjetos[i].ingredientes) {
+        string += j
+    }
+    console.log("Relatório" + string);
+}
+
+let mediaPreco = 0
+
+function mediaDePreco(arrayDeObjetos) {
+    for (i in arrayDeObjetos)
+        mediaPreco += arrayDeObjetos[i].preco
+
+    return mediaPreco /= arrayDeObjetos.length
+}
+
+console.log("Media de preco:", mediaDePreco(arrayDeObjetos));
+
+for (i in arrayDeObjetos) {
+    if (arrayDeObjetos[i].disponivel === true) {
+        console.log("Estão disponíveis?", true);
+    }
+}
