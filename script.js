30c3b37602cad00ed4c218217a71787a83fd6551
Atreus66 <brunoragnar06@gmail.com>
2022-10-16 23:37:15 -0300

    Projeto-Final

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const MugiwarasBando = [{
-    Pirata: "Monkey D. Luffy",
+    Pirata: "Luffy",
     idade: 19,
     procurado: true,
     Recompensa: "B$: 3.000.000,00",
-    Atributos: ["Capitão dos Chapeús de Palha" ,"Gomu Gomu no MI" ,"Haki Do conquistador" ,"Haki da observação" ,"Força incomum"],
+    Atributos: ["Capitão dos Chapeús de Palha" ,"Portador da Gomu Gomu no MI" ,"Haki Do conquistador" ,"Haki da observação" ,"Força incomum"],
     imagem: "https://images4.alphacoders.com/127/thumbbig-1273867.webp",
     link: "https://onepiece.fandom.com/pt/wiki/Monkey_D._Luffy"
     },
     {
 
-    Pirata: "Roronoa Zoro",
+    Pirata: "Zoro",
     idade: 21,
     procurado:true,
     Recompensa: "B$: 1.111.000,00",
@@ -19,7 +19,7 @@ const MugiwarasBando = [{
 },
     {
 
-    Pirata: "Vinsmoke Sanji",
+    Pirata: "Sanji",
     idade: 20,
     procurado:true,
     Recompensa: "B$: 1.032.000,00",
@@ -39,7 +39,7 @@ const MugiwarasBando = [{
 },
     {
     
-    Pirata: "Tony Tony Chopper",
+    Pirata: "Chopper",
     idade: 17,
     procurado:true,
     Recompensa: "B$: 1.000,00",
@@ -49,7 +49,7 @@ const MugiwarasBando = [{
 },
     {
 
-    Pirata: "Nico Robin",
+    Pirata: "Robin",
     idade: 30,
     procurado:true,
     Recompensa: "B$: 930.000,00",
@@ -88,6 +88,43 @@ const MugiwarasBando = [{
     imagem: "https://images6.alphacoders.com/122/thumbbig-1225550.webp"
 }]
 
+function criarAtributos(Array) {
+    const itensPiratas = document.getElementById("itensPiratas")
+    itensPiratas.innerHTML = ""
+    for (let i = 0; i < Array.length; i++) {
+        itensPiratas.innerHTML += `
+        <section id="Pirata">
+            
+        <ul>
+            <li><a href= ${Array[i].link} target="_blank">${Array[i].Pirata}</a></li>
+            <li>Idade: ${Array[i].idade}</li>
+            <li>Procurado: ${Array[i].procurado}</li>
+            <li>Recompensa: ${Array[i].Recompensa}</li>
+            <li>Atributos:${Array[i].Atributos}</li>
+            <img src=${Array[i].imagem} alt="Monkey D. Luffy">
+        </ul>
+    </section>`
+    }
+}
+criarAtributos(MugiwarasBando)
+
+function chamaPirata() {
+    const input = document.getElementById("search")
+    console.log(input.value)
+    const escolhido = prendePirata(input)
+    console.log(`${escolhido}`? escolhido:alert("Não encontrado"))
+    criarAtributos(escolhido)
+}
+
+function prendePirata(input){
+    const escolhido = MugiwarasBando.filter((item)=>{return item.Pirata.toUpperCase()===input.value.toUpperCase()})
+    console.log(`${escolhido}`? escolhido:alert("Não encontrado"))
+    if (escolhido.length>=1) {
+        return escolhido
+            } else {
+                return MugiwarasBando
+            }
+} 
 // const Mugiwaras = MugiwarasBando.slice()
 // Mugiwaras.push({
 //     nome: "Yamato",
@@ -101,37 +138,38 @@ const MugiwarasBando = [{
 
 //semana 4//
 
-const Mugiwaras ={
-    Pirata: "Yamato",
-    idade: 18,
-    procurado: false,
-    Recompensa: "B$: 1.100.000,00",
-    Atributos: ["Super força", "Agilidade", "Inteligência"]
+// const Mugiwaras ={
+//     Pirata: "Yamato",
+//     idade: 18,
+//     procurado: false,
+//     Recompensa: "B$: 1.100.000,00",
+//     Atributos: ["Super força", "Agilidade", "Inteligência"]
 
-}
-if (Mugiwaras.procurado){
-    MugiwarasBando.push(Mugiwaras)
-} else {
-    alert ("Yamato está cuidando de Wano, ainda não faz parte do bando")
-}
-
-// Semana 5 
-
-// for (let indexM of MugiwarasBando){
-//     console.log(`"${indexM.Atributos}"`)
+// }
+// if (Mugiwaras.procurado){
+//     MugiwarasBando.push(Mugiwaras)
+// } else {
+//     alert ("Yamato está cuidando de Wano, ainda não faz parte do bando")
 // }
 
-for (let index of MugiwarasBando) {
-    console.log(`Pirata: ${index.Pirata}, \n Idade: ${index.idade}, \n ${index.procurado}, \n Recompensa: ${index.Recompensa}, \n Atributos: ${index.Atributos} `)
-}
+// // Semana 5 
 
-// Semana 6 
+// // for (let indexM of MugiwarasBando){
+// //     console.log(`"${indexM.Atributos}"`)
+// // }
 
-const stringPirata = MugiwarasBando.map(p => p.Pirata);
-console.log(stringPirata)
+// for (let index of MugiwarasBando) {
+//     console.log(`Pirata: ${index.Pirata}, \n Idade: ${index.idade}, \n ${index.procurado}, \n Recompensa: ${index.Recompensa}, \n Atributos: ${index.Atributos} `)
+// }
 
-const Capitão = MugiwarasBando.filter((objeto)=>{return objeto.Pirata==="Monkey D. Luffy"})
-console.log(`${Capitão}`? Capitão : alert ("tripulante não encontrado"))
+// // Semana 6 
+
+// const stringPirata = MugiwarasBando.map(p => p.Pirata);
+// console.log(stringPirata)
+
+// const Capitão = MugiwarasBando.filter((objeto)=>{return objeto.Pirata==="Monkey D. Luffy"})
+// console.log(`${Capitão}`? Capitão : alert ("tripulante não encontrado"))
 
 
 //------------------------------------------------------------------ 
+
