424be7f8d68b11ca7db6dcb4e250f43328b54e49
slvgs <gabriellasilveriosantana15@gmail.com>
2022-10-24 11:48:02 -0300

    Projeto Concluido

--- index.js
+++ index.js
@@ -1,32 +1,85 @@
-const BandaZimbra = {
-    Artista: "Banda Zimbra",
+
+//Semana 1 E 2
+
+let BandaZimbra = {
+    nome: "Banda Zimbra",
     isArtista : true,
     QuantosAlbuns: 04,
     TemMaisDe5anos: 12 > 5, 
-    AlgunsAlbuns: ["Azul", "Sala Dois", "Verniz", "O tudo, o Nada e o Mundo"]}
+    AlgunsAlbuns: ["Azul", "Sala Dois", "Verniz", "O tudo, o Nada e o Mundo"],
+    html: document.getElementById("b1")
+}
 
-const Miley = {
-    Artista: "Miley Cyrus", 
+let Miley = {
+    nome: "Miley Cyrus", 
     isArtista : true,
     QuantosAlbuns: 09, 
     TemMaisDe5anos: 21 > 5, 
-    AlgunsAlbuns: ["Bangerz", "Can't Be Tamed", "Plastic Hearts" ]
-    }
+    AlgunsAlbuns: ["Bangerz", "Can't Be Tamed", "Plastic Hearts" ],
+    html: document.getElementById("b2")
+}
 
-const PedroSampaio = {
-    Artista : "Pedro Sampaio",
+let PedroSampaio = {
+    nome : "Pedro Sampaio",
     isArtista: true,
     QuantosAlbuns: 02,
     TemMaisDe5anos: 3 > 5,
-    AlgunsAlbuns: ["Chama meu Nome", "REMIXES"]
-
+    AlgunsAlbuns: ["Chama meu Nome", "REMIXES"],
+    html: document.getElementById("b3")
 }
 
-let MediaAlbuns = 24 / 3
-console.log(MediaAlbuns)
+let ImagineDragons = [{
+    nome: "Imagine Dragons", 
+    isArtista : true,
+    QuantosAlbuns: 09, 
+    TemMaisDe5anos: 12 > 5, 
+    AlgunsAlbuns:["Mercury - Act 2", "Nigth Visions", "Evolve", "It's Time"],
+    html: document.getElementById("b4")
+
+}]
+
+let Djonga = [{
+    nome: "Djonga",
+    isArtista: true,
+    QuantosAlbuns: 04,
+    TemMaisDe5anos: 12 > 5,
+    AlgunsAlbuns:["Heresia", "Nu", "O Menino Que Queria Ser Deus", "Ladrão"],
+    html: document.getElementById("b5")
+
+}]
+
+
+
+
+
+
+
+
+let MediaAlbuns = 24 / 4
+console.log("Média de Albuns de todos os artistas no MEU PORTAL: ", MediaAlbuns)
 let ArmarioArtistas = []
 
 
+
+ArmarioArtistas.push(ImagineDragons[0])
+ArmarioArtistas.push(Djonga[0])
+
+for(i = 0; i<=(ArmarioArtistas.length-1); i++){
+    console.log(ArmarioArtistas[i])
+}
+
+
+
+
+
+
+
+
+if (Djonga.isArtista === true){
+    ArmarioArtistas.push(Djonga)
+}else{
+    alert(`O artista ${Djonga.Artista} não foi adicionado`)
+}
 
 if (BandaZimbra.isArtista === true){
     ArmarioArtistas.push(BandaZimbra)
@@ -49,9 +102,12 @@ if(PedroSampaio.isArtista === true){
 }
 console.log(ArmarioArtistas)
 
+
+
 //semana 3:
 //1. Reescrevendo o relatório criado utilizando console.log() que começamos na semana 2, altere a forma que a característica de array dos itens seja escrita como um laço que guarde todos os valores da propriedade array do objeto em uma mesma string. Utilize esta string no relatório.
 var stringarray = ""
+
 for ( let i = 0; i < (BandaZimbra.AlgunsAlbuns.length); i++){
     stringarray += BandaZimbra.AlgunsAlbuns[i] + ","
 }
@@ -73,38 +129,30 @@ stringarray = ""
 
 //2 - adicionar um objeto ao array de objetos 
 
-const ImagineDragons = [{
-    Artista: "Imagine Dragons", 
-    isArtista : true,
-    QuantosAlbuns: 09, 
-    TemMaisDe5anos: 12 > 5, 
-    AlgunsAlbuns:["Mercury - Act 2", "Nigth Visions", "Evolve", "It's Time"]
-
-}]
-ArmarioArtistas.push(ImagineDragons[0])
-
-for(i = 0; i<=(ArmarioArtistas.length-1); i++){
-    console.log(ArmarioArtistas[i])
-}
+function toUpperCase () {
 
 for(let artistass of ArmarioArtistas){
     console.log(`${artistass.Artista.toUpperCase()},\n ${artistass.isArtista}, \n ${artistass.TemMaisDe5anos}, \n ${artistass.AlgunsAlbuns}`);
 }
-
-// faça uma função para que o objeto pedido seja encontrado com a string do relatorio com os dados do objt
-function encontrarBanda (){
-    for(let artistass of ArmarioArtistas){
-        console.log(`${artistass.Artista.toUpperCase()},\n ${artistass.isArtista}, \n ${artistass.TemMaisDe5anos}, \n ${artistass.AlgunsAlbuns}`);
-    }
 }
 
-    console.log(encontrarBanda())
-//
-//const arrayobjt = [{BandaZimbra, Miley, ImagineDragons, PedroSampaio}]
+
+
+// SEMANA 6
+
+    for(let artistass of ArmarioArtistas){
+        console.log(`${artistass.nome.toUpperCase()},\n ${artistass.isArtista}, \n ${artistass.TemMaisDe5anos}, \n ${artistass.AlgunsAlbuns}`);
+
+    }
+    
+
+
+
+
 
 function arrayAramarioDeBandas(array, titulo) {
     let resultado = array.filter(arm => {
-        return arm.Artista.toUpperCase().includes(titulo.toUpperCase())
+        return arm.nome.toUpperCase().includes(titulo.toUpperCase())
     })
     if(resultado.length >= 1){
         return resultado
@@ -113,8 +161,24 @@ function arrayAramarioDeBandas(array, titulo) {
     }
 }
 
-arrayAramarioDeBandas(ArmarioArtistas, "Banda Zimbra")
+function buscarItens (arrayObj, string) {
+    let busca = document.getElementById("nome");
+    string = busca.value
 
-//4//
+    let container = document.getElementById("todos");
 
-// encontrarBanda(prompt("Qual banda/artista deseja encontrar?"))
+    for(objeto of arrayObj){
+        if(objeto.nome === string){
+            return container.innerHTML = objeto.html.innerHTML
+        }
+    }
+    return alert(`Seu produto ${string} não foi encontrado!`)
+
+    if(!string) {
+        return alert("Faça uma busca valida")
+    }
+
+} 
+buscarItens(arrayAramarioDeBandas, "Banda Zimbra")
+
+
