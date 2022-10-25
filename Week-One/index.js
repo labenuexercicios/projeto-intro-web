50e7b612f0dbaaa497b21e65127304b0c768c783
cblouzada <minhavidaemfloripa@hotmail.com>
2022-10-24 22:04:57 -0300

    Semana um

---
+++ Week-One/index.js
@@ -0,0 +1,45 @@
+// ##########    Semana 1 do projeto intro web #####################
+
+
+// *********  Exercicio One  ********
+
+
+const namePlayerOne = ("Michael Jordan")
+const heigthPlayerOne = 198;
+const stillPlayOne = false;
+
+// console.log(nameplayer, heigthplayer, playerOne);
+
+
+// *********  Exercicio Two  ********
+
+
+const nameplayerTwo = ("Lebron James")
+const heightplayerTwo = 206 ;
+const stillPlayTwo = true;
+
+const nameplayerThree = ("Shaquille O'Neal")
+const heigthplayerThree = 216;
+const stillPlayThree = false;
+
+const nameplayerFour = ("Allen Iverson")
+const heigthplayerFour = 183;
+const stillPlayFour = true;
+
+// *********  Exercicio Three  ********
+const heightStandard = 185
+
+const comparingHeightOne = heigthPlayerOne >= heightStandard
+console.log(` ${namePlayerOne.toUpperCase()} é maior que 185 ? ${comparingHeightOne}`);
+
+const comparingHeightTwo = heigthPlayerOne >= heightStandard
+console.log(comparingHeightTwo);
+
+const comparingHeightThree = heigthPlayerOne >= heightStandard
+console.log(comparingHeightThree);
+
+const comparingHeightFour = heigthPlayerOne >= heightStandard
+console.log(comparingHeightFour);
+
+
+
