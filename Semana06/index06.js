43561be8a0da94aedd5710a3273e9446a6fb8830
Pedro Maia Filho <pedromaiaf21@gmail.com>
2022-10-25 22:00:12 -0400

    finalizado 90%

--- Semana06/index06.js
+++ Semana06/index06.js
@@ -1,37 +1,37 @@
 //Como estava na semana 02(concluída):
 
 objetoPizzaSal01 = {
-    // foto: ''
-    // fonte:
+    foto: "./mídiasparasite/Pizza-calabresaemozarela.png",
+    // fonte:,
     pizza: "Pizza de calabresa com mozarela",
     codigo: "01",
     tempoPreparo: 60,
     pizzaSalgada: true,
-    ingredientes: ["🍕 1 receita de massa para pizza de forno","🍕 1 receita de molho básico de tomate","🍕 300g de calabresa fatiada","🍕 400g de queijo mozarela fatiado"]
+    ingredientes: ["🍕 1 receita de massa para pizza de forno", "🍕 1 receita de molho básico de tomate", "🍕 300g de calabresa fatiada", "🍕 400g de queijo mozarela fatiado"]
 }
 //console.log(objetoPizzaSal01)
 //console.table(objetoPizzaSal01)
 
 //Pizza 02:
 objetoPizzaSal02 = {
-    // foto:
+    foto: "./mídiasparasite/Pizza-quatroqueijos.png",
     pizza: "Pizza de quatro queijos",
     codigo: "02",
     tempoPreparo: 60,
     pizzaSalgada: true,
-    ingredientes: ["🍕 1 receita de massa para pizza de forno","🍕 1 receita de molho básico de tomate","🍕 100g de queijo parmesão","🍕 150g de queijo provolone","🍕 150g de queijo mozarela","🍕 100g de queijo gorgonzola"]
+    ingredientes: ["🍕 1 receita de massa para pizza de forno", "🍕 1 receita de molho básico de tomate", "🍕 100g de queijo parmesão", "🍕 150g de queijo provolone", "🍕 150g de queijo mozarela", "🍕 100g de queijo gorgonzola"]
 }
 //console.log(objetoPizzaSal02)
 //console.table(objetoPizzaSal02)
 
 //Pizza 03:
 objetoPizzaSal03 = {
-    // foto:
+    foto: "./mídiasparasite/Pizza-abobrinhacomqueijo.png",
     pizza: "Pizza de abobrinha",
     codigo: "03",
     tempoPreparo: 50,
     pizzaSalgada: true,
-    ingredientes: ["🍕 1 receita de massa para pizza de forno","🍕 1 receita de molho básico de tomate","🍕 2 abobrinhas cortadas em rodelas finas","🍕 300g de queijo mozarela de búfala ralada ou cortada em fatias","🍕 1 dente de alho amassado","🍕 4 colheres de sopa de azeite","🍕 Sal a gosto"]    
+    ingredientes: ["🍕 1 receita de massa para pizza de forno", "🍕 1 receita de molho básico de tomate", "🍕 2 abobrinhas cortadas em rodelas finas", "🍕 300g de queijo mozarela de búfala ralada ou cortada em fatias", "🍕 1 dente de alho amassado", "🍕 4 colheres de sopa de azeite", "🍕 Sal a gosto"]
 }
 //console.log(objetoPizzaSal03)
 //console.table(objetoPizzaSal03)
@@ -39,12 +39,12 @@ objetoPizzaSal03 = {
 //Pizza 04:
 //Tipos de pizza doces:
 objetoPizzaDoce01 = {
-    // foto:
+    foto: "./mídiasparasite/Pizza-chocolate.png",
     pizza: "Pizza de chocolate",
     codigo: "04",
     tempoPreparo: 60,
     pizzaSalgada: false,
-    ingredientes: ["🍕 2 xícaras (de chá) de chocolate forneável ao leite","🍕 1 disco de massa doce para pizza - pré assado","🍕 1 colher de sopa de chocolate em pó","🍕 1/2 xícara (de chá) de chocolate granulado (para povilhar)","🍕 1/2 xícara (de chá) de cereja em calda (para decorar)"]
+    ingredientes: ["🍕 2 xícaras (de chá) de chocolate forneável ao leite", "🍕 1 disco de massa doce para pizza - pré assado", "🍕 1 colher de sopa de chocolate em pó", "🍕 1/2 xícara (de chá) de chocolate granulado (para povilhar)", "🍕 1/2 xícara (de chá) de cereja em calda (para decorar)"]
 }
 //console.log(objetoPizzaDoce01)
 //console.table(objetoPizzaDoce01)
@@ -150,8 +150,8 @@ console.log(pizzaArraySaborosas0)
 
 //ESSE É O QUE VALE DE VERDADE! SEGUE ABAIXO:
 console.log("Exercício 2")
-for(pizzaString of pizzaArraySaborosas0){
-    for (propriedadeObjeto in pizzaString){
+for (pizzaString of pizzaArraySaborosas0) {
+    for (propriedadeObjeto in pizzaString) {
         console.log(`${propriedadeObjeto}: ${pizzaString[propriedadeObjeto]}`)
     }
 }
@@ -162,18 +162,18 @@ for(pizzaString of pizzaArraySaborosas0){
 console.log("Exercício 3")
 let saborDePizza = "Pizza de calabresa com mozarela"
 
-const separarSabores = pizzaArraySaborosas0.filter((substitutoPizza) =>{
-    return substitutoPizza.pizza===saborDePizza
-} )
+const separarSabores = pizzaArraySaborosas0.filter((substitutoPizza) => {
+    return substitutoPizza.pizza === saborDePizza
+})
 console.log(separarSabores)
 
 
 const pizzasSalgadas = (saborosasSalgadas) => {
-    for(let i = 0; i < saborosasSalgadas.length; i++){
+    for (let i = 0; i < saborosasSalgadas.length; i++) {
         console.log(`${saborosasSalgadas[i].pizza}, ${saborosasSalgadas[i].tempoPreparo}, ${saborosasSalgadas[i].pizzaSalgada}, ${saborosasSalgadas[i].ingredientes}`)
     }
 }
- pizzasSalgadas(separarSabores)
+pizzasSalgadas(separarSabores)
 
 
 // 4. Crie uma função que recebe um array de objetos e uma string. Esta função deve retornar um objeto, 
@@ -183,8 +183,10 @@ const pizzasSalgadas = (saborosasSalgadas) => {
 console.log("Exercício 4")
 const inputPizza = document.getElementById("pizzaBuscador")
 const inputCodigoPizza = inputPizza.value
-function clicadoPesquisar () {console.log(inputPizza.value)
-     recebeArrayObjetos(pizzaArraySaborosas0, inputPizza.value)
+function clicadoPesquisar() {
+    // consolelog que me ajuda a ver o valor exato que está sendo impresso:
+    console.log(inputPizza.value)
+    recebeArrayObjetos(pizzaArraySaborosas0, inputPizza.value)
 }
 const recebeArrayObjetos = (arrayInformado, codigoInformado) => {
     const resultado = arrayInformado.filter((pizza) => {
@@ -196,7 +198,7 @@ const recebeArrayObjetos = (arrayInformado, codigoInformado) => {
     } else {
         //  alert("Nenhuma pizza de anchovas encontrada")
     }
-    
+
 }
 
 
@@ -206,35 +208,119 @@ const recebeArrayObjetos = (arrayInformado, codigoInformado) => {
 //----------------------------------------------------------------------------------------------------------------------------------------------//
 // SEMANA 06 - PASSANDO TUDO POR MEIO DO DOM
 
-// const minhasPizzas = document.getElementsByClassName("ajusteIngredientes")
+const minhasPizzas = document.getElementsByClassName("ajusteIngredientes")
 
-// console.log(minhasPizzas)
-// for( let i in pizzaArraySaborosas0) {
-// for(let j in pizzaArraySaborosas0[i].ingredientes){
-//     const criandoLi = document.createElement("li")
-//     const conteudoNovo = document.createTextNode(pizzaArraySaborosas0[i].j)
-//     console.log(pizzaArraySaborosas0[0].ingredientes[j])
-//     criandoLi.innerText= pizzaArraySaborosas0[0].ingredientes[j]
-//     minhasPizzas[0].insertAdjacentElement('beforeend', criandoLi)}
-    
-// }
-// console.log("A")
+console.log(minhasPizzas)
+for (let i in objetoPizzaSal01.ingredientes) {
+    let linhasIngrendientes = document.createElement("li")
+    const novoConteudoPizza = document.createTextNode(objetoPizzaSal01.ingredientes[i])
+    linhasIngrendientes.appendChild(novoConteudoPizza)
+    minhasPizzas[0].insertAdjacentElement("beforeend", linhasIngrendientes)
+    console.log(minhasPizzas)
+}
+for (let i in objetoPizzaSal02.ingredientes) {
+    let linhasIngrendientes = document.createElement("li")
+    const novoConteudoPizza = document.createTextNode(objetoPizzaSal02.ingredientes[i])
+    linhasIngrendientes.appendChild(novoConteudoPizza)
+    minhasPizzas[1].insertAdjacentElement("beforeend", linhasIngrendientes)
+    console.log(minhasPizzas)
+}
+for (let i in objetoPizzaSal03.ingredientes) {
+    let linhasIngrendientes = document.createElement("li")
+    const novoConteudoPizza = document.createTextNode(objetoPizzaSal03.ingredientes[i])
+    linhasIngrendientes.appendChild(novoConteudoPizza)
+    minhasPizzas[2].insertAdjacentElement("beforeend", linhasIngrendientes)
+    console.log(minhasPizzas)
+}
+for (let i in objetoPizzaDoce01.ingredientes) {
+    let linhasIngrendientes = document.createElement("li")
+    const novoConteudoPizza = document.createTextNode(objetoPizzaDoce01.ingredientes[i])
+    linhasIngrendientes.appendChild(novoConteudoPizza)
+    minhasPizzas[3].insertAdjacentElement("beforeend", linhasIngrendientes)
+    console.log(minhasPizzas)
+}
 
-// Criando os elementos:
-// const criandoDivPaiDasPizzas = document.createElement('div')
-// const criandoDivPadraoContainerPizzas = document.createElement('div')
-// const criandoImg = document.createElement("img")
-// const criandoH2 = document.createElement("h2")
-// const criandoH3 = document.createElement("h3")
-// const criandoUl = document.createElement("ul")
 
+let linhasIngrendientes01 = document.createElement("li")
+const novoConteudoPizza01 = document.createTextNode(`⏲️Tempo de preparo:${objetoPizzaSal01.tempoPreparo}min`)
+linhasIngrendientes01.appendChild(novoConteudoPizza01)
+minhasPizzas[0].insertAdjacentElement("beforeend", linhasIngrendientes01)
+console.log(minhasPizzas)
 
-// const elementosCriados = [criandoDivPaiDasPizzas, criandoDivPaiDasPizzas, 
-//     criandoImg, criandoH2, criandoH3, criandoUl, criandoLi]
-// console.log(elementosCriados)
+let linhasIngrendientes02 = document.createElement("li")
+const novoConteudoPizza02 = document.createTextNode(`⏲️Tempo de preparo:${objetoPizzaSal02.tempoPreparo}min`)
+linhasIngrendientes02.appendChild(novoConteudoPizza02)
+minhasPizzas[1].insertAdjacentElement("beforeend", linhasIngrendientes02)
+console.log(minhasPizzas)
 
+let linhasIngrendientes03 = document.createElement("li")
+const novoConteudoPizza03 = document.createTextNode(`⏲️Tempo de preparo:${objetoPizzaSal03.tempoPreparo}min`)
+linhasIngrendientes03.appendChild(novoConteudoPizza02)
+minhasPizzas[2].insertAdjacentElement("beforeend", linhasIngrendientes03)
+console.log(minhasPizzas)
 
+let linhasIngrendientes04 = document.createElement("li")
+const novoConteudoPizza04 = document.createTextNode(`⏲️Tempo de preparo:${objetoPizzaDoce01.tempoPreparo}min`)
+linhasIngrendientes04.appendChild(novoConteudoPizza02)
+minhasPizzas[3].insertAdjacentElement("beforeend", linhasIngrendientes04)
+console.log(minhasPizzas)
 
+//------------------------------
 
+const paiPizzasHtml = document.querySelectorAll(".paiDasPizzas")
+const padraoPizzasHtml = document.querySelectorAll(".padraoContainerPizzas")
+const imagemPizzasHtml = document.querySelectorAll(".imagensPizza")
+const tituloPizzasHtml = document.querySelectorAll(".tituloPizza")
+const ingredientePizzasHtml = document.querySelectorAll(".ingredientePizza")
+const ajusteIngredientesPizzasHtml = document.querySelectorAll(".ajusteIngredientes")
+// -----------------
+const removerpaiPizza01 = document.querySelector("#pizza01")
+const paiPizza01 = removerpaiPizza01.parentNode
 
+const removerpaiPizza02 = document.querySelector("#pizza02")
+const paiPizza02 = removerpaiPizza02.parentNode
 
+const removerpaiPizza03 = document.querySelector("#pizza03")
+const paiPizza03 = removerpaiPizza03.parentNode
+
+const removerpaiPizza04 = document.querySelector("#pizza04")
+const paiPizza04 = removerpaiPizza04.parentNode
+// ----------
+function removerPizza() {
+    if (objetoPizzaSal01.codigo === inputPizza.value) {
+        paiPizza02.removeChild(removerpaiPizza02)
+        paiPizza03.removeChild(removerpaiPizza03)
+        paiPizza04.removeChild(removerpaiPizza04)
+        inputPizza.value = ""
+    } else if (objetoPizzaSal02.codigo === inputPizza.value) {
+        removerpaiPizza01.parentNode.removeChild(removerpaiPizza01)
+        removerpaiPizza03.parentNode.removeChild(removerpaiPizza03)
+        removerpaiPizza04.parentNode.removeChild(removerpaiPizza04)
+        inputPizza.value = ""
+    } else if (objetoPizzaSal03.codigo === inputPizza.value) {
+        removerpaiPizza01.parentNode.removeChild(removerpaiPizza01)
+        removerpaiPizza02.parentNode.removeChild(removerpaiPizza02)
+        removerpaiPizza04.parentNode.removeChild(removerpaiPizza04)
+        inputPizza.value = ""
+    } else if (objetoPizzaDoce01.codigo === inputPizza.value) {
+        removerpaiPizza01.parentNode.removeChild(removerpaiPizza01)
+        removerpaiPizza02.parentNode.removeChild(removerpaiPizza02)
+        removerpaiPizza03.parentNode.removeChild(removerpaiPizza03)
+        inputPizza.value = ""
+    } else {
+        inputPizza.value = ""   
+        alert("nenhuma pizza encontrada")
+       
+    }
+
+    // fazer das outras pizzas conforme acima e acrescentar 
+}
+
+function devolverPesquisaDasPizzas() {
+    if (inputPizza.value === "") {
+        paiPizza01.appendChild(removerpaiPizza01)
+        paiPizza02.appendChild(removerpaiPizza02)
+        paiPizza03.appendChild(removerpaiPizza03)
+        paiPizza04.appendChild(removerpaiPizza04)
+    }
+}
