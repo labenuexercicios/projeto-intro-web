c79161cbd7fded10e6daa1dcdcc43eb44b02bc0f
Peeox <pedrofelippe644@gmail.com>
2022-10-25 22:42:29 -0300

    Projeto semana 1

---
+++ script.js
@@ -0,0 +1,29 @@
+/*Pense em alguma coisa do cotidiano que seja contável e estruturada. 
+Por enquanto, vamos chamar esta coisa de item. Com esse item, nós vamos 
+construir nossas lógicas e evoluir no projeto através das semanas. 
+Pode ser qualquer coisa que possa ser descrita com características 
+escritas.
+
+1. Para o item que você decidiu, pense nas características 
+que são importantes para construí-lo. 
+Vamos usar estas características para definir exemplos destes objetos 
+usando o código.
+Você precisará criar pelo menos:
+  * uma característica String;
+  * uma característica Number;
+  * uma característica Boolean;*/
+
+const nome1 = `Beetlejuice - Os Fantasmas se divertem`;
+const tempo1 = 92;
+const temJohnnyDepp1 = false; 
+const elenco1 = [`Michael Keaton`, ` Winona Ryder`, ` Alec Baldwin`, ` Geena Davis`]
+
+const nome2 = `Batman`;
+const tempo2 = 126;
+const temJohnnyDepp2 = false;
+const elenco2 = [`Michael Keaton`, ` Jack Nicholson`, ` Pat Hingle`, ` Michael Gough`]
+
+const nome3 = `Edward mãos de tesoura`;
+const tempo3 = 105;
+const temJohnnyDepp3 = true;
+const elenco3 = [`Winona Ryder`, ` Johnny Depp`, ` Dianne Wiest`, ` Kathy Baker`]
