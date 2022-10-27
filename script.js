f1246379a7806e01943a8616c4cfe9226181a8fc
Millyana.sg <millyanasg@gmail.com>
2022-10-27 19:07:03 -0300

    Projeto concluído

--- script.js
+++ script.js
@@ -1,102 +1,180 @@
-//SEMANA 3
-
-// 1. Reescrevendo o relatório criado utilizando console.log() que começamos na semana 2, altere a forma que a característica de array dos itens seja escrita como um laço que guarde todos os valores da propriedade array do objeto em uma mesma string. Utilize esta string no relatório.
-
- array = [{
- Personagem: "Saci-Pererê",
-personagemProtetor : false,  
-localidade : "mata",
-anoDaLenda : 1918,
-elementoConjunto : ["Redemoinho", "Velocidade", "Invisibilidade"]
-},
-
-{
- Personagem : "Lobisomem",
- personagemProtetor : true,
- localidade : "cidade do interior",
- anoDaLenda : 1400,
- elementoConjunto : ["Super-Força", "Velocidade", "Ressucitar", "Mudar de aparencia"]
-},
-
-{
-Personagem : "Boto-Rosa",
-personagemProtetor : false,
-localidade : "lago",
-anoDaLenda : 1800,
-elementoConjunto : ["Mudar aparencia", "invisilidade", "Ressucitar"]
-},
-
-{
-Personagem : "Cuca",
-personagemProtetor : false,
- localidade : "cidade grande",
- anoDaLenda: 1921,
- elementoConjunto : ["Super mordida", "Disfarces", "Poderes de feitiço"]
-},
-
-{
-Personagem : "Caipora",
-personagemProtetor : true,
-localidade : "mata fechada",
-anoDaLenda: 1870,
-elementoConjunto : ["Proteção", "Velocidade", "Super audição"]
-
-},
-{
- Personagem : "Mula sem Cabeça",
- personagemProtetor : false,
- localidade : "cidade",
- anoDaLenda: 1972,
- elementoConjunto : ["Fogo", "Velocidade", "Invisibilidade"]
-
- },
-
-{
- Persoanagem : "Curupira",
- personagemProtetor : true,
-localidade : "mata",
-anoDaLenda : 1560,
-elementoConjunto : ["velocidade", "camuflagem com a floresta", "visão de raio - X"]
- },
-
- {
-Personagem : "Iara",
-personagemProtetor : true,
- localidade : "mar",
- anoDaLenda: 1600,
- elementoConjunto : ["hipnose pela voz", "super-audição", "camuflagem com o mar"]
- }]
+const cards =[]
 
 
-const novoArray =[]
-for (let i in array){
-   if (array[i].personagemProtetor){
-      novoArray.push(array[i])
-   } else {
-      alert (`${array[i].Personagem} Não foi adicionado!`)
-   }
+let Personagem1 = {
+    nome : "Lobisomem",
+    Imagem1 : 'imagens/lobisomen1.png',
+    Imagem2 : 'imagens/lobisomen.png',
+   texto : "O Lobisomen é um homem que trabalha de dia, e de noite se transforma em um simpático lobo. Ele vive no interior do país, e nas noites de lua cheia gosta de uivar no topo da montanha, antes de encontrar os animais da floresta para contar histórias do nosso Brasil. É homem lobo contador de histórias do folclore brasileiro."
+
 }
 
+let Personagem2 = {
+    nome : "Iara",
+    Imagem1 : 'imagens/iara1.png',
+    Imagem2 : 'imagens/iara2.jpg',
+    texto : "É uma sereia que vive nas águas amazônicas.Com longos cabelos pretos e olhos castanhos, a sereia Iara emite uma melodia que atrai os homens, os quais ficam rendidos e hipnotizados com seu canto e sua voz doce. Dependendo da região brasileira, a representação da índia pode diferir, por exemplo, na cor dos olhos e dos seus cabelos, que ora são escuros, ora são claros."
 
-// 2. Ainda no relatório, altere-o para que ele seja criado utilizando laços. Ou seja, você não deve mais imprimir individualmente cada item do relatório. Cada item deve ser exibido a partir de uma iteração do laço. Para testar, adicione mais um item ao array de objetos, e verifique se ele é exibido corretamente.
-
-
-for(let i in array){
-   array[i].elementoConjunto = array[i].elementoConjunto.toString()
-   console.log(array[i])
 }
-// 3. Crie uma função que receba como parâmetro um objeto, e devolva a string do relatório com os dados do objeto.
-
-function obj (objeto){
-   return objeto.Personagem
+let Personagem3 = {
+   nome : "Curupira",
+   Imagem1 : 'imagens/curupira1.png',
+   Imagem2: 'imagens/curupira2.jpg',
+   texto : 'Curupira é um personagem descrito como um anão forte e ágil de cabelos ruivos que possui os pés virados para trás. Assim, ao caminhar, o curupira consegue enganar alguém que pretenda segui-lo olhando para suas pegadas. O perseguidor pensará sempre que ele foi na direção contrária. A lenda afirma que o Curupira vive na mata fazendo travessuras, sendo considerado o protetor das florestas.'
 }
-// 4. Crie uma função que recebe um array de objetos e uma string. Esta função deve retornar um objeto, e o objeto retornado deve possuir apenas os itens que tenham o nome/título igual à string passada como parâmetro. Caso não exista um item, exiba um ALERT indicando que nenhum item foi encontrado.
 
-function busca (array,string){
-     for(let i in array){
-       if (array[i].Personagem === string){
-          return array[i]
-          }
-       }  
-         return alert(`O ${string}, não foi encontrado!`)
+let Personagem4 = {
+   nome : "Caipora",
+   Imagem1: 'imagens/caipora.png',
+   Imagem2 :'imagens/caipora2.jpg',
+   texto : 'Caipora é uma índia anã. Ela vive nas florestas e tem o poder de dominar e ressuscitar os animais. Faz armadilhas e confunde os caçadores. Emitindo diversos ruídos, ela distrai os caçadores oferecendo pistas falsas que eles se percam na floresta. Ela também tem o poder de controlar os animais e, os espanta quando sente que algo de mal pode acontecer.'
+}
+
+let Personagem5 = {
+   nome:'Cuca',
+   Imagem1: 'imagens/cuca1.png',
+   Imagem2: 'imagens/cuca1.png',
+   texto:'Trata-se de uma bruxa velha com aparência assustadora que possui cabeça de jacaré e unhas imensas. Dona de uma voz assustadora, a Cuca rapta as crianças desobedientes. Reza a lenda que a bruxa Cuca dorme uma vez a cada sete anos. Por isso, os pais tentam convencer as crianças a dormirem nas horas corretas, pois, do contrário, serão levadas pela Cuca.'
+}
+
+let Personagem6 = {
+   nome:'Mula Sem Cabeça',
+   Imagem1: 'imagens/mulaFrente.png',
+   Imagem2: 'imagens/mulaverso.jpg',
+   texto : 'A lenda da mula sem cabeça conta a história de uma burrinha de cor preta ou marrom, que em lugar da cabeça tem uma tocha de fogo. A burrinha possui ferraduras de aço ou prata e relincha tão alto que se ouve a muitos metros de distância. Também é comum ouvir o animal soluçando como um ser humano. Diz-se que a mula costuma correr pelas matas e campos assustando pessoas e animais.'
+}
+
+let Personagem7 = {
+   nome :'Saci-Pererê',
+   Imagem1:'imagens/saci1.png',
+   Imagem2 :'imagens/saci2.jpg',
+   texto : 'O Saci-pererê é um personagem muito travesso que se diverte fazendo brincadeiras com os animais e com as pessoas. De acordo com as estórias, as suas principais travessuras são fazer tranças no rabo dos animais durante a noite, esconder objetos (como os dedais das costureiras), assobiar de maneira muito estridente para assustar os viajantes, trocar o recipiente de sal pelo de açúcar e distrair as cozinheiras para elas queimarem a comida.'
+}
+
+let Personagem8= {
+   nome :'Boto Rosa',
+   Imagem1:'imagens/boto1.png',
+   Imagem2:'imagens/boto2.jpg',
+   texto : 'Reza a lenda que o boto-cor-de-rosa se transforma num jovem belo e elegante nas noites de lua cheia. Ele aparece nas festividades de junho, nas comemorações dos Santos Populares, as chamadas Festas Juninas. Vem vestido de branco e com um grande chapéu a fim de esconder suas narinas, pois sua transformação não ocorre totalmente.'
+}
+
+let Personagem9 = {
+   nome: "Boitatá",
+   Imagem1: 'imagens/boitataf.png',
+   Imagem2: 'imagens/boitata2.jpg',
+   texto : 'A lenda do Boitatá descreve esse personagem folclórico como uma grande serpente de fogo. Ele protege os animais e as matas das pessoas que lhe fazem mal e, principalmente, que realizam queimadas nas florestas.'
+}
+
+let Personagem10 = {
+   nome : ' Boi Bumbá',
+   Imagem1 :'imagens/boibumba1.png',
+   Imagem2:'imagens/boibumba2.jpg',
+   texto : 'O bumba meu boi é uma manifestação artística e popular do folclore brasileiro. É reconhecido como Patrimônio Cultural Imaterial da Humanidade, pela Organização das Nações Unidas para a Educação, a Ciência e a Cultura (Unesco), e como Patrimônio Cultural do Brasil, pelo Instituto do Patrimônio Histórico e Artístico Nacional (Iphan).'
+}
+
+let Personagem11 ={
+   nome : 'Negrinho do Pastoreiro',
+   Imagem1 :'imagens/n.pastoreiroFrente1.png',
+   Imagem2 : 'imagens/n.pastoreiro.png',
+   texto : 'O Negrinho do Pastoreio é um personagem do folclore brasileiro muita conhecido na região sul do país. Acredita-se que se algum objeto está perdido, o Negrinho do Pastoreio pode ajudar a encontrá-lo. Basta acender uma vela perto de um formigueiro e pedir com muita fé que objeto reaparecerá..'
+}
+
+let Personagem12 = {
+   nome : 'Vitória Régia',
+   Imagem1 :'imagens/ritoriaregia.png',
+   Imagem2 : 'imagens/vitoriaregia2.jpg',               
+   texto: 'Segundo essa lenda indígena e amazônica, a vitória-régia é originalmente uma índia que se afogou após se inclinar no rio para tentar beijar o reflexo da lua. Para os índios, a lua era Jaci, divindade da Lua na mitologia tupi, por quem a índia estava apaixonada.'
+}
+cards.push(Personagem1, Personagem2, Personagem3, Personagem4, Personagem5, Personagem6, Personagem7, Personagem8, Personagem9, Personagem10, Personagem11, Personagem12)
+
+
+
+const cardPai = document.querySelector("#containerPai")
+
+
+let nomeDigitado 
+cards.map(personagem=>{
+    cardPai.innerHTML+= `
+
+     <div class="card">
+         <div class=" card-interno">
+             <div class=" face-card  card-frente">
+                 <div class="imagem">
+                     <img src="./${personagem.Imagem1}" alt ="" class="imagem-principal">
+                     <h2>${personagem.nome}</h2>
+                 </div>
+             </div>
+             <div class="face-card card-verso">
+                 <div class ="texto">
+                     <div class="cabecalho-card">
+                         <img src="${personagem.Imagem2}" alt="" class="pp">
+                         <h3>${personagem.nome}</h3>
+                     </div>
+                     <div class="corpo-card">
+                         <p>${personagem.texto}</p>
+    
+                     </div>
+             </div>
+         </div>
+     </div>
+     </div>
+     
+     `
+})
+const valorInput = document.getElementById('leitura')
+const busca = document.getElementsByTagName('h2')
+const cardsN = document.querySelectorAll('.card')
+
+
+
+
+
+let nomesCard = []
+
+
+for (let i in busca){
+    nomesCard.push(busca[i].innerText)
+}
+  let nomesCards2 = nomesCard.filter((itens) => 
+     itens!== undefined
+  )
+
+
+ nomesCards2 = nomesCards2.map((itens) =>
+     itens.toLowerCase()
+ )
+
+const pesquisar = (event) => {
+    
+    const textoDePesquisa = valorInput.value.toLowerCase()
+    event.preventDefault()
+    cardsN.forEach((itens) =>{
+        itens.classList.add("sumir")
+        itens.classList.remove("centralizar")
+     
+    })
+
+    for (let i in cardsN){
+      if (nomesCards2[i] === textoDePesquisa){
+         cardsN[i].classList.remove("sumir")
+         cardsN[i].classList.add("centralizar")
       }
+    }
+    const mensagem = nomesCards2.includes(textoDePesquisa)
+    const limpar = () =>{
+        if (textoDePesquisa === ""){
+            cardsN.forEach((itens) =>
+                itens.classList.remove("sumir")
+            )
+        } else if (mensagem === false){
+            alert ("Personagem não encontrado!")
+         }
+    } 
+    limpar()
+}
+
+
+
+
+
+
