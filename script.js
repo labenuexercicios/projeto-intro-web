ef1b5ed9fed8905ebb2fdae149a5fd43ea71a6e1
MailsonAndradeCardoso <mailsoncardoso1988@gmail.com>
2022-10-28 10:14:13 -0300

    projeto concluido

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 //Proejto Intro Webb - semana 1
 //1
 const receitas = ["Strogonof", "Lasanha", "Vegetariano"]
-const quantidadeIngredientes = [6, 5, 5]
+const quantidadeIngredientes = [12, 7, 9]
 const ehvegetariana = false
 
 //2
 let receita1 = "Strogonoff"
-let quantitadeIngredientes1 = 6
+let quantitadeIngredientes1 = 12
 let ehvegetariana1 = false
 
 let receita2 = "Lasanha"
-let quantitadeIngredientes2 = 4
+let quantitadeIngredientes2 = 7
 let ehvegetariana2 = false
 
 let receita3 = "Vegetariano"
-let quantidadeIngredientes3 = 5
+let quantidadeIngredientes3 = 9
 let ehvegetariana3 = true
 
 //3
@@ -58,18 +58,19 @@ receita3 = {
 
 //2
 const receita = []
+/* console.log("receita", receita) */
 
 //3
-//receitas.push(receita1, receita2, receita3)
+receitas.push(receita1, receita2, receita3)
 
 //4
 if(receita1 === receita2 === receita3){
     console.log(receita.push(receita1, receita2, receita3))
 } else{
-    alert("O item nao foi adicionado")
+   /*  alert("O item nao foi adicionado") */
 }
 
-//Proejto Intro Webb - Semana 3
+
 //1
 for(i = 0; i < receitas.length; i++){
     console.log(receitas[i])
@@ -92,11 +93,11 @@ recebeObjeto(receita2.nome)
 
 
 
-
-let pesquisa = Number(0)
+/* REFAZER SEMANA 4 */
+/* let pesquisa = Number(0)
 function recebeObjetosStrings(receita, stringFunction)
 {
-    pesquisa = +0
+    pesquisa += 0
     for(i=0; i<receita.length; i++) 
     {    
         if(receita[i]. receita1 === stringFunction)
@@ -109,16 +110,36 @@ function recebeObjetosStrings(receita, stringFunction)
     if(pesquisa !== 1)
     {
     return alert("ALERT item não encontrado")
-    }
-}
+    } */
 
-let frase = prompt("digite algo")
-recebeObjetosStrings(receita, frase)
+
+/* let frase = prompt("digite algo")
+recebeObjetosStrings(receita, frase) */
 
 //Proejto Intro Webb - Semana 4
 
 
+let arrayDeReceitas = [
 
-
-
-
+    {nome:"Strogonoff de Carne", ingredientes: "500 gramas de carne, 1 pitada de pimenta-do-reino, 3 colheres (sopa) de manteiga, meia cebola, 1 tablete de MAGGI Caldo Carne, 4 colheres (sopa) de água fervente, meia xícara (chá) de conhaque, 200 g de champignons em conserva fatiados, 3 colheres (sopa) de purê de tomate, 1 colher (sopa) de mostarda, 2 colheres (sopa) de ketchup, 1 lata Creme de Leite",
+    
+    },
+    
+    {nome:"Risoto vegetariano", ingredientes: "4 xícaras (chá) de água, 2 tabletes de Caldo Legumes, 3 colheres (sopa) de manteiga, 1 xícara (chá) de arroz integral tipo agulinha, 2 colheres (sopa) de semente de linhaça, 1 pitada de açafrão, 1 cenoura em cubos, 1 xícara (chá) de brócolis aferventados, 1 caixa de creme de leite"
+    
+    }
+]
+function userInput() {
+    let digitado = document.getElementById('user-input').value;
+  
+    let container = document.getElementById('receitas');
+  
+    container.innerHTML = '';
+    let receitasEscolhidas = receitas.filter(
+      receita => receita.name.toLowerCase().includes(digitado.toLowerCase())
+    );
+  
+    for (let receita of receitasEscolhidas) {
+      container.innerHTML += `<div> ${receita.name} </div>`;
+    }
+  }
