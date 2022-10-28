60b54a3a0cdf6e0c65090a16c428dad9a7a0840f
Juliano Amaral Costa <jaygovinda@MacBook-Air-de-Juliano.local>
2022-10-28 12:57:32 -0300

    semana6feito

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const livro1 = {
   titulo: "Fundação",
   genero: "Ficção Cientifica ",
-  sequecias: 2,
+  valor: 96.00,
   adquirido: true,
   lido: false,
   autor: "Isaac Azimov",
@@ -13,47 +13,47 @@ const livro1 = {
 const livro2 = {
   titulo: "Duna",
   genero: "Ficção Cientifica",
-  sequecias: 4,
+  valor: 58.00,
   adquirido: true,
   lido: false,
   autor: "Frank Herbet",
-  imagem: "./img/fundacao.png"
+  imagem: "./img/duna.jpg"
 };
 
 const livro3 = {
   titulo: "Jurassic Park",
   genero: "Ficção Cientifica ",
-  sequecias: 1,
+  valor: 38.00,
   adquirido: false,
   lido: false,
   autor: "Michael Crichton",
-  imagem: "./img/jurasic.png"
+  imagem: "./img/jurasic.jpg"
 };
 
 const livro4 = {
   titulo: "A Guerra do Velho",
   genero: "Ficção Cientifica",
-  sequecias: 2,
+  valor: 38.00,
   adquirido: true,
   lido: true,
   autor: "John Scalzi",
-  imagem: "./img/fundacao.png"
+  imagem: "./img/guerradovelho.jpg"
 };
 
 const livro5 = {
   titulo: "Bhagavad-Gita: Como Ele É",
   genero: "Filosofia",
-  valor: 80,
+  valor: 120.00,
   adquirido: true,
   lido: true,
   autor: "A. C. Bhaktivedanta Swami Prabhupada",
-  imagem: "./img/gita.png"
+  imagem: "./img/gita.jpg"
 };
 
 const livro6 = {
   titulo: "Jaiva Dharma",
   genero: "Filosofia",
-  valor: 80,
+  valor: 80.00,
   adquirido: true,
   lido: true,
   autor: "Śrīla Bhaktivinoda Ṭhākura",
@@ -63,7 +63,7 @@ const livro6 = {
 const livro7 = {
   titulo: "Joias do Coração",
   genero: "Filosofia",
-  valor: 80,
+  valor: 50.00,
   adquirido: true,
   lido: false,
   autor: "Srila Bhaktivedanta Vana Gosvami Maharaj",
@@ -73,46 +73,51 @@ const livro7 = {
 const livro8 = {
   titulo: "Srimad Bhagavatam, Canto 1, Parte 1",
   genero: "Filosofia",
-  valor: 80,
+  valor: 78.00,
   adquirido: false,
   lido: false,
-  autor: "A. C. Bhaktivedanta Swami Prabhupada"
+  autor: "A. C. Bhaktivedanta Swami Prabhupada",
+  imagem: "./img/srimad.jpg"
 }
 
 const livro9 = {
-  titulo: "Srimad Bhagavatam, Canto 1, Parte 1",
-  genero: "Filosofia",
-  valor: 80,
-  adquirido: false,
-  lido: false,
-  autor: "A. C. Bhaktivedanta Swami Prabhupada"
+  titulo: "Guerra do Trono",
+  genero: "Fantasia",
+  valor: 58.00,
+  adquirido: true,
+  lido: true,
+  autor: "G. R. R. Martin",
+  imagem: "./img/trono.jpg"
 }
 
 const livro10 = {
-  titulo: "Srimad Bhagavatam, Canto 1, Parte 1",
-  genero: "Filosofia",
-  valor: 80,
-  adquirido: false,
-  lido: false,
-  autor: "A. C. Bhaktivedanta Swami Prabhupada"
+  titulo: "A Sociedade do Anel",
+  genero: "Fantasia",
+  valor: 38.00,
+  adquirido: true,
+  lido: true,
+  autor: "J. R. R. Tolkien",
+  imagem: "./img/sociedade.jpg"
 }
 
 const livro11 = {
-  titulo: "Srimad Bhagavatam, Canto 1, Parte 1",
-  genero: "Filosofia",
-  valor: 80,
+  titulo: "A Blumas de Avalon",
+  genero: "Fantasia",
+  valor: 38.00,
   adquirido: false,
   lido: false,
-  autor: "A. C. Bhaktivedanta Swami Prabhupada"
+  autor: "Marion Zimmer Bradley",
+  imagem: "./img/blumas.jpg"
 }
 
 const livro12 = {
-  titulo: "Srimad Bhagavatam, Canto 1, Parte 1",
-  genero: "Filosofia",
-  valor: 80,
+  titulo: "Deuses Americanos",
+  genero: "Fantasia",
+  valor: 62.00,
   adquirido: false,
   lido: false,
-  autor: "A. C. Bhaktivedanta Swami Prabhupada"
+  autor: "Neil Gaiman",
+  imagem: "deuses.jpg"
 }
 
 // Acervo
@@ -132,119 +137,148 @@ livro4.adquirido
   : alert("Guerra do Velho ainda não adquirido");
 livro5.adquirido
   ? acervo.push(livro5)
-  : alert("Jaiva Dharma ainda não adquirido");
+  : alert("Bhagavad-Gita: Como Ele É aninda não adiquirido");
 livro6.adquirido
   ? acervo.push(livro6)
-  : alert("Joias do Coração ainda não adquirido");
-  livro7.adquirido
+  : alert("Jaiva Dharma ainda não adquirido");
+livro7.adquirido
   ? acervo.push(livro7)
-  : alert("Fundação ainda não adquirido");
+  : alert("Joias do Coração ainda não adquirido");
 livro8.adquirido
   ? acervo.push(livro8)
-  : alert("Duna ainda não adquirido");
+  : alert("Srimad Bhagavatam, Canto 1, Parte 1 ainda não adquirido");
 livro9.adquirido
   ? acervo.push(livro9)
-  : alert("Jurassic Park ainda não adquirido");
+  : alert("Guerra do Trono ainda não adquirido");
 livro10.adquirido
   ? acervo.push(livro10)
-  : alert("Guerra do Velho ainda não adquirido");
+  : alert("Sociedade do Anel ainda não adquirido");
 livro11.adquirido
   ? acervo.push(livro11)
-  : alert("Jaiva Dharma ainda não adquirido");
+  : alert("As Blumas de Avalon ainda não adquirido");
 livro12.adquirido
   ? acervo.push(livro12)
-  : alert("Joias do Coração ainda não adquirido");
+  : alert("Deuses Americanos ainda não adquirido");
 
 
 
-console.log(acervo)
+// console.log(acervo)
 
 
 // Livros lidos
 const lidos = [];
 
-livro1.lido
-  ? lidos.push(livro1)
-  : alert("Fundação ainda não lido");
-livro2.lido
-  ? lidos.push(livro2)
-  : alert("Duna ainda não lido");
-livro3.lido
-  ? lidos.push(livro3)
-  : alert("Jurassic Park ainda não lido");
-livro4.lido
-  ? lidos.push(livro4)
-  : alert("A Guerra do Velho ainda não lido");
-livro5.lido
-  ? lidos.push(livro5)
-  : alert("Bhagavad-Gita: Como Ele É ainda não lido");
-livro6.lido
-  ? lidos.push(livro6)
-  : alert("Jaiva Dharma ainda não lido");
-livro7.lido
-  ? lidos.push(livro7)
-  : alert("Joias do Coração ainda não lido");
-  livro8.lido
-  ? lidos.push(livro8)
-  : alert("Joias do Coração ainda não lido");
-console.log(lidos);
-livro9.lido
-  ? lidos.push(livro9)
-  : alert("Fundação ainda não lido");
-livro10.lido
-  ? lidos.push(livro10)
-  : alert("Duna ainda não lido");
-livro11.lido
-  ? lidos.push(livro11)
-  : alert("Jurassic Park ainda não lido");
-livro12.lido
-  ? lidos.push(livro12)
-  : alert("A Guerra do Velho ainda não lido");
+// livro1.lido
+//   ? lidos.push(livro1)
+//   : alert("Fundação ainda não lido");
+// livro2.lido
+//   ? lidos.push(livro2)
+//   : alert("Duna ainda não lido");
+// livro3.lido
+//   ? lidos.push(livro3)
+//   : alert("Jurassic Park ainda não lido");
+// livro4.lido
+//   ? lidos.push(livro4)
+//   : alert("A Guerra do Velho ainda não lido");
+// livro5.lido
+//   ? lidos.push(livro5)
+//   : alert("Bhagavad-Gita: Como Ele É ainda não lido");
+// livro6.lido
+//   ? lidos.push(livro6)
+//   : alert("Jaiva Dharma ainda não lido");
+// livro7.lido
+//   ? lidos.push(livro7)
+//   : alert("Joias do Coração ainda não lido");
+// livro8.lido
+//   ? lidos.push(livro8)
+//   : alert("Joias do Coração ainda não lido");
+// livro9.lido
+//   ? lidos.push(livro9)
+//   : alert("Guerra do Trono ainda não lido");
+// livro10.lido
+//   ? lidos.push(livro10)
+//   : alert("A Sociedade do Anel ainda não lido");
+// livro11.lido
+//   ? lidos.push(livro11)
+//   : alert("As Blumas de Avalon ainda não lido");
+// livro12.lido
+//   ? lidos.push(livro12)
+//   : alert("Deuses Americanos ainda não lido");
 
-function relatorio() {
+function relatorioAcervo() {
   for (unidade of acervo) {
     console.log(unidade)
   }
 }
 
-relatorio()
+relatorioAcervo()
 
 // DOM
 function limpaInput() {
-  document.getElementById('buscador').value= '';
+  document.getElementById('campo-de-busca').value= '';
 }
 
 // Função para buscar no array de objetos
 function buscarArrayPeloNome(array, name) {
-  const total = array.filter((acervo)=> acervo.titulo.toLowerCase().includes(titulo.toLowerCase()));
+  const titulos = array.filter((acervo)=> acervo.titulo.toLowerCase().includes(name.toLowerCase()));
 
-  if(acervo.length){
-    return acervo;
+  if(titulos.length){
+    return titulos;
   } else {
     return array;
   }
 }
 
 // Função que pega o valor do input e chama a função que busca os objetos
-function buscarExpansao(event){
+function buscarLivro(event){
   event.preventDefault()
 
-  const input = document.getElementById('buscador').value;
-  const expansaoFiltro = buscarArrayPeloNome(todasAsExpansoes, input);
+  const input = document.getElementById('campo-de-busca').value;
+  const livroFiltro = buscarArrayPeloNome(acervo, input);
   const footer = document.getElementById('footer');
   const container = document.getElementsByClassName('container')[0];
 
   if(input === ''){
-    alert('Digite um nome de uma expansão válida!');
-    criarObjetosNoHtml(todasAsExpansoes);
+    alert('Digite por favor um titulo válido');
+    criarObjetosNoHtml(todasOsLivros);
     footer.classList.remove('footer-fixado');
     container.removeAttribute('id', 'container');
   } else {
-    console.log(expansaoFiltro);
-    criarObjetosNoHtml(expansaoFiltro);
+    console.log(livroFiltro);
+    criarObjetosNoHtml(livroFiltro);
     footer.classList.add('footer-fixado');
     container.setAttribute('id', 'container');
   }
-  console.log(expansaoFiltro.includes(input)); 
+  console.log(livroFiltro.includes(input)); 
   limpaInput()
 }
+
+// Habilitar o botão enter do teclado com o botão do campo de busca
+const input = document.getElementById('campo-de-busca');
+input.addEventListener("keyup", function(event){
+  if(event.key === 'Enter'){
+    event.preventDefault();
+    document.getElementById('botao-buscar').click()
+  }
+})
+
+// adicionar objetos no HTML
+function criarObjetosNoHtml (acervo) {
+  let container = document.getElementsByClassName('container')[0]
+  container.innerHTML= ""
+  let card = ""
+  acervo.map(element => {
+    card = `<section class="card">
+      <img src="${element.imagem}">
+      <ul>
+        <li>Nome: ${element.titulo.toUpperCase()}</li>
+        <li>Autor: ${element.autor}</li>
+        <li>Adquirido: ${element.adquirido}</li>
+        <li>Tema: ${element.genero}</li>
+      </ul>
+    </section>`
+    container.innerHTML += card;
+  });
+}
+
+criarObjetosNoHtml(acervo)
