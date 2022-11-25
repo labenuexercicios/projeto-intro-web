118fb7c03bb1b2d2af2afb0012c6f625533f4363
claraferreira <ferreirabatistaclara@gmail.com>
2022-11-25 17:38:43 -0300

    Alteração

--- projeto-1.js
+++
@@ -1,7 +0,0 @@
-// Item: 
-// Composto químico: string 
-// Nome mais conhecido: string 
-// Átomos da formula: string 
-// Massa molar em gramas por mol: number 
-// Qual tipo de ligação? Iônico, covalente ou metálico? arrays
-// É solúvel em água? bolleano          
