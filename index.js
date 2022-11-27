c47c862f5fb72d9aa0f5a50253826afd091f839d
Italo Rocha <italo.rocha.de.oliveira@gmail.com>
2022-11-27 10:15:34 -0300

    Mandando a semana-2

--- index.js
+++ index.js
@@ -1,69 +1,97 @@
 //Comida
 
-const nomeDaComida = "Pizza";
-const descricao = "Uma massa com 'recheio' em cima";
-const ingredientes = [
-  "Farinha de trigo",
-  "Água",
-  "Sal",
-  "Açucar",
-  "Fermento biológico",
-  "+Recheio opicional",
-];
-const preço = 20;
-const vegetariano1 = true;
-const sabores1 = ["frango", "queijo", "carne", "outros"];
+const cardapio = [];
 
-const nomeDaComida2 = "Coxinha de frango";
-const descricao2 = "Uma massa com 'recheio' dentro de frango";
-const ingredientes2 = [
-  "Farinha de trigo",
-  "leite",
-  "Caldo de frango",
-  "Margarina",
-  "+Recheio de frango",
-];
-const preço2 = 5;
-const vegetariano2 = false;
-const sabores2 = ["Apenas frango"];
+const comida1 = {
+  nome: "Pizza",
+  descricao: "Uma massa com 'recheio' em cima",
+  ingredientes: [
+    "Farinha de trigo",
+    "Água",
+    "Sal",
+    "Açucar",
+    "Fermento biológico",
+    "+Recheio opicional",
+  ],
+  preço: 20,
+  vegetariano: true,
+  sabores: ["frango", "queijo", "carne", "outros"],
+};
 
-const nomeDaComida3 = "Pastel";
-const descricao3 = "Uma massa com recheio dentro";
-const ingredientes3 = [
-  "Farinha de trigo",
-  "Água",
-  "Sal",
-  "Óleo",
-  "aguardente",
-  "+Recheio opicional",
-];
-const preço3 = 10;
-const vegetariano3 = true;
-const sabores3 = ["frango", "queijo", "carne", "outros"];
+const comida2 = {
+  nome: "Coxinha de frango",
+  descricao: "Uma massa com 'recheio' dentro de frango",
+  ingredientes: [
+    "Farinha de trigo",
+    "leite",
+    "Caldo de frango",
+    "Margarina",
+    "+Recheio de frango",
+  ],
+  preço: 5,
+  vegetariano: false,
+  sabores: ["Apenas frango"],
+};
 
-const mediaDosPreços = (preço + preço2 + preço3) / 3;
+const comida3 = {
+  nome: "Pastel",
+  descricao: "Uma massa com recheio dentro",
+  ingredientes: [
+    "Farinha de trigo",
+    "Água",
+    "Sal",
+    "Óleo",
+    "aguardente",
+    "+Recheio opicional",
+  ],
+  preço: 10,
+  vegetariano: true,
+  sabores: ["frango", "queijo", "carne", "outros"],
+};
+
+cardapio.push(comida1);
+cardapio.push(comida2);
+cardapio.push(comida3);
+
+const mediaDosPreços =
+  (cardapio[0].preço + cardapio[1].preço + cardapio[2].preço) / 3;
 console.log("media dos preços: ", mediaDosPreços.toFixed(2));
 
-const vegetariano = vegetariano1 && vegetariano2 && vegetariano3;
-console.log("Os três são vegetariano?", vegetariano);
+const vegetariano =
+  cardapio[0].vegetariano && cardapio[1].vegetariano && cardapio[2].vegetariano;
+console.log("Os três tem opção vegetariano?", vegetariano);
 
-console.log(`Nome: ${nomeDaComida}
-Descrição: ${descricao}
-Ingredientes: ${ingredientes}
-Preço: ${preço}
-Tem opção vegetariana? ${vegetariano1}
-Sabores: ${sabores1}
+//Para adicionar novos valores:
 
-Nome: ${nomeDaComida2}
-Descrição: ${descricao2}
-Ingredientes: ${ingredientes2}
-Preço: ${preço2}
-Tem opção vegetariana? ${vegetariano2}
-Sabores: ${sabores2}
+const add = confirm(
+  "você deseja adicionar uma nova comida no cardapio? (sim ou não)"
+);
+cardapio.push(adicionarNovaComida(add));
 
-Nome: ${nomeDaComida3}
-Descrição: ${descricao3}
-Ingredientes: ${ingredientes3}
-Preço: ${preço3}
-Tem opção vegetariana? ${vegetariano3}
-Sabores: ${sabores3}`);
+function adicionarNovaComida(add) {
+  if (add) {
+    const comida = new Object();
+    comida.vegetariano = confirm("Essa comida tem opção vegetariana?");
+    if (comida.vegetariano) {
+      comida.nome = prompt("Nome da comida: ");
+      comida.descricao = prompt("Descreva essa comida: ");
+      comida.ingredientes = prompt(
+        "Coloque todos os ingredientes e separe com virgulas"
+      );
+      comida.ingredientes = comida.ingredientes.split(",");
+      comida.preço = Number(prompt("Insira a média de valor desse produto: "));
+      comida.sabores = prompt(
+        "Insira os possiveis sabores dessa comida e separa por virgula"
+      );
+      comida.sabores = comida.sabores.split(",");
+
+      return comida;
+    } else {
+      alert(
+        "Não foi possivel adicionar essa comida por não ter opção vegetariana"
+      );
+    }
+  }
+}
+
+console.log(cardapio);
