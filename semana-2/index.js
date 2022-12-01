b4f267ec257416691160bf2bcd528155d8956431
RocioSuarez <suarez.rocio4344@gmail.com>
2022-12-01 17:47:29 -0300

    commit de semana-2

---
+++ semana-2/index.js
@@ -0,0 +1,262 @@
+
+    const comidaArrozComPequi = {
+        Nome:"Arroz com pequi", 
+        Descricao: "O pequi é uma fruta brasileira, originária de Minas Gerais. Pode acompanhar o pequi com vários pratos, frango, arroz, entre outros.",
+        Ingredientes: "1 xícara de Arroz" 
+        +"1 xícara de Pequi"
+        +"2 colheres de sopa de Óleo de canola"
+        +"3 dentes de Alho"
+        +"2 xícaras de Água fervente"
+        +"1 unidade de Cebola pequena"
+        +"Sal a gosto"
+        +"Pimenta do reino a gosto"
+        +"Malagueta a gosto",
+        Preco: 300,
+        Categoria: "prato principal",
+        Vegetariano: "sim"
+       }
+       
+       let precoArrozComPequi = 300;
+       let vegetarianoArrozComPequi = true;
+       
+       //const comidaArrozComPequiArray = [];
+       //comidaArrozComPequiArray.push()
+    
+       //const comidaArrozComPequiArray = [ "1 xícara de Arroz","1 xícara de Pequi","2 colheres de sopa de Óleo de canola","3 dentes de Alho","2 xícaras de Água fervente",
+       //"1 unidade de Cebola pequena","Sal a gosto","Pimenta do reino a gosto","Malagueta a gosto"]
+       
+       const comidaVatapa = {
+           Nome:"Vatapá", 
+           Descricao: "O vatapá um prato típico das cozinhas das regiões Nordeste e Norte do Brasil. Ele é marca da influência da população negra nesses lugares. Quer conhecer outros pratos típicos nordestinos? Confira as receitas no TudoGostoso de acarajé, tapioca, moqueca, baião de dois, caruru, carne de sol e outras.",
+           Ingredientes: "1 kg de camarão salgado" 
+           +"1 garrafa pequena de leite de coco"
+           +"Cerca de 6 xícaras de trigo"
+           +"1 garrafa pequena de azeite de dendê"
+           +"Tomate, cebola, alho, pimenta e coloral a gosto"
+           +"Bastante cheiro verde",
+           Preco: 500,
+           Categoria: "prato principal",
+           Vegetariano: "nao"       
+       }
+       
+       let precoVatapa = 500;
+       let vegetarianoVatapa = false; 
+       
+       //const comidaVatapaArray = [];
+       //comidaVatapaArray.push()
+    
+       /*const comidaVatapaArray = ["1 kg de camarão salgado", "1 garrafa pequena de leite de coco","Cerca de 6 xícaras de trigo","1 garrafa pequena de azeite de dendê",
+       "Tomate, cebola, alho, pimenta e coloral a gosto", "Bastante cheiro verde"]*/
+       
+       const comidaBarreado = {
+           Nome:"Barreado", 
+           Descricao: "O barreado ou carne barreada é um prato típico do litoral paranaense, sendo o mais tradicional do estado",
+           Ingredientes: "2, 5 k de coxão mole"
+           +"300 g de bacon em cubos"
+           +"2 cebolas grandes, picadas"
+           +"5 dentes de alho, picados"
+           +"10 g de cominho em pó"
+           +"3 folhas de louro"
+           +"Sal e pimenta do reino, a gosto"
+           +"200 ml de vinagre de vinho"
+           +"4 colheres de sopa de óleo de milho"
+           +"2 l de água fervente",
+           Preco: 350,
+           Categoria: "prato principal",
+           Vegetariano: "nao"             
+       }
+       
+       let precoBarreado = 350;
+       let vegetarianoBarreado = false;
+       
+       //const comidaBarreadoArray = []
+       //comidaBarreadoArray.push()
+    
+       /*const comidaBarreadoArray = ["2, 5 k de coxão mole","300 g de bacon em cubos","2 cebolas grandes, picadas","5 dentes de alho, picados","10 g de cominho em pó",
+       "3 folhas de louro","Sal e pimenta do reino, a gosto","200 ml de vinagre de vinho","4 colheres de sopa de óleo de milho","2 l de água fervente"]*/
+       
+       const comidaPatoDoTucupi = {
+           Nome:"PATO NO TUCUPI", 
+           Descricao: "Pato no tucupi é típico da culinária brasileira, especialmente do estado do Pará. Tal é a fama do pato no tucupi, o prato mais icônico da culinária amazônica de Belém,",
+           Ingredientes: "1 pato de 3 kg"
+           +"3 litro de tucupi"
+           +"5 maços de jambu"
+           +"10 dentes de alho"
+           +"2 cebola"
+           +"1 maço de chicória do Pará"
+           +"sal a gosto"
+           +"2 tomates"
+           +"2 limão"
+           +"1 pitada de pimenta do reino"
+           +"1 colhe de manteiga"
+           +"2 colheres de azeite"
+           +"pimenta de cheiro",
+           Preco: 200,
+           Categoria: "prato principal",
+           Vegetariano: "nao"             
+       }
+       
+       let precoPatoDoTucupi = 200;
+       let vegetarinoPatoDoTucupi = false;
+       
+       //const comidaPatoNoTucupiArray = [];
+       //comidaPatoNoTucupiArray.push();
+    
+       /*const comidaPatoNoTucupiArray = [ "1 pato de 3 kg","3 litro de tucupi","5 maços de jambu","10 dentes de alho","2 cebola","1 maço de chicória do Pará",
+       "sal a gosto","2 tomates", "2 limao" , "1 pitada de pimenta do reino","1 colhe de manteiga","2 colheres de azeite","pimenta de cheiro"]*/
+       
+       const comidaTutuDoFeijao = {
+           Nome:"TUTU DE FEIJÃO", 
+           Descricao: "O TUTU de feijão! Que nada mais é, que uma junção do feijão com a farinha, mas com uma preparação e história diferentes do tropeiro! A palavra tutu vem do quimbundo quitutu, da língua africana “banta” mais falada em Angola, e quer dizer “papão”.",
+           Ingredientes: "2 xícaras de feijão cozido"
+           +"1 cebola pequena picada em cubinhos"
+           +"100 g de bacon picado em cubinhos"
+           +"3 dentes de alho picadinhos"
+           +"1 xícara de cheiro verde picado (opcional salsão)"
+           +"1 gomo de linguiça calabreza cortada em rodelas"
+           +"Farinha de mandioca crua"
+           +"Orégano, sal e pimenta do reino a gosto"
+           +"Óleo de milho ou soja",
+           Preco: 405,
+           Categoria: "prato principal",
+           Vegetariano: "nao"             
+       }
+       
+       let precoTutuDoFeijao = 405;
+       let vegeterianoTutuDoFeijao = false;
+       
+       //const comidaTutuDoFeijaoArray = [];
+       //comidaTutuDoFeijaoArray.push();
+    
+       /*const comidaTutuDoFeijaoArray = ["2 xícaras de feijão cozido","1 cebola pequena picada em cubinhos","100 g de bacon picado em cubinhos","3 dentes de alho picadinhos",
+       "1 xícara de cheiro verde picado (opcional salsão)","1 gomo de linguiça calabreza cortada em rodelas","Farinha de mandioca crua","Orégano, sal e pimenta do reino a gosto",
+       "Óleo de milho ou soja"]*/
+       
+       const comidaPiracucuDeCasacao = {
+           Nome:"PIRARUCU DE CASACA", 
+           Descricao: "Este prato é muito comum no estado do Amazonas, principalmente em Manaus, porém é muito apreciado também no Acre.",
+           Ingredientes: "1 kg de pirarucu (peixe) salgado"
+           +"200 g de farinha de mandioca"
+           +"5 bananas da terra bem maduras"
+           +"1 vidro de leite de coco (200 ml)"
+           +"1 lata de azeite (200 ml)"
+           +"Batata palha"
+           +"4 tomates"
+           +"2 cebolas grandes"
+           +"1 pimentão pequeno"
+           +"1 cheiro verde",
+           Preco: 350,
+           Categoria: "prato principal",
+           Vegetariano: "nao"                 
+       }
+       
+       let precoPiracucuDeCasacao = 350;
+       let vegetarianoPiracucuDeCasacao = false;
+       
+       //const comidaPiracucuDeCasacaoArray = [];
+       //comidaPiracucuDeCasacaoArray.push(); 
+    
+       /*const comidaPiracucuDeCasacaoArray = ["1 kg de pirarucu (peixe) salgado","200 g de farinha de mandioca","5 bananas da terra bem maduras","1 vidro de leite de coco (200 ml)"
+       ,"1 lata de azeite (200 ml)","Batata palha","4 tomates","2 cebolas grandes","1 pimentão pequeno","1 cheiro verde"]*/
+       
+       const comidaArroz = {
+           Nome: "Arroz",
+           Descricao: "O arroz é uma erva gramínea cujas sementes (grãos) são comestíveis.",
+           Ingredientes: "1 xícara de arroz lavado"
+           +"2 xícaras de água fervente"
+           +"1 dente de alho amassado"
+           +"1/4 de cebola picada"
+           +"azeite o suficiente"
+           +"sal a gosto",
+           Preco: 50,
+           Categoria: "acompanhamento",
+           Vegetariano: "sim"    
+       }
+       
+       let precoArroz = 50;    
+       let vegeterianoArroz = true;
+       
+       //const comidaArrozArray = [];
+       //comidaArrozArray.push();
+    
+    
+       /*const comidaArrozArray = ["1 xícara de arroz lavado", "2 xícaras de água fervente","1 dente de alho amassado", "1/4 de cebola picada", "azeite o suficiente", 
+       "sal a gosto"]*/
+       
+       const comidaBrigadeiro = {
+           Nome: "Brigadeiro",
+           Descricao: "Pequeno doce redondo feito com leite condensado e chocolate, geralmente coberto de chocolate granulado",
+           Ingredientes: "1 pote de leite condensado"
+           +"¼ xícara de Chocolate Amargo Gourmet em pó"
+           +"2 colheres de sopa de manteiga, macia"
+           +"1 colher de chá de Essência Gourmet de Baunilha"
+           +"100 gr de Chocolate Granulado Gourmet",
+           Preco: 30,
+           Categoria: "sobremesa",
+           Vegetariano: "nao"        
+       }
+       
+       let precoBrigadeiro = 30;
+       let vegeterianoBrigadeiro = false;
+       
+       //const comidaBrigadeiroArray = [];
+       //comidaBrigadeiroArray.push();
+    
+        /*const comidaBrigadeiroArray = [ "1 pote de leite condensado","¼ xícara de Chocolate Amargo Gourmet em pó", "2 colheres de sopa de manteiga, macia", "1 colher de chá de Essência Gourmet de Baunilha", 
+        "100 gr de Chocolate Granulado Gourmet"]*/
+       
+        
+
+   //media
+   
+   let preco = precoArroz + precoArrozComPequi + precoBarreado + precoBrigadeiro + precoPatoDoTucupi + precoPiracucuDeCasacao + precoTutuDoFeijao + precoVatapa;
+   let precos = 8;
+   
+   
+   let media = preco/precos;
+   
+   console.log(media)
+   
+   
+   /**Com um console.log, imprima o resultado de uma operação lógica que checa se todos os valores de variáveis booleanas criadas até aqui são verdadeiras. */
+   
+   const verificacionVegetariano = vegetarianoArrozComPequi && vegetarianoBarreado && vegetarianoPiracucuDeCasacao && vegetarianoVatapa && vegetarinoPatoDoTucupi
+   && vegeterianoArroz && vegeterianoBrigadeiro && vegeterianoTutuDoFeijao;
+   verificacionVegetariano && false;
+   console.log(verificacionVegetariano);  
+   
+   
+   console.log(comidaArroz)
+   console.log(comidaArrozComPequi)
+   console.log(comidaBarreado)
+   console.log(comidaPatoDoTucupi)
+   console.log(comidaPiracucuDeCasacao)
+   console.log(comidaVatapa)
+   console.log(comidaPiracucuDeCasacao)
+  /* console.log(comidaArrozArray)
+   console.log(comidaArrozComPequiArray)
+   console.log(comidaBarreadoArray)
+   console.log(comidaBrigadeiroArray)
+   console.log(comidaPatoNoTucupiArray)
+   console.log(comidaPiracucuDeCasacaoArray)*/
+
+
+   //creando un array
+    const COMIDA = [];
+  
+    const verificacionArray = COMIDA >= -1;
+    console.log(verificacionArray)
+
+    COMIDA.push(comidaArroz, comidaArrozComPequi, comidaBarreado, comidaBrigadeiro, comidaPatoDoTucupi, comidaPiracucuDeCasacao, comidaTutuDoFeijao,)   
+    console.log(COMIDA)
+   
+    let valorUm = true
+    let valorDois = false
+
+    if ( valorUm && valorDois  ){
+       COMIDA.push(comidaArroz, comidaArrozComPequi, comidaBarreado, comidaBrigadeiro, comidaPatoDoTucupi, comidaPiracucuDeCasacao, comidaTutuDoFeijao,)   
+       console.log(COMIDA)
+   
+    } else{
+        alert("item não foi adicionado, e não faça o push")
+    }
