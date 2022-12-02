b7d43c896a95b29743c875748573a537688e57fb
DAVID BRUNO <stephanierodrigues75@gmail.com>
2022-12-02 20:35:35 -0300

    Semana 2 resolvida

--- Projeto1.js
+++ Projeto1.js
@@ -29,35 +29,35 @@ const cilind2 = [125, 240, 300, 400]
 const media = (anofab + anofab1 + anofab2)/3
 console.log(media.toFixed(0)) //2014
 
-//Parte 2
+// //Parte 2
 const economias = economia && economia1 && economia2
 console.log(economias) //false
 
-//Se fosse tabelado
-//const titulo = "MOTO 1"
-//console.table(titulo)
-/*const informacoes = [
-    { Nome: 'MOTO1',
-      Marca: 'Honda',
-      Ano: '2015',
-      Economica: true,
-      Cilindrada: '125'
-    },
-    { Nome: 'MOTO2',
-    Marca: 'Yamaha',
-    Ano: '2010',
-    Economica: true,
-    Cilindrada: '160'
-    },
-    {Nome: 'MOTO3',
-    Marca: 'Kawasaki',
-    Ano: '2018',
-    Economica: false,
-    Cilindrada: '400'
-}
-]
-console.table(informacoes)
-*/
+// //Se fosse tabelado
+// //const titulo = "MOTO 1"
+// //console.table(titulo)
+// /*const informacoes = [
+//     { Nome: 'MOTO1',
+//       Marca: 'Honda',
+//       Ano: '2015',
+//       Economica: true,
+//       Cilindrada: '125'
+//     },
+//     { Nome: 'MOTO2',
+//     Marca: 'Yamaha',
+//     Ano: '2010',
+//     Economica: true,
+//     Cilindrada: '160'
+//     },
+//     {Nome: 'MOTO3',
+//     Marca: 'Kawasaki',
+//     Ano: '2018',
+//     Economica: false,
+//     Cilindrada: '400'
+// }
+// ]
+// console.table(informacoes)
+// */
 console.log(`MOTO HONDA
 Marca: ${marca}
 Ano: ${anofab}
@@ -77,32 +77,56 @@ Econômica: ${economia2}
 Cilindrada: ${cilind2}`)
 
 //Semana 2
+// Item 3
+
 const informacoes = []
 
-informacoes.push( {
-  Nome: 'MOTO1',
-      Marca: 'Honda',
-      Ano: '2015',
-      Economica: true,
-      Cilindrada: '125'
-    },
-    { Nome: 'MOTO2',
-    Marca: 'Yamaha',
-    Ano: '2010',
-    Economica: true,
-    Cilindrada: '160'
-    },
-    {Nome: 'MOTO3',
-    Marca: 'Kawasaki',
-    Ano: '2018',
-    Economica: false,
-    Cilindrada: '400'
-}
-);
-console.log(informacoes)
+moto1 = {
+   nome: "MOTO1",
+  marca: "Honda",
+  ano: 2015,
+  economica: true,
+  cilindrada: 125
+};
+
+moto2 = { 
+mome: "MOTO2",
+marca: "Yamaha",
+ano: 2010,
+economica: true,
+cilindrada: 160
+};
+
+moto3 =  {
+nome: "MOTO3",
+marca: "Kawasaki",
+ano: 2018,
+economica: false,
+cilindrada: 400
+};
+
 
 //Altere o item 3 “Adicione os novos objetos no array de objetos,
 //utilizando o push()”, para criar uma verificação antes de dar o push.
 //A caraterística booleana do objeto deve ser validada.
 // Isto é, o objeto só deve ser adicionado ao array 
 //se a propriedade booleana for true;
+
+
+if (moto1.economica === true) {
+  informacoes.push(moto1)
+  console.table(moto1)
+}else{
+  alert(moto1.nome +  " Não é econômica")
+}
+if (moto2.economica === true) {
+  informacoes.push(moto2)
+  console.table(moto2)
+}else{
+  alert(moto2.nome +  " Não é econômica")
+}
+if(moto3.economica === true){
+  informacoes.push(moto3)
+}else{
+  alert(moto3.nome + " Não é econômica")
+}
