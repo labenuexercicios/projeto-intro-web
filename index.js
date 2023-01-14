ae9f81eecc402b6ad75e41646c8d3be8789637d1
claraferreira <ferreirabatistaclara@gmail.com>
2023-01-13 21:17:58 -0300

    13-01 Finalizações do projeto

--- index.js
+++ index.js
@@ -1,42 +1,68 @@
-
 let input = document.getElementById("search");
-console.log(input);
-input.addEventListener("keypress", function(event) {
-  if (event.key === "Enter") {
-    printSearch()
-  }
+input.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        printSearch()
+    }
 });
-
-//list de objects 
-const chemicalCompound = [
+/* - [*]  Tela de listagem com título
+- [*]  ao menos três itens listados */
+const chemicalCompoundList = [
     {
-        nome: "Hipoclorito de sódio",
+        id: "0",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Sodium-hypochlorite.png/1024px-Sodium-hypochlorite.png",
+        nomeCientifico: "Hipoclorito de sódio",
         nomeMaisConhecido: "Água Sanitária",
         atomosDaFormula: ["Sódio", "Cloro", "Oxigênio"],
         massaMolar: 74.44,
         usos: ["Remoção de manchas de roupas sujas", "desenfetar superfícies", "desinfecção da água potável"],
-        solubilidadeEmAgua: true
+        solubilidadeEmAgua: true,
+        link: "https://pt.wikipedia.org/wiki/Hipoclorito_de_s%C3%B3dio"
     },
 
     {
-        nome: "Ácido acético",
+        id: "1",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Acetic-acid-2D-flat.png/1024px-Acetic-acid-2D-flat.png",
+        nomeCientifico: "Ácido acético",
         nomeMaisConhecido: "Vinagre",
         atomosDaFormula: ["Carbono", "Hidrogênio", "Oxigênio"],
         massaMolar: 60.052,
         usos: ["Condimentar saladas", "produção da aspirina", "síntese de perfumes e corantes"],
-        solubilidadeEmAgua: true
+        solubilidadeEmAgua: true,
+        link: "https://pt.wikipedia.org/wiki/%C3%81cido_etanoico"
     },
     {
-        nome: "Ácido ascórbico",
+        id: "2",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/L-Ascorbic_acid.svg/1024px-L-Ascorbic_acid.svg.png",
+        nomeCientifico: "Ácido ascórbico",
         nomeMaisConhecido: "Vitamina C",
         atomosDaFormula: ["Carbono", "Hidrogênio", "Oxigênio"],
         massaMolar: 176.09,
         usos: ["Suplementação alimentar", "adubo para plantas", "conservação de alimentos industrializados"],
-        solubilidadeEmAgua: true
+        solubilidadeEmAgua: true,
+        link: "https://pt.wikipedia.org/wiki/%C3%81cido_asc%C3%B3rbico"
+
     }]
+/* - [*]  Itens listados **utilizando manipulação do DOM** */
+function render(chemicalCompoundList) {
+    const cards = document.getElementById("cards")
+    let card = "";
+    for (const chemicalCompound of chemicalCompoundList) {
+        card += `
+        <div id="${chemicalCompound.id}" class = "container">
+        <img class= "molecule" src= "${chemicalCompound.image}">
+        <ul>
+            <li id ="${chemicalCompound.nomeCientifico}">Nome Cientifico: <a href="${chemicalCompound.link}"> ${chemicalCompound.nomeCientifico}</a></li>
+            <li>Nome mais conhecido: ${chemicalCompound.nomeMaisConhecido}</li>
+            <li>Usos: ${chemicalCompound.usos.join(", ")}</li>
+            <li>Massa Molar: ${chemicalCompound.massaMolar} g/mol</li>
+            </ul>
 
-//Verificar se é true ou false 
-
+        </div>`
+    }
+    cards.innerHTML = card;
+}
+render(chemicalCompoundList)
+/* - [*]  Verificação de valores booleanos true impressa no console */
 const specimen = []
 function verificar(list) {
     for (const i in list) {
@@ -44,15 +70,13 @@ function verificar(list) {
             specimen.push(list)
         }
         else {
-            alert(`${list[i].nome} não é solúvel em água`)
+            alert(`${list[i].nomeCientifico} não é solúvel em água`)
 
         }
     }
 }
-verificar(chemicalCompound)
-
-//cálculo das médias 
-
+verificar(chemicalCompoundList)
+/* - [*]  Média numérica calculada e impressa no console */
 function averageCalculation(list) {
     let numerator = 0
     for (const i in list) {
@@ -60,53 +84,56 @@ function averageCalculation(list) {
     }
     return numerator / list.length
 }
-
-averageCalculation(chemicalCompound)
-
-console.log(`A média dos valores de massa molar é: ${averageCalculation(chemicalCompound).toFixed(2)}`)
+averageCalculation(chemicalCompoundList)
+console.log(`A média dos valores de massa molar é: ${averageCalculation(chemicalCompoundList).toFixed(2)}`)
 console.log("\n")
-
-//refatoração 
+/* - [*]  Relatório impresso no console com as informações de cada item */
+/* - [*]  título dos itens em letras maiúsculas */
 function print(array) {
     for (const object of array) {
         for (const property in object) {
-            if (Array.isArray(object[property])) {
+            if ((object[property]) === object["nomeCientifico"]) {
+                console.log(`${object["nomeCientifico"].toUpperCase()}`)
+            }
+            else if (Array.isArray(object[property])) {
                 console.log(`${property}: ${object[property].join(", ")}`)
-            } else {
+            }
+
+            else if ((object[property]) === object["link"] || ((object[property]) === object["image"])) {
+                continue
+            }
+            else {
                 console.log(`${property}: ${object[property]}`);
             }
+
         }
         console.log("\n")
     }
 
 }
-print(chemicalCompound)
+print(chemicalCompoundList)
+/* - [*]  Barra de pesquisa com alerta se tentar pesquisar com campo em branco
+- [*]  Clique do botão executa busca entre os itens  */
 
-//função de busca
-function returnName(array, string) {
-    let busca
-    for (i in array) {
-        if (array[i].nome.toUpperCase().includes(string.toUpperCase())) {
-            busca = array[i]
-            break
-        } else {
-            busca = null
+function displayElement(chemicalCompoundList, searchString) {
+    for (i in chemicalCompoundList) {
+        if (chemicalCompoundList[i].nomeCientifico.toUpperCase().includes(searchString.toUpperCase())) {
+            let item = document.getElementById(chemicalCompoundList[i].id)
+            item.style.display = ""
         }
     }
-    return (busca)
 }
 
 function printSearch() {
-    let container = document.getElementsByClassName("container")
-    for (container of document.getElementsByClassName("container")) {
-        container.style.display="none"
+    let containerList = document.getElementsByClassName("container")
+    for (container of containerList) {
+        container.style.display = "none"
     }
     let item = document.getElementById("search").value
-    const card = returnName(chemicalCompound, item)
-
-    if (card !== null){
-        container.style.display="block"
+    if (document.getElementById("search").value.length === 0) {
+        alert("Digite algo no campo de pesquisa")
+    }
+    else {
+        displayElement(chemicalCompoundList, item)
     }
 }
-
-
